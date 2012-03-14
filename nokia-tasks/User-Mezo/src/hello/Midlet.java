2b05d0adc2fa0100f6d5e5844e2f928df3c37d0c
mouaz.alabsawi <mouaz.alabsawi@gmail.com>
2012-03-14 23:48:15 +0000

    Mouaz - C3 - Calculator - my commit

---
+++ nokia-tasks/User-Mezo/src/hello/Midlet.java
@@ -0,0 +1,157 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * @author Mouaz
+ */
+
+package hello;
+
+import com.sun.lwuit.Button;
+import com.sun.lwuit.Command;
+import com.sun.lwuit.Display;
+import com.sun.lwuit.Form;
+import com.sun.lwuit.Label;
+import com.sun.lwuit.TextArea;
+import com.sun.lwuit.TextField;
+import com.sun.lwuit.events.ActionEvent;
+import com.sun.lwuit.events.ActionListener;
+import com.sun.lwuit.layouts.BoxLayout;
+import javax.microedition.midlet.*;
+
+/**
+ * @author MESAI
+ */
+public class Midlet extends MIDlet {
+
+    Form mainForm; //this is your main Form
+    public void startApp() {
+         Display.init(this);
+         createMainUI();//this method is a way to organize your code ican write whats inside createMainUI()here
+         
+    }
+    public void createMainUI(){
+        mainForm = new Form("Tutorial 1");
+        mainForm.setLayout(new BoxLayout(BoxLayout.Y_AXIS));
+        //// A TextField,it is final to be able to use in the action listener part 
+        //and in addition i will no change its value later so no problem
+        // but if you need to change the value later you can set as a global variable
+        final TextField input = new TextField();
+        input.setConstraint(TextField.NUMERIC);
+        input.setHint("first operand");
+        final TextField input2 = new TextField();
+        input2.setConstraint(TextField.NUMERIC);
+        input2.setHint("second operand");
+        //// A TextArea, it is final to be able to use in the action listener part 
+        //and in addition i will no change its value later so no problem
+        // but if you need to change the value later you can set as a global variable
+        final Label output = new Label("Output");
+        //// Abutton to insert
+        Button add = new Button("Add them");
+        add.addActionListener(new ActionListener(){
+
+            public void actionPerformed(ActionEvent ae) {
+                
+                if(input.getText().length()!=0){
+                    
+                    int x = Integer.parseInt(input.getText()) + Integer.parseInt(input2.getText());
+                    
+                    String y = ""+x;
+               output.setText(y);
+                }
+                else{
+                    output.setText("Write Something Please");
+                    
+                }
+            }
+            
+        });
+        Button subtract = new Button("subtract them");
+        subtract.addActionListener(new ActionListener(){
+
+            public void actionPerformed(ActionEvent ae) {
+                
+                if(input.getText().length()!=0){
+                    
+                    int x = Integer.parseInt(input.getText()) - Integer.parseInt(input2.getText());
+                    
+                    String y = ""+x;
+               output.setText(y);
+                }
+                else{
+                    output.setText("Write Something Please");
+                    
+                }
+            }
+            
+        });
+        Button multiply = new Button("multiply them");
+        multiply.addActionListener(new ActionListener(){
+
+            public void actionPerformed(ActionEvent ae) {
+                
+                if(input.getText().length()!=0){
+                    
+                    int x = Integer.parseInt(input.getText()) * Integer.parseInt(input2.getText());
+                    
+                    String y = ""+x;
+               output.setText(y);
+                }
+                else{
+                    output.setText("Write Something Please");
+                    
+                }
+            }
+            
+        });
+        Button divide = new Button("divide them");
+        divide.addActionListener(new ActionListener(){
+
+            public void actionPerformed(ActionEvent ae) {
+                
+                if(input.getText().length()!=0){
+                    
+                    double x = Double.parseDouble(input.getText()) / Double.parseDouble(input2.getText());
+                    
+                    String y = ""+x;
+               output.setText(y);
+                }
+                else{
+                    output.setText("Write Something Please");
+                    
+                }
+            }
+            
+        });
+        // a command to exit
+        Command exitCommand = new Command("Exit"){
+
+            public void actionPerformed(ActionEvent evt) {
+                notifyDestroyed();
+            }
+            
+        };
+        mainForm.addCommand(exitCommand);
+        mainForm.addComponent(output);
+        mainForm.addComponent(input);
+        mainForm.addComponent(input2);
+        mainForm.addComponent(add);
+        mainForm.addComponent(subtract);
+        mainForm.addComponent(multiply);
+        mainForm.addComponent(divide);
+        mainForm.addCommand(exitCommand);
+        // last thing you need to shoe all this on the screen
+        mainForm.show();
+        
+        
+        
+    }
+    public void pauseApp() {
+    }
+    
+    public void destroyApp(boolean unconditional) {
+    }
+}
+
