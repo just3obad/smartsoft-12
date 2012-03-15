4c6daed7c6f690743700a2f55f99d4edc4a99111
just3obad <just3obad@gmail.com>
2012-03-15 21:57:07 +0000

    [3OABD]; [C2]; [Basic Calculator]; [Modified Calculator]

--- nokia-tasks/user-3OBAD/src/hello/Midlet.java
+++ nokia-tasks/user-3OBAD/src/hello/Midlet.java
@@ -1,56 +1,91 @@
 package hello;
 
-import com.sun.lwuit.Button;
-import com.sun.lwuit.Command;
-import com.sun.lwuit.Display;
-import com.sun.lwuit.Form;
-import com.sun.lwuit.Label;
-import com.sun.lwuit.TextArea;
-import com.sun.lwuit.TextField;
+import com.sun.lwuit.*;
 import com.sun.lwuit.events.ActionEvent;
 import com.sun.lwuit.events.ActionListener;
 import com.sun.lwuit.layouts.BoxLayout;
+import com.sun.lwuit.layouts.GridLayout;
+import com.sun.lwuit.plaf.UIManager;
+import com.sun.lwuit.util.Resources;
 import javax.microedition.midlet.*;
 
 public class Midlet extends MIDlet {
 
     Form mainForm;
+    double result=0;
+    String old="";
+    boolean flag = true;
+    int operator ;
+    int counter=0;
+    
+    
+    
     public void startApp() {
          Display.init(this);
          createMainUI();
+        // createTheme();
          
     }
     public void createMainUI(){
         mainForm = new Form("Simple Calculator");
         mainForm.setLayout(new BoxLayout(BoxLayout.Y_AXIS));
+        mainForm.setDraggable(false);
+        Container cont = new Container();
+        cont.setLayout(new GridLayout(3, 2));
+       
         
         final TextField input1 = new TextField();
-       // String in1 = input1.getText();
-        //final int n1 = Integer.parseInt(in1);
         input1.setHint("First number here");
+        input1.setConstraint(TextField.NUMERIC);
         
-        final TextField input2 = new TextField();
-        //String in2 = input2.getText();
-        //final int n2 = Integer.parseInt(in2);
-        input2.setHint("Second number here");
+        final TextArea op = new TextArea();
+        op.setHint("Equation");
+        op.setEditable(false);
+        //op.setHeight(100);
+        op.setPreferredH(100);
         
-        final Label result = new Label("Result here");
+        final Label out = new Label("Result here");
+        
+        
         
         Button add = new Button("+");
         Button sub = new Button("-");
         Button mul = new Button("X");
         Button div = new Button("÷");
+        Button eq = new Button("=");
+        Button cl = new Button("C");
+        
+        cont.addComponent(add);
+        cont.addComponent(sub);
+        cont.addComponent(mul);
+        cont.addComponent(div);
+        cont.addComponent(eq);
+        cont.addComponent(cl);
+        
+        cl.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent ae) {
+                input1.clear();
+                result = 0;
+                out.setText("0");
+                op.setText("");
+            }
+        });
         
         add.addActionListener(new ActionListener(){
 
             public void actionPerformed(ActionEvent ae)  {
 
-                    try{
-                        result.setText(Integer.parseInt(input1.getText()) + Integer.parseInt(input2.getText()) + "");
-                    }
-                    catch(NumberFormatException e){
-                        result.setText("input error");
-                    }
+                   try{
+                        operator = 0;
+                        result += Double.parseDouble(input1.getText());
+                        flag=false;
+  
+                   }
+                   catch(Exception e){
+                       out.setText("Input Error");
+                   }
+    
             }
             
         });
@@ -60,10 +95,13 @@ public class Midlet extends MIDlet {
             public void actionPerformed(ActionEvent ae)  {
 
                     try{
-                        result.setText(Integer.parseInt(input1.getText()) / Integer.parseInt(input2.getText()) + "");
+                        operator = 1;
+                        result /= Double.parseDouble(input1.getText());
+                        flag=false;
+                       
                     }
                     catch(NumberFormatException e){
-                        result.setText("input error");
+                        out.setText("input error");
                     }
             }
             
@@ -74,10 +112,13 @@ public class Midlet extends MIDlet {
             public void actionPerformed(ActionEvent ae)  {
 
                     try{
-                        result.setText(Integer.parseInt(input1.getText()) - Integer.parseInt(input2.getText()) + "");
+                        operator = 2;
+                        result -= Double.parseDouble(input1.getText());
+                        flag=false;
+                      
                     }
                     catch(NumberFormatException e){
-                        result.setText("input error");
+                        out.setText("input error");
                     }
             }
             
@@ -88,16 +129,87 @@ public class Midlet extends MIDlet {
             public void actionPerformed(ActionEvent ae)  {
 
                     try{
-                        result.setText(Integer.parseInt(input1.getText()) * Integer.parseInt(input2.getText()) + "");
+                         operator = 3;
+                         result *= Double.parseDouble(input1.getText());
+                         flag=false;
+                         
                     }
                     catch(NumberFormatException e){
-                        result.setText("input error");
+                        out.setText("input error");
                     }
             }
             
         });
         
       
+           eq.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent ae) {
+
+                switch(operator)
+                {
+                    case 0:
+                        if(flag == true)
+                        {
+                          op.setText("");  
+                          
+                          
+                        }
+                        else{
+                        op.setText(old+"\n"+"+"+"\n"+input1.getText());
+                        old=result+"";
+                        
+                        }
+                        break;
+                    case 1:
+                        if(flag == true)
+                        {
+                          op.setText("");  
+                         
+                        }
+                        else{
+                            
+                        op.setText(old+"\n"+"÷"+"\n"+input1.getText());
+                        old=result+"";
+                        
+                        }
+                        break;
+                    case 2:
+                        if(flag == true)
+                        {
+                          op.setText("");  
+                          
+                        }
+                        else{
+                          
+                        op.setText(old+"\n"+"-"+"\n"+input1.getText());
+                        old=result+""; 
+                        
+                        }
+                        break;
+                    case 3:
+                        if(flag == true)
+                        {
+                          op.setText("");  
+                          
+                        }
+                        else{
+                        
+                        op.setText(old+"\n"+"x"+"\n"+input1.getText());
+                        old=result+"";
+                        
+                        }  
+                        break;
+                     default: op.setText(input1.getText()); 
+                }
+                 
+               out.setText(result+"");
+               
+               
+ 
+            }
+        });
+           
         Command exitCommand = new Command("Exit"){
 
             public void actionPerformed(ActionEvent evt) {
@@ -106,19 +218,28 @@ public class Midlet extends MIDlet {
             
         };
         mainForm.addCommand(exitCommand);
-        mainForm.addComponent(result);
+        mainForm.addComponent(out);
+        mainForm.addComponent(op);
         mainForm.addComponent(input1);
-        mainForm.addComponent(input2);
-        mainForm.addComponent(add);
-        mainForm.addComponent(sub);
-        mainForm.addComponent(mul);
-        mainForm.addComponent(div);
+        mainForm.addComponent(cont);
         mainForm.addCommand(exitCommand);
         mainForm.show();
         
         
         
     }
+    
+   /* private void createTheme() { 
+        try { 
+            Resources r; 
+            r = Resources.open("/Theme1.res");
+            UIManager.getInstance().setThemeProps(r.getTheme("Theme1")); 
+           mainForm.refreshTheme(); 
+           mainForm.show();
+        } catch (Exception e) { 
+            e.printStackTrace(); 
+        }
+    }*/
     public void pauseApp() {
     }
     
