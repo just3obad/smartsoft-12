16234ab577bf64e8f62626bf2108df4efeda1559
lydia.sidhom <lydia.sidhom@gmail.com>
2012-04-17 19:05:05 +0000

    Added controllers and models for User, Admin, Story and Interest + Updated database and routes

--- nokia_ruby/config/routes.rb
+++ nokia_ruby/config/routes.rb
@@ -1,4 +1,10 @@
 NokiaRuby::Application.routes.draw do
+
+  resources :users
+  resources :admins
+  resources :stories
+  resources :interests
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
