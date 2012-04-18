70f044f6f0ed710cf141d3d7d56ccdf3e8a37cde
christinesed <christinesed@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-04-18 23:00:43 +0000

    Update issue 16 adding statistics controller and helper

---
+++ trunk/nokia_ruby/app/helpers/statistics_helper.rb
@@ -0,0 +1,11 @@
+module StatisticsHelper
+ def getInterests
+  dataa=[[1,2,3,4,5],[2,4,6,8,10],[2,10,5,15,4],[8,5,8,3,0]]
+ end
+ def getNames
+  namesa=['Chr','koo','kkk']
+ end
+ def getkk
+  kkk= %w{one two three}
+ end
+end
