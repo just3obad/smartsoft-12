9a3708a915a48b63176fc0f5b2cf181b115b3aea
christinesed <christinesed@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-04-19 18:04:33 +0000

    Update issue 16 linked story page to its statistics page

--- nokia_ruby/app/controllers/statistics_controller.rb
+++ nokia_ruby/app/controllers/statistics_controller.rb
@@ -1,8 +1,11 @@
 class StatisticsController < ApplicationController
  def interests
+   @id=params[:id]
  end
  def stories
+   @id=params[:id]
  end
  def users
+   @id=params[:id]
  end
 end
--- nokia_ruby/app/controllers/stories_controller.rb
+++ nokia_ruby/app/controllers/stories_controller.rb
@@ -1,3 +1,4 @@
 class StoriesController < ApplicationController
-
+ def show
+ end
 end
