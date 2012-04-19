73c78550fba8256b830349e8cb9ea190b2c237c6
lydia.sidhom <lydia.sidhom@gmail.com>
2012-04-19 10:32:23 +0000

    Update issue 22 handling success and failure scenarios in shares_helper

--- nokia_ruby/app/helpers/shares_helper.rb
+++ nokia_ruby/app/helpers/shares_helper.rb
@@ -1,22 +1,41 @@
 module SharesHelper
-def get_no_of_shares(storyId)
-#this method gets the number of shares of a certain story using its id in the last 4 weeks
-  shares_by_day = Share.where(:created_at => 30.days.ago..Time.zone.now.end_of_day, :story_id => storyId).group("date(created_at)").select("created_at, count(story_id) as noOfSharesPerDay")
- (30.days.ago.to_date..Date.today).map do |date|
+  def get_no_of_shares(storyId)
+#this method gets the number of shares of a certain story using its id in the last 30 days
+creationOfStory = Story.find(storyId).created_at_before_type_cast.to_date
+lastUpdated = Story.find(storyId).updated_at_before_type_cast.to_date
+deleted = false
+#Story.where(:id => story_id).select("deleted")
+  if deleted && creationOfStory > 30.days.ago.to_date && lastUpdated > 30.days.ago.to_date
+    shares_by_day = Share.where(:created_at => creationOfStory..lastUpdated, :story_id => storyId).group("date(created_at)").select("created_at, count(story_id) as noOfSharesPerDay")
+    (creationOfStory.to_date..lastUpdated.to_date).map do |date|
       share = shares_by_day.detect { |share| share.created_at.to_date == date }
       share && share.noOfSharesPerDay.to_i || 0
     end.inspect
-end
+  elsif deleted && creationOfStory <= 30.days.ago.to_date && lastUpdated > 30.days.ago.to_date
+    shares_by_day = Share.where(:created_at => 30.days.ago..lastUpdated, :story_id => storyId).group("date(created_at)").select("created_at, count(story_id) as noOfSharesPerDay")
+    (30.days.ago.to_date..lastUpdated.to_date).map do |date|
+      share = shares_by_day.detect { |share| share.created_at.to_date == date }
+      share && share.noOfSharesPerDay.to_i || 0
+    end.inspect
+  elsif deleted && creationOfStory <= 30.days.ago.to_date && lastUpdated <= 30.days.ago.to_date
+    shares_by_day = [0]
+  elsif creationOfStory <= 30.days.ago.to_date
+    shares_by_day = Share.where(:created_at => 30.days.ago..Time.zone.now.end_of_day, :story_id => storyId).group("date(created_at)").select("created_at, count(story_id) as noOfSharesPerDay")
+    (30.days.ago.to_date..Date.today).map do |date|
+      share = shares_by_day.detect { |share| share.created_at.to_date == date }
+      share && share.noOfSharesPerDay.to_i || 0
+    end.inspect
+  else
+    shares_by_day = Share.where(:created_at => creationOfStory.beginning_of_day..Time.zone.now.end_of_day, :story_id => storyId).group("date(created_at)").select("created_at, count(story_id) as noOfSharesPerDay")
+    (creationOfStory.to_date..Date.today).map do |date|
+      share = shares_by_day.detect { |share| share.created_at.to_date == date }
+      share && share.noOfSharesPerDay.to_i || 0
+    end.inspect
+  end
+  end
 
-def get_who_shared_story(storyId)
-#this method gets the users who shared a certain story (of the given id) in the last 4 weeks
- ids = (Share.where(["created_at >= ? AND created_at <= ? AND story_id == ?",4.weeks.ago,Time.zone.now.end_of_day,storyId]))
-   
-
-#:created_at => 4.weeks.ago..Time.zone.now.end_of_day, :story_id => storyId).select("user_id")
- #users_who_shared=[]
- #ids.each do |id|
- # users_who_shared << User.find(id)
- #end
-end
+  def get_who_shared_story(storyId)
+#this method gets the users who shared a certain story (of the given id) in the last 30 days
+ ids = (Share.where(["created_at >= ? AND created_at <= ? AND story_id == ?",30.days.ago,Time.zone.now.end_of_day,storyId]))
+  end
 end
