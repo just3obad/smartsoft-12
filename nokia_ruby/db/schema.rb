d0bbc4068c5761cd077e9d0bc1ac57d97f85a63d
elmenisy <elmenisy@gmail.com>
2012-04-20 02:03:49 +0000

    Update issue 35 Added email and name to User model and migrated to DB Added User Views Added actions in User controller

--- nokia_ruby/db/schema.rb
+++ nokia_ruby/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120417213528) do
+ActiveRecord::Schema.define(:version => 20120420012715) do
 
   create_table "admins", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(:version => 20120417213528) do
   create_table "users", :force => true do |t|
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "name"
+    t.string   "email"
   end
 
 end
