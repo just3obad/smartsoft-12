54ac67f154d4c95135b1f1e0dd94fe06db3773fb
christinesed <christinesed@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-04-20 10:33:19 +0000

    Update issue 15 adding get_all_interests to interests_helper with documentation

--- nokia_ruby/app/helpers/interests_helper.rb
+++ nokia_ruby/app/helpers/interests_helper.rb
@@ -1,2 +1,6 @@
 module InterestsHelper
+  #This method when called will return an array of ActiveRecords having all interests in the database.
+  def get_all_interests
+    interests=Interest.all
+  end
 end
