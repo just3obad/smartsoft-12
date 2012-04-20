294287891cacc2866999ded533e1611d382eb938
christinesed <christinesed@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-04-20 19:53:21 +0000

    Update issue 15 making get_all_interests get the non-deleted interests

--- trunk/nokia_ruby/app/helpers/interests_helper.rb
+++ trunk/nokia_ruby/app/helpers/interests_helper.rb
@@ -1,7 +1,7 @@
 module InterestsHelper
   #This method when called will return an array of ActiveRecords having all interests in the database.
   def get_all_interests
-    interests=Interest.all
+    interests=Interest.where(:deleted => false)
   end
   
   #This method when called will return the difference between today and the day the interest was created in days.
