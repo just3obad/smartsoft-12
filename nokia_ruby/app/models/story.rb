0696498d373649f1c3731a1806211e32de1430c6
elmenisy <elmenisy@gmail.com>
2012-04-20 03:36:47 +0000

    Update issue 35 Re-commiting changes from last time

--- nokia_ruby/app/models/story.rb
+++ nokia_ruby/app/models/story.rb
@@ -1,5 +1,5 @@
 class Story < ActiveRecord::Base
-  # attr_accessible :title, :body
+  # attr_accessible :title,  :body
 
   has_many :shares, dependent: :destroy
   has_many :users, through: :shares
