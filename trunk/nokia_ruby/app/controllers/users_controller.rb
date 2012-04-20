4c84b6ffefbedfdd496bff9d00a5db6f4f111c6f
rana.helal92@gmail.com <rana.helal92@gmail.com@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-04-20 20:54:59 +0000

    Update issue 5 : adding methods for blocking feeds of interest.

--- trunk/nokia_ruby/app/controllers/users_controller.rb
+++ trunk/nokia_ruby/app/controllers/users_controller.rb
@@ -31,4 +31,19 @@ def create
   def index
     respond_with(@users = User.all)
   end
+ #this method takes an array of interests and blocks these interests   from the user's feed by changing the is_blocked attribute to true. The method depends on another method that gets the list of stories under these interests.
+  
+  #@story_list = getStories(self.interests) 
+  def block_interest(@story_list)
+    0.upto(@story_list.length) do |i|
+       @story_list[i].is_blocked = true
+    end
+  end
+
+  #this method takes a story as input and blocks it by setting the is_blocked attribute to true.
+
+  @story = Story.find(params[:id])
+  def block_story(@story)
+    @story.is_blocked = true
+  end
 end
