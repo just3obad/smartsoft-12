48510331520d7c4b2a64d39e30bc2e09a4651b58
christinesed <christinesed@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-04-20 19:55:18 +0000

    Update issue 16 Adding more documentation

--- trunk/nokia_ruby/app/helpers/statistics_helper.rb
+++ trunk/nokia_ruby/app/helpers/statistics_helper.rb
@@ -4,10 +4,22 @@ include StoriesHelper
 include InterestsHelper
 include UsersHelper
 
+#This is the method that should return the data of statistics of an interest
+#with this format first element in the data arrays is ARRAY OF "No Of Users",
+#second one is "No Of Articles"
+#third one is "No of videos"
+#and forth one is "No of images"
  def get_interest_stat(interest_id)
-  dataa=[[1,2,3,4,5,6,9,7,8,14,1,2,3,4,5,6,9,7,8,14,1,2,3,4,5,6,9,7,8,14],[2,4,6,8,10,7,4,6,8,7,2,4,6,8,10,7,4,6,8,7,2,4,6,8,10,7,4,6,8,7],[2,10,5,15,4,1,3,5,7,9,2,10,5,15,4,1,3,5,7,9,2,10,5,15,4,1,3,5,7,9],[8,5,8,3,0,7,5,6,4,3,8,5,8,3,0,7,5,6,4,3,8,5,8,3,0,7,5,6,4,3]]
+  #dataa=[[1,2,3,4,5,6,9,7,8,14,1,2,3,4,5,6,9,7,8,14,1,2,3,4,5,6,9,7,8,14],[2,4,6,8,10,7,4,6,8,7,2,4,6,8,10,7,4,6,8,7,2,4,6,8,10,7,4,6,8,7],[2,10,5,15,4,1,3,5,7,9,2,10,5,15,4,1,3,5,7,9,2,10,5,15,4,1,3,5,7,9],[8,5,8,3,0,7,5,6,4,3,8,5,8,3,0,7,5,6,4,3,8,5,8,3,0,7,5,6,4,3]]
   
  end
+ 
+
+#This is the method that should return the data of statistics of an story
+#with this format first element in the data arrays is ARRAY OF "No Of Shares",
+#second one is "No Of Likes"
+#third one is "No of Dislikes"
+#and forth one is "No of Spams"
  def get_story_stat(story_id)
  
  s = get_no_of_shares(story_id)
@@ -17,9 +29,15 @@ include UsersHelper
  data = "[#{s},#{n},#{m},#{p}]"
  end
 
- 
+
+ #This is the method that should return the data of statistics of an story
+#with this format first element in the data arrays is ARRAY OF "No Of Shares",
+#second one is "No Of Likes"
+#third one is "No of Dislikes"
+#and forth one is "No of Spams"
+#and fifth one is "No of Comments"
  def get_user_stat(user_id)
-  dataa=[[1,2,3,4,5,6,9,7,8,14,1,2,3,4,5,6,9,7,8,14,1,2,3,4,5,6,9,7,8,14],[2,4,6,8,10,7,4,6,8,7,2,4,6,8,10,7,4,6,8,7,2,4,6,8,10,7,4,6,8,7],[2,10,5,15,4,1,3,5,7,9,2,10,5,15,4,1,3,5,7,9,2,10,5,15,4,1,3,5,7,9],[8,5,8,3,0,7,5,6,4,3,8,5,8,3,0,7,5,6,4,3,8,5,8,3,0,7,5,6,4,3]]
+  #dataa=[[1,2,3,4,5,6,9,7,8,14,1,2,3,4,5,6,9,7,8,14,1,2,3,4,5,6,9,7,8,14],[2,4,6,8,10,7,4,6,8,7,2,4,6,8,10,7,4,6,8,7,2,4,6,8,10,7,4,6,8,7],[2,10,5,15,4,1,3,5,7,9,2,10,5,15,4,1,3,5,7,9,2,10,5,15,4,1,3,5,7,9],[8,5,8,3,0,7,5,6,4,3,8,5,8,3,0,7,5,6,4,3,8,5,8,3,0,7,5,6,4,3]]
  end
  
 end
