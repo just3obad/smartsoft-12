80d7072cc072f60ec7b325aa70250488917fd7ad
christinesed <christinesed@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-04-20 11:42:12 +0000

    Update issue 16 modifying the helpers and views providing a small API

--- trunk/nokia_ruby/app/helpers/stories_helper.rb
+++ trunk/nokia_ruby/app/helpers/stories_helper.rb
@@ -1,3 +1,6 @@
 module StoriesHelper
-
+  #This method when called will return the difference between today and the day the story was created in days.
+ def get_story_start_date(story_id)
+ 
+ end
 end
