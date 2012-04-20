22edca41e23934995d1e943dbcb5c4be4fe38c62
yahiaelgamal <yahiaelgamal@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-04-20 11:25:10 +0000

    Update issue 31
    Fixed rails issue of executing authentication_helper.rb script. Sorry for the inconvenience

--- nokia_ruby/app/helpers/authentication_helper.rb
+++ nokia_ruby/app/helpers/authentication_helper.rb
@@ -1,52 +1,89 @@
 require 'oauth'
 require 'twitter'
 
-$consumer_token  = 'cuE56v2eK79gsxK1baasA'
-$consumer_secret = 'vIJmDpiqDNpiau2YaiHH19fW1zj5DRXjI1VUbuM7lk' 
-$access_token
-$access_secret
+module AuthenticationHelper
+  # Applicatoin specific
+  $consumer_token  = 'cuE56v2eK79gsxK1baasA'
+  $consumer_secret = 'vIJmDpiqDNpiau2YaiHH19fW1zj5DRXjI1VUbuM7lk' 
 
-def generate_request_token
-  oa = OAuth::Consumer.new($consumer_token, $consumer_secret,
-                           :site=> 'http://api.twitter.com',
-                           :request_endpoint => 'http://api.twitter.com',
-                           :sign_in => true)
-  req = oa.get_request_token
-  request_secret = req.secret
-  request_token  = req.token
+  # User specific in this case @testapi9 it's password is testapi! 
+  $access_token    = '558012292-Sz82kMwDBxlt8Yo3wK4NA0m72VfirgE7TJBzOG1j'
+  $access_secret   = '1MuiXTaGQUrHiAKowJn6YAVHe9EONajEUBaTaB1tos'
 
-  url = "http://api.twitter.com/oauth/authorize?oauth_token=#{ request_token }"
-  puts url
-  req
+  =begin 
+  TODO handle excpetion such as,
+      Twitter is not over capacity (ServiceUnavailable),
+      You are not authozorized,
+      Duplicate status, 
+
+  =end
+  def generate_request_token
+    oa = OAuth::Consumer.new($consumer_token, $consumer_secret,
+                             :site=> 'http://api.twitter.com',
+                             :request_endpoint => 'http://api.twitter.com',
+                             :sign_in => true)
+    req = oa.get_request_token
+    request_secret = req.secret
+    request_token  = req.token
+
+    url = "http://api.twitter.com/oauth/authorize?oauth_token=#{ request_token }"
+    puts url
+    req
+  end 
+
+
+  def generate_access_token(req)
+    access = req.get_access_token
+    $access_token = access.token
+    puts 'access_token is ' + $access_token
+    $access_secret= access.secret
+    puts 'access_secret is ' + $access_secret
+  end 
+
+
+  def config_twitter
+    Twitter.configure do |config|
+      config.consumer_key         = $consumer_token
+      config.consumer_secret      = $consumer_secret
+      config.oauth_token          = $access_token
+      config.oauth_token_secret   = $access_secret
+    end
+  end 
+
+
+  def tweet(tweet)
+    client = Twitter::Client.new
+    client.update(tweet)
+  end 
+
+
+  # returns the time of the last tweet received
+  def get_main_feed(count=10)
+    feed = Twitter.home_timeline(:count => count)
+  end 
 end 
 
 
-def generate_access_token(req)
-  access = req.get_access_token
-  $access_token = access.token
-  $access_secret= access.secret
-end 
+#req = generate_request_token
+#puts 'Please visit the above url'
+#
+#puts "Press enter when authenticated"
+#gets
+#generate_access_token(req) 
 
+#config_twitter
 
-def tweet(tweet)
-  Twitter.configure do |config|
-    config.consumer_key         = $consumer_token
-    config.consumer_secret      = $consumer_secret
-    config.oauth_token          = $access_token
-    config.oauth_token_secret   = $access_secret
-  end
-  client = Twitter::Client.new
-  client.update(tweet)
-end 
+#puts Twitter.home_timeline(:count => 5)
+#Twitter.update("Will this work?")
 
-req = generate_request_token
-puts 'Please visit the above url'
+#puts "(t)weet or (f)eed?"
+#choice = gets.chomp
 
-puts "Press enter when authenticated"
-gets
-generate_access_token(req) 
-
-
-puts 'Please enter the tweet'
-tweet("#{ gets.chomp }")
-
+#if choice == 't'
+#  puts 'Please enter the tweet'
+#  tweet("#{ gets.chomp }")
+#elsif choice == 'f'
+#  puts 'coming soon'
+#else
+#end
+#
