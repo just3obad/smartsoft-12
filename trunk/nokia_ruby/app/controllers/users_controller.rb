2f576bcb438803c7a0c247b5fcb2a981696c926b
elmenisy@gmail.com <elmenisy@gmail.com@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-04-20 02:03:49 +0000

    Update issue 35 Added email and name to User model and migrated to DB Added User Views Added actions in User controller

--- trunk/nokia_ruby/app/controllers/users_controller.rb
+++ trunk/nokia_ruby/app/controllers/users_controller.rb
@@ -1,2 +1,34 @@
 class UsersController < ApplicationController
+	respond_to :html,:json
+
+ def show
+    respond_with(@user = User.find(params[:id]))
+  end  
+
+ 
+     def new
+    @user = User.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @user }
+   end
+  end
+def create
+    @user = User.new(params[:user])
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.json { render json: @user, status: :created, location: @user }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def index
+    respond_with(@users = User.all)
+  end
 end
--- trunk/nokia_ruby/app/models/user.rb
+++ trunk/nokia_ruby/app/models/user.rb
@@ -1,8 +1,14 @@
 class User < ActiveRecord::Base
   # attr_accessible :title, :body
-
+  attr_accessible :name, :email
   has_many :shares, dependent: :destroy
   has_many :stories, through: :shares
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :name, :presence => true,
+  :length => { :maximum => 20 }
+  validates :email, :presence => true,
+  :format=> {:with => email_regex },
+  :uniqueness => { :case_sensitive => false}
 
    def share!(storyid)
      shares.create!(story_id: storyid)
