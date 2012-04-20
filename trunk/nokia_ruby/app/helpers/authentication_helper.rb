328c640fe2971db146662dae15d6ae54fd3054a5
yahiaelgamal <yahiaelgamal@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-04-20 03:32:06 +0000

    Update issue 31
    Better testing scenario. Anyone can run it by going to the directory and typing ruby authenticate_hlper.rb

--- trunk/nokia_ruby/app/helpers/authentication_helper.rb
+++ trunk/nokia_ruby/app/helpers/authentication_helper.rb
@@ -3,6 +3,8 @@ require 'twitter'
 
 $consumer_token  = 'cuE56v2eK79gsxK1baasA'
 $consumer_secret = 'vIJmDpiqDNpiau2YaiHH19fW1zj5DRXjI1VUbuM7lk' 
+$access_token
+$access_secret
 
 def generate_request_token
   oa = OAuth::Consumer.new($consumer_token, $consumer_secret,
@@ -13,41 +15,38 @@ def generate_request_token
   request_secret = req.secret
   request_token  = req.token
 
-  url = "http://api.twitter.com/oauth/authorize?oauth_token=#{ rtoken }"
-  {'url' => url, 'request' => req}
-end
+  url = "http://api.twitter.com/oauth/authorize?oauth_token=#{ request_token }"
+  puts url
+  req
+end 
 
 
 def generate_access_token(req)
   access = req.get_access_token
-  access_token = access.token
-  acces_secret = access.secret 
-  returh Hash['access_token', access_token, 'access_secret', acces_secret]
-end
-  
+  $access_token = access.token
+  $access_secret= access.secret
+end 
 
-def configure_twitter(access_token, access_secret)
+
+def tweet(tweet)
   Twitter.configure do |config|
-    config.consumer_key     = $consumer_token
-    config.consumer_secret  = $consumer_secret
-    config.access_token     = access_token
-    config.access_secret    = access_secret
+    config.consumer_key         = $consumer_token
+    config.consumer_secret      = $consumer_secret
+    config.oauth_token          = $access_token
+    config.oauth_token_secret   = $access_secret
   end
-end
-
-
-def tweet(tweet, access_token, access_secret)
-  configure_twitter($consumer_secret, access_token, access_secret) 
   client = Twitter::Client.new
   client.update(tweet)
-end
+end 
+
+req = generate_request_token
+puts 'Please visit the above url'
+
+puts "Press enter when authenticated"
+gets
+generate_access_token(req) 
 
 
-def get_main_feed($consumer_token,
-                  $consumer_secret,
-                  access_token,
-                  access_secret)
-  ['coming', 'soon']
-end
-                   
-  
+puts 'Please enter the tweet'
+tweet("#{ gets.chomp }")
+
