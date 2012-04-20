e09fc4099834bd257b8acca3c41149f0dfac3512
essamahmedhafez <essamahmedhafez@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-04-20 20:45:51 +0000

    resolving conflicts

--- trunk/nokia_ruby/app/controllers/stories_controller.rb
+++ trunk/nokia_ruby/app/controllers/stories_controller.rb
@@ -4,13 +4,13 @@ respond_to :html,:json
    @comments = Comment.find_all_by_story_id(params[:id]) # get comments of this story
    @story = Story.find(params[:id])
  end
-<<<<<<< .mine
+#<<<<<<< .mine
 
 def index
     respond_with(@stories = Story.all)
  end
 
-=======
+#=======
  
->>>>>>> .r169
+#>>>>>>> .r169
 end
