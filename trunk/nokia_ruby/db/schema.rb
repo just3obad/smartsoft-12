07aa809183e50d0470a08cb50bcc23e51300aa11
elmenisy@gmail.com <elmenisy@gmail.com@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-04-20 17:07:07 +0000

    Update issue 36 Status: InProgress Added attributes thumbs up and down in story and migrates to db

--- trunk/nokia_ruby/db/schema.rb
+++ trunk/nokia_ruby/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120420131006) do
+ActiveRecord::Schema.define(:version => 20120420170236) do
 
   create_table "admins", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -19,11 +19,13 @@ ActiveRecord::Schema.define(:version => 20120420131006) do
   end
 
   create_table "comments", :force => true do |t|
-    t.string   "content",    :limit => 500
+    t.string   "content",     :limit => 500
     t.integer  "user_id"
     t.integer  "story_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "thumbs_up"
+    t.integer  "thumbs_down"
   end
 
   create_table "interests", :force => true do |t|
