6f6b910925c334d9129088abb1980969a12933f8
jailan.salah@gmail.com <jailan.salah@gmail.com@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-04-21 20:06:00 +0000

    Update issue 41

--- trunk/nokia_ruby/app/controllers/interests_controller.rb
+++ trunk/nokia_ruby/app/controllers/interests_controller.rb
@@ -2,5 +2,29 @@ class InterestsController < ApplicationController
 
   def show
     @interest = Interest.find(params[:id])
+    @feeds = @interest.feeds
+    @title = @interest.name
+    @image = @interest.image
+
   end
+
+
+
+  def new
+   @feed = Feed.new
+  @interest = Interest.new
+  @title = "Add interest"
+  end
+
+  def create
+   @interest = Interest.new(params[:interest])
+   if @interest.save
+     flash[:success] = "Your Interest was added Successfully"
+     redirect_to @interest
+
+   else
+     @title = "Add Interest"
+     render 'new'
+    end
+ end
 end
--- trunk/nokia_ruby/app/models/interest.rb
+++ trunk/nokia_ruby/app/models/interest.rb
@@ -1,3 +1,20 @@
 class Interest < ActiveRecord::Base
-  # attr_accessible :title, :body
+
+  attr_accessible :RSS, :description, :name, :image
+  
+ 
+  has_many :feeds, :dependent => :destroy
+
+  RSS_regex = /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,6}(:[0-9]{1,5})?(\/.*)?$/ix
+ 
+  validates :name, :presence => true,
+                   :uniqueness => true
+
+  validates :RSS,  :presence => true,
+            :format   => { :with => RSS_regex },
+            :uniqueness => { :case_sensitive => false }
+
+  validates :description,  :length   => { :maximum => 240 }
+
+  validates :image,  :format   => { :with => RSS_regex }
 end
