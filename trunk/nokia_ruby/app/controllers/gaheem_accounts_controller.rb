fdd2018ff2426a5575f5424b641bd7296e3d698a
just3obad@gmail.com <just3obad@gmail.com@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-04-21 22:49:34 +0000

    Update issue 29 : Added method update in gaheem

--- trunk/nokia_ruby/app/controllers/gaheem_accounts_controller.rb
+++ trunk/nokia_ruby/app/controllers/gaheem_accounts_controller.rb
@@ -1,2 +1,25 @@
 class GaheemAccountsController < ApplicationController
+
+  # GET /gaheem_accounts/1/edit
+  def edit
+    @gaheem_account = GaheemAccount.find(params[:id])
+  end
+
+  # PUT /gaheem_accounts/1
+  # PUT /gaheem_accounts/1.json
+  def update
+    @gaheem_account = GaheemAccount.find(params[:id])
+
+    respond_to do |format|
+      if @gaheem_account.update_attributes(params[:gaheem_account])
+        format.html { redirect_to @gaheem_account, notice: 'Gaheem account was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @gaheem_account.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
 end
