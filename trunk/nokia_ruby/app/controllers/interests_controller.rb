3bf37094aeb9379fc191d2f51baac7d7829f3e8e
christinesed <christinesed@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-04-21 15:49:38 +0000

    Removing some redundant files adding some new files

--- trunk/nokia_ruby/app/controllers/interests_controller.rb
+++ trunk/nokia_ruby/app/controllers/interests_controller.rb
@@ -1,2 +1,6 @@
 class InterestsController < ApplicationController
+
+  def show
+    @interest = Interest.find(params[:id])
+  end
 end
--- trunk/nokia_ruby/app/controllers/statistics_controller.rb
+++ trunk/nokia_ruby/app/controllers/statistics_controller.rb
@@ -1,4 +1,7 @@
 class StatisticsController < ApplicationController
+ def index
+   @interests= Interest.all
+ end
  def interests
    @id=params[:id]
  end
