afa8c3ef02c7c34d7cba8453fa5648fd59826f92
christinesed <christinesed@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-04-21 21:35:35 +0000

    Update issue 16 Finalizing graphs and adding more style

--- nokia_ruby/app/controllers/statistics_controller.rb
+++ nokia_ruby/app/controllers/statistics_controller.rb
@@ -1,4 +1,5 @@
 class StatisticsController < ApplicationController
+ layout 'statisticsPage'
  def index
    @interests= Interest.all
  end
@@ -10,5 +11,11 @@ class StatisticsController < ApplicationController
  end
  def users
    @id=params[:id]
+ end
+ def all_users
+ end
+ def all_stories
+ end
+ def all_interests
  end
 end
