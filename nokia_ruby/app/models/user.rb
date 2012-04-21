c298affc84863fffccc5bf13914192a8f7959f46
just3obad <just3obad@gmail.com>
2012-04-21 23:58:07 +0000

    Update issue 29 : Added some validations to Models user and gaheem_account

--- nokia_ruby/app/models/user.rb
+++ nokia_ruby/app/models/user.rb
@@ -1,13 +1,22 @@
 class User < ActiveRecord::Base
   # attr_accessible :title, :body
-  attr_accessible :name, :email, :deactivated
+  attr_accessible :name, :email, :deactivated, :first_name, :last_name, :date_of_birth
   
+  has_one :gaheem_account
+
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name, :presence => true,
   :length => { :maximum => 20 }
   validates :email, :presence => true,
   :format=> {:with => email_regex },
   :uniqueness => { :case_sensitive => false}
+  validates :first_name, :presence => true,
+  :length => { :maximum => 20 }
+  validates :last_name, :presence => true,
+  :length => { :maximum => 20 }
+  validates :date_of_birth, :presence => true,
+  
+  
   
   
   
