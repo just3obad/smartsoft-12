a519060dd35e80b1101a061c6b21be51a4e5829d
mouaz.alabsawi@gmail.com <mouaz.alabsawi@gmail.com@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-04-22 09:48:09 +0000

    Update issue 30: Adding date to story attributes

--- trunk/nokia_ruby/app/models/story.rb
+++ trunk/nokia_ruby/app/models/story.rb
@@ -2,12 +2,12 @@ class Story < ActiveRecord::Base
 #definition of some attributes:-
 # :rank==>hottness of a story, :category==> name of the related interest, :interest_id==>id of the related interest,
 # :type==> 1 (Article) 2 (Image) 3 (video)
-  attr_accessible :title, :body, :rank, :image, :category, 
+  attr_accessible :title, :date, :body, :rank, :image, :category, 
                   :content, :deleted, :hidden , :likes , :dislikes ,
 		  :flags, :type, :interest_id
   belongs_to :interests
 
-  def initialize(title, body)
+  def initialize(title, date, body)
     @title = title
     @body = body
   end
