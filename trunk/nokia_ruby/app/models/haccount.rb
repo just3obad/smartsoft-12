3eab1b549c2e5e5f750b73b04aff396e664e3db5
just3obad@gmail.com <just3obad@gmail.com@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-04-22 18:43:29 +0000

    Update issue 29 : Modified Haccount model

--- trunk/nokia_ruby/app/models/haccount.rb
+++ trunk/nokia_ruby/app/models/haccount.rb
@@ -2,4 +2,9 @@ class Haccount < ActiveRecord::Base
   attr_accessible :email, :password, :user_id
   
   belongs_to :user, class_name: "User"
+
+  validates :email, :presence => true,
+  :format=> {:with => email_regex }
+  :uniqueness => { :case_sensitive => false}
+  validates :user_id, uniqueness =>true
 end
