fc34134a9adedea2743f2d31e201c3f145244f8f
jailan.salah <jailan.salah@gmail.com>
2012-04-22 07:07:31 +0000

    Update issue 41
    Adding Documentation

--- nokia_ruby/app/models/interest.rb
+++ nokia_ruby/app/models/interest.rb
@@ -1,20 +1,30 @@
 class Interest < ActiveRecord::Base
-
+  
+#attributes  that can be modified automatically by outside users
   attr_accessible :RSS, :description, :name, :image
   
   has_many :stories
   has_many :feeds, :dependent => :destroy
 
-  RSS_regex = /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,6}(:[0-9]{1,5})?(\/.*)?$/ix
+ # RSS feed link has to be of the form "http://www.abc.com"
+RSS_regex = /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,6}(:[0-9]{1,5})?(\/.*)?$/ix
  
-  validates :name, :presence => true,
+  # name cannot be duplicated and has to be there .
+
+validates :name, :presence => true,
                    :uniqueness => true
 
+
+# we cannot create an Interest without an rss .. 
   validates :RSS,  :presence => true,
             :format   => { :with => RSS_regex },
-            :uniqueness => { :case_sensitive => false }
+ 
 
+
+#description can never exceed 240 characters .
   validates :description,  :length   => { :maximum => 240 }
 
+
+# the image will be entered using a URL link whci should also be of the form  "http://www. xxxx.jpg"
   validates :image,  :format   => { :with => RSS_regex }
 end
