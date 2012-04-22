7ae4a7a68c23e69a6b31e06abd30163f02d7b1a8
yahiaelgamal <yahiaelgamal@gmail.com>
2012-04-22 03:30:17 +0000

    Update issue 31
    Removed authentication_helper 
    Added TwitterAccount model and 
    Added route authentication/get_twitter_url
    Added generate_request_token in the twitter_account_controller
    removed a buffer from routes

---
+++ nokia_ruby/app/controllers/twitter_accounts_controller.rb
@@ -0,0 +1,26 @@
+require 'oauth'
+class TwitterAccountsController < ApplicationController
+
+  CONSUMER_TOKEN  = 'cuE56v2eK79gsxK1baasA'
+  CONSUMER_SECRET = 'vIJmDpiqDNpiau2YaiHH19fW1zj5DRXjI1VUbuM7lk' 
+
+  def generate_request_token
+    oa = OAuth::Consumer.new(CONSUMER_TOKEN, CONSUMER_SECRET,
+                             :site=> 'http://api.twitter.com',
+                             :request_endpoint => 'http://api.twitter.com',
+                             :sign_in => true)
+    req = oa.get_request_token
+    request_secret = req.secret
+    request_token  = req.token
+
+    url = "http://api.twitter.com/oauth/authorize?oauth_token=#{ request_token }"
+    #puts "######## " + url
+    session[:twitter_auth_req] = req
+    #format.json { [twitter_auth_url: url] } 
+    #render(:text => url)
+    # I am rendering because this will only be called from the mobile side
+    render text: url
+  end 
+
+
+end
