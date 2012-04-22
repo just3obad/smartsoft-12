0502e013d5324cbb07239bd2a88842158f07702e
kemooo17@gmail.com <kemooo17@gmail.com@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-04-22 20:10:16 +0000

    issue 10 : edited some methods and routes

--- trunk/nokia_ruby/app/controllers/likedislikes_controller.rb
+++ trunk/nokia_ruby/app/controllers/likedislikes_controller.rb
@@ -3,26 +3,30 @@ class LikedislikesController < ApplicationController
 respond_to :html,:json
 
 #this method is called when a user presses Like on the mobile App
-def like(user_id,story_id)  
-   thumped = Likedislike.where(:stroy_id => story_id, :user_id => user_id)
-     if thumped.nil?
-        Likedislike.create!(:user_id => user_id, :story_id => story_id , :action => 1)
-       @story = Story.find(story_id)
+def thumb
+  
+   @user_id = params[:uid]
+    @story_id = params[:sid]
+    @action = params[:act] 
+   thumped = Likedislike.where(:story_id => @story_id, :user_id => @user_id)
+     if thumped.empty?
+        Likedislike.create!(:user_id => @user_id, :story_id => @story_id , :action => @action)
+       @story = Story.find(@story_id)
        @story.likes = @story.likes+1
        @story.save   
      end
   
 end
 
-def dislike(user_id,story_id)
-    thumped = Likedislike.where(:stroy_id => story_id, :user_id => user_id)
-     if thumped.nil?
-       Likedislike.create!(:user_id => user_id, :story_id => story_id , :action => -1 )
-       @story = Story.find(story_id)
-       @story.likes = @story.dislikes+1
-       @story.save
-     end
-end
+#def dislike(user_id,story_id)
+ #   thumped = Likedislike.where(:stroy_id => story_id, :user_id => user_id)
+  #   if thumped.empty?
+   #    Likedislike.create!(:user_id => user_id, :story_id => story_id , :action => -1 )
+    #   @story = Story.find(story_id)
+     #  @story.likes = @story.dislikes+1
+      # @story.save
+     #end
+#end
 
 
 end
