83ce93cf6199689664fc4542f2d35f78d24ca501
jailan.salah <jailan.salah@gmail.com>
2012-04-22 20:12:43 +0000

    Update issue 42
    adding RSS feed

--- nokia_ruby/app/controllers/interests_controller.rb
+++ nokia_ruby/app/controllers/interests_controller.rb
@@ -1,40 +1,42 @@
+
 class InterestsController < ApplicationController
-  
+
   def show
-    @interest = Interest.find(params[:id]) #retrieving the interest from the database using the id
-    @feeds = @interest.feeds
+ # creating a new interest and returning it in a variable @interest used in the form in new.html.erb 
+    @interest = Interest.find(params[:id])
+
+    @feed = Feed.find_by_interest_id(params[:id])
+
+    if @feed == nil 
+        @feed = Feed.new
+    end
     @title = @interest.name
     @image = @interest.image
-  end
 
-  def index
-   @interests = Interest.all
   end
 
 
   def new
-
   @feed = Feed.new
-  @interest = Interest.new # creating a new interest and returning it in a variable @interest used in the form in new.html.erb 
+  @interest = Interest.new
   @title = "Add interest"
-
-
   end
-
   #" CREATE " is a method to create a new interest using if-else statements, which allows us to handle the cases of failure and success separately based on the value of @interest.save, if saving succeeds ( according to validations ) then we redirect to the main page of the interest 
  # otherwise , we render once more  the "New" page  
-
-#Render 'new' , evaluates the contents in the error_messages partial contents, and insert the results into the view.
-def create 
+  def create
    @interest = Interest.new(params[:interest])
    if @interest.save
      flash[:success] = "Your Interest was added Successfully"
 # here we check if flash returned a sucess then we display the success message 
+
      redirect_to @interest
 
    else
      @title = "Add Interest"
      render 'new'
+#Render 'new' , evaluates the contents in the error_messages partial contents, and insert the results into the view.
+
+
     end
  end
 end
--- nokia_ruby/app/models/feed.rb
+++ nokia_ruby/app/models/feed.rb
@@ -1,5 +1,14 @@
 class Feed < ActiveRecord::Base
-  attr_accessible :interest_id, :link
-  
-  belongs_to :interest, class_name: "Interest"
+  attr_accessible  :link, :interest_id
+  belongs_to :interest
+  LINK_regex = /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,6}(:[0-9]{1,5})?(\/.*)?$/ix
+
+  validates :link,  :presence => true,
+            :format   => { :with => LINK_regex },
+            :uniqueness => { :case_sensitive => false }
+
+
+  validates :interest_id, :presence => true
+
+
 end
