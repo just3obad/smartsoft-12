9a270c2ffb3bdd6f30e38d64616566e3addb4c5b
kerolos.bebawy <kerolos.bebawy@gmail.com>
2012-04-22 05:49:48 +0000

    Added method addVEntry to the model verificationCode

--- nokia_ruby/app/models/verification_code.rb
+++ nokia_ruby/app/models/verification_code.rb
@@ -8,4 +8,13 @@ class VerificationCode < ActiveRecord::Base
   
   validates :user_id, presence => true,
   :uniqueness => true
+
+  def 
+
+ # lets a user share a story given its id
+    def addVEntry
+      Verification_Code.create :code=>( (0..9).to_a + ('a'..'z').to_a).shuffle[0..3].join, :user_id=>self.id
+    end 
+  end
+
 end
