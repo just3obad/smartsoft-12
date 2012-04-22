a6f85d95d25196ec6e7cbfddb8dba7b1f2de139d
jailan.salah <jailan.salah@gmail.com>
2012-04-22 21:42:18 +0000

    Update issue 42

--- nokia_ruby/app/controllers/interests_controller.rb
+++ nokia_ruby/app/controllers/interests_controller.rb
@@ -1,4 +1,3 @@
-
 class InterestsController < ApplicationController
   
   def index
@@ -6,7 +5,6 @@ class InterestsController < ApplicationController
   end
 
   def show
- # creating a new interest and returning it in a variable @interest used in the form in new.html.erb 
     @interest = Interest.find(params[:id])
 
     @feed = Feed.find_by_interest_id(params[:id])
@@ -25,22 +23,25 @@ class InterestsController < ApplicationController
   @interest = Interest.new
   @title = "Add interest"
   end
-  #" CREATE " is a method to create a new interest using if-else statements, which allows us to handle the cases of failure and success separately based on the value of @interest.save, if saving succeeds ( according to validations ) then we redirect to the main page of the interest 
- # otherwise , we render once more  the "New" page  
+
   def create
    @interest = Interest.new(params[:interest])
    if @interest.save
      flash[:success] = "Your Interest was added Successfully"
-# here we check if flash returned a sucess then we display the success message 
-
+ # here we check if flash returned a sucess then we display the success message
      redirect_to @interest
 
    else
      @title = "Add Interest"
      render 'new'
 #Render 'new' , evaluates the contents in the error_messages partial contents, and insert the results into the view.
-
-
     end
  end
 end
+
+
+ 
+
+
+
+
