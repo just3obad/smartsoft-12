ad76d411d5a996a850a75715e01b73fedd99b271
yahiaelgamal@gmail.com <yahiaelgamal@gmail.com@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-04-22 18:05:07 +0000

    Update issue
    TwitterAccount#get_feeds works. returns a list of story (no content as it's not in the database yet)

--- trunk/nokia_ruby/app/models/twitter_account.rb
+++ trunk/nokia_ruby/app/models/twitter_account.rb
@@ -14,32 +14,45 @@ class TwitterAccount < ActiveRecord::Base
   validates :user_id, uniqueness: true
 
 
+  def config_twitter
+    Twitter.configure do |config|
+      config.consumer_key         = CONSUMER_TOKEN
+      config.consumer_secret      = CONSUMER_SECRET
+      config.oauth_token          = self.auth_token
+      config.oauth_token_secret   = self.auth_secret
+    end
+    puts 'twitter configured'
+  end 
+
   def get_feed(count=10)
-    Twitter.home_timeline(:count => count)
+    self.config_twitter
+    puts "getting the feed"
+    feed = Twitter.home_timeline(:count => count)
+    puts feed
     stories = Array.new
     feed.each do |tweet|
-      temp = tweet_to_story(tweet) 
+      temp = TwitterAccount.tweet_to_story(tweet) 
       stories.push(temp) 
     end 
+    stories
   end 
-  
 
   # This representation was asked by C1 lead. 
-  def self.tweet_to_story (tweet)
+  def self.tweet_to_story(tweet)
     #story = [author: tweet['user']['name'], text: tweet['text']]
     story = Story.new
+    story.instance_variables
     story.title = tweet['user']['name']
     story.body = tweet['text']
-    story.content = tweet['text']
+    #story.content = tweet['text']
     story.category = 'twitter'
+    return story 
   end
 
-  
 
 #  def tweet(tweet)
 #    client = Twitter::Client.new
 #    client.update(tweet)
 #  end 
 #
-
 end
