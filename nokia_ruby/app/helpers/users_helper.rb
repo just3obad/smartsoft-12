de0860f192263ba8d71624b69129b752fd359b08
mohamed.shafei90 <mohamed.shafei90@gmail.com>
2012-04-22 19:53:43 +0000

    Update issue 17
    replace spam with flag and fix some errors

--- nokia_ruby/app/helpers/users_helper.rb
+++ nokia_ruby/app/helpers/users_helper.rb
@@ -109,21 +109,21 @@ last_updated = User.find(user_id).updated_at_before_type_cast.to_date
 deactivated = false
   #deactivated = User.find(user_id).deactivated_before_type_cast     remove the hash when deactivated is defined in the database
 #1) If the user registered and was deactivated within the last 30 days
-  if deleted && user_reg_date > 30.days.ago.to_date && last_updated > 30.days.ago.to_date
+  if deactivated && user_reg_date > 30.days.ago.to_date && last_updated > 30.days.ago.to_date
     likes_by_day = Likedislike.where(:created_at => user_reg_date..last_updated, :user_id => user_id, :action => 1).group("date(created_at)").select("created_at, count(user_id) as noOfLikesPerDay")
     (user_reg_date.to_date..last_updated.to_date).map do |date|
       like = likes_by_day.detect { |like| like.created_at.to_date == date }
       like && like.noOfLikesPerDay.to_i || 0
     end.inspect
   #2) If the user registered before the last 30 days and was dactivated within the last 30 days
-  elsif deleted && user_reg_date <= 30.days.ago.to_date && last_updated > 30.days.ago.to_date
+  elsif deactivated && user_reg_date <= 30.days.ago.to_date && last_updated > 30.days.ago.to_date
     likes_by_day = Likedislike.where(:created_at => 30.days.ago..last_updated, :user_id => user_id, :action => 1).group("date(created_at)").select("created_at, count(user_id) as noOfLikesPerDay")
     (30.days.ago.to_date..last_updated.to_date).map do |date|
       like = likes_by_day.detect { |like| like.created_at.to_date == date }
       like && like.noOfLikesPerDay.to_i || 0
     end.inspect
   #3) If the user registered and deactivated before the last 30 days
-  elsif deleted && user_reg_date <= 30.days.ago.to_date && last_updated <= 30.days.ago.to_date
+  elsif deactivated && user_reg_date <= 30.days.ago.to_date && last_updated <= 30.days.ago.to_date
     likes_by_day = []
   #4) if the user registered before 30 days and wasn't deactivated
   elsif user_reg_date <= 30.days.ago.to_date
@@ -150,21 +150,21 @@ last_updated = User.find(user_id).updated_at_before_type_cast.to_date
 deactivated = false
   #deactivated = User.find(user_id).deactivated_before_type_cast     remove the hash when deactivated is defined in the database
 #1) If the user registered and was deactivated within the last 30 days
-  if deleted && user_reg_date > 30.days.ago.to_date && last_updated > 30.days.ago.to_date
+  if deactivated && user_reg_date > 30.days.ago.to_date && last_updated > 30.days.ago.to_date
     dislikes_by_day = Likedislike.where(:created_at => user_reg_date..last_updated, :user_id => user_id, :action => -1).group("date(created_at)").select("created_at, count(user_id) as noOfDislikesPerDay")
     (user_reg_date.to_date..last_updated.to_date).map do |date|
        dislike = dislikes_by_day.detect { |dislike| dislike.created_at.to_date == date }
        dislike && like.noOfDislikesPerDay.to_i || 0
     end.inspect
   #2) If the user registered before the last 30 days and was dactivated within the last 30 days
-  elsif deleted && user_reg_date <= 30.days.ago.to_date && last_updated > 30.days.ago.to_date
+  elsif deactivated && user_reg_date <= 30.days.ago.to_date && last_updated > 30.days.ago.to_date
     dislikes_by_day = Likedislike.where(:created_at => 30.days.ago..last_updated, :user_id => user_id, :action => -1).group("date(created_at)").select("created_at, count(user_id) as noOfDislikesPerDay")
     (30.days.ago.to_date..last_updated.to_date).map do |date|
       dislike = dislikes_by_day.detect { |dislike| dislike.created_at.to_date == date }
       dislike && dislike.noOfDislikesPerDay.to_i || 0
     end.inspect
   #3) If the user registered and deactivated before the last 30 days
-  elsif deleted && user_reg_date <= 30.days.ago.to_date && last_updated <= 30.days.ago.to_date
+  elsif deactivated && user_reg_date <= 30.days.ago.to_date && last_updated <= 30.days.ago.to_date
     dislikes_by_day = []
   #4) if the user registered before 30 days and wasn't deactivated
   elsif user_reg_date <= 30.days.ago.to_date
@@ -186,26 +186,26 @@ end
 #This method gets the number of Comments by this user in the last 30 days	  		
  def get_no_of_comments_user(user_id)
 
-user_reg_date = Story.find(user_id).created_at_before_type_cast.to_date
-last_updated = Story.find(user_id).updated_at_before_type_cast.to_date
+user_reg_date = User.find(user_id).created_at_before_type_cast.to_date
+last_updated = User.find(user_id).updated_at_before_type_cast.to_date
 deactivated = false
   #deactivated = User.find(user_id).deactivated_before_type_cast     remove the hash when deactivated is defined in the database
 #1) If the user registered and was deactivated within the last 30 days
-  if deleted && user_reg_date > 30.days.ago.to_date && last_updated > 30.days.ago.to_date
+  if deactivated && user_reg_date > 30.days.ago.to_date && last_updated > 30.days.ago.to_date
     comments_by_day = Comment.where(:created_at => user_reg_date..last_updated, :user_id => user_id).group("date(created_at)").select("created_at, count(user_id) as noOfCommentsPerDay")
     (user_reg_date.to_date..last_updated.to_date).map do |date|
       comment = comments_by_day.detect { |comment| comment.created_at.to_date == date }
       comment && comment.noOfcommentsPerDay.to_i || 0
     end.inspect
   #2) If the user registered before the last 30 days and was dactivated within the last 30 days
-  elsif deleted && user_reg_date <= 30.days.ago.to_date && last_updated > 30.days.ago.to_date
+  elsif deactivated && user_reg_date <= 30.days.ago.to_date && last_updated > 30.days.ago.to_date
     comments_by_day = Comment.where(:created_at => 30.days.ago..last_updated, :user_id => user_id).group("date(created_at)").select("created_at, count(user_id) as noOfCommentsPerDay")
     (30.days.ago.to_date..last_updated.to_date).map do |date|
       comment = comments_by_day.detect { |comment| comment.created_at.to_date == date }
       comment && comment.noOfCommentsPerDay.to_i || 0
     end.inspect
   #3) If the user registered and deactivated before the last 30 days
-  elsif deleted && user_reg_date <= 30.days.ago.to_date && last_updated <= 30.days.ago.to_date
+  elsif deactivated && user_reg_date <= 30.days.ago.to_date && last_updated <= 30.days.ago.to_date
     comments_by_day = []
   #4) if the user registered before 30 days and wasn't deactivated
   elsif user_reg_date <= 30.days.ago.to_date
@@ -224,41 +224,41 @@ deactivated = false
   end
 	end
 	
-#This method gets the number of Spams by this user in the last 30 days	  		
- def get_no_of_spams_user(user_id)
-user_reg_date = Story.find(user_id).created_at_before_type_cast.to_date
-last_updated = Story.find(user_id).updated_at_before_type_cast.to_date
+#This method gets the number of flags by this user in the last 30 days	  		
+ def get_no_of_flags_user(user_id)
+user_reg_date = User.find(user_id).created_at_before_type_cast.to_date
+last_updated = User.find(user_id).updated_at_before_type_cast.to_date
 deactivated = User.find(user_id).deactivated_before_type_cast
 #1) If the user registered and was deactivated within the last 30 days
-  if deleted && user_reg_date > 30.days.ago.to_date && last_updated > 30.days.ago.to_date
-    spams_by_day = Spam.where(:created_at => user_reg_date..last_updated, :user_id => user_id).group("date(created_at)").select("created_at, count(user_id) as noOfSpamsPerDay")
+  if deactivated && user_reg_date > 30.days.ago.to_date && last_updated > 30.days.ago.to_date
+    flags_by_day = Flag.where(:created_at => user_reg_date..last_updated, :user_id => user_id).group("date(created_at)").select("created_at, count(user_id) as noOfFlagsPerDay")
     (user_reg_date.to_date..last_updated.to_date).map do |date|
-      spam = spams_by_day.detect { |spam| spam.created_at.to_date == date }
-      spam && spam.noOfSpamsPerDay.to_i || 0
+      flag = flags_by_day.detect { |flag| flag.created_at.to_date == date }
+      flag && flag.noOfFlagsPerDay.to_i || 0
     end.inspect
   #2) If the user registered before the last 30 days and was dactivated within the last 30 days
-  elsif deleted && user_reg_date <= 30.days.ago.to_date && last_updated > 30.days.ago.to_date
-    spams_by_day = Spam.where(:created_at => 30.days.ago..last_updated, :user_id => user_id).group("date(created_at)").select("created_at, count(user_id) as noOfSpamsPerDay")
+  elsif deactivated && user_reg_date <= 30.days.ago.to_date && last_updated > 30.days.ago.to_date
+    flags_by_day = Flag.where(:created_at => 30.days.ago..last_updated, :user_id => user_id).group("date(created_at)").select("created_at, count(user_id) as noOfFlagsPerDay")
     (30.days.ago.to_date..last_updated.to_date).map do |date|
-      spam = spams_by_day.detect { |spam| spam.created_at.to_date == date }
-      spam && spam.noOfSpamsPerDay.to_i || 0
+      flag = flags_by_day.detect { |flag| flag.created_at.to_date == date }
+      flag && flag.noOfFlagsPerDay.to_i || 0
     end.inspect
   #3) If the user registered and deactivated before the last 30 days
-  elsif deleted && user_reg_date <= 30.days.ago.to_date && last_updated <= 30.days.ago.to_date
-    spams_by_day = []
+  elsif deactivated && user_reg_date <= 30.days.ago.to_date && last_updated <= 30.days.ago.to_date
+    flags_by_day = []
   #4) if the user registered before 30 days and wasn't deactivated
   elsif user_reg_date <= 30.days.ago.to_date
-    spams_by_day = Spam.where(:created_at => 30.days.ago..Time.zone.now.end_of_day, :user_id => user_id).group("date(created_at)").select("created_at, count(user_id) as noOfSpamsPerDay")
+    flags_by_day = Flag.where(:created_at => 30.days.ago..Time.zone.now.end_of_day, :user_id => user_id).group("date(created_at)").select("created_at, count(user_id) as noOfFlagsPerDay")
     (30.days.ago.to_date..Time.zone.now.to_date).map do |date|
-      spam = spams_by_day.detect { |spam| spam.created_at.to_date == date }
-      spam && spam.noOfSpamsPerDay.to_i || 0
+      flag = flags_by_day.detect { |flag| flag.created_at.to_date == date }
+      flag && flag.noOfFlagsPerDay.to_i || 0
     end.inspect
   #5) if the user registered within 30 days and wasn't deactivated
   else
-    spams_by_day = Spam.where(:created_at => user_reg_date.beginning_of_day..Time.zone.now.end_of_day, :user_id => user_id).group("date(created_at)").select("created_at, count(user_id) as noOfSpamsPerDay")
+    flags_by_day = Flag.where(:created_at => user_reg_date.beginning_of_day..Time.zone.now.end_of_day, :user_id => user_id).group("date(created_at)").select("created_at, count(user_id) as noOfFlagsPerDay")
     (user_reg_date.to_date..Time.zone.now.to_date).map do |date|
-      spam = spams_by_day.detect { |spam| spam.created_at.to_date == date }
-      spam && spam.noOfSpamsPerDay.to_i || 0
+      flag = flags_by_day.detect { |flag| flag.created_at.to_date == date }
+      flag && flag.noOfFlagsPerDay.to_i || 0
     end.inspect
   end
 end
