27261b356fe0d98115ff27fb0ef082b92cfae3d6
kemooo17@gmail.com <kemooo17@gmail.com@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-04-22 17:00:56 +0000

    Dropped user interests

--- trunk/nokia_ruby/db/schema.rb
+++ trunk/nokia_ruby/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120422155026) do
+ActiveRecord::Schema.define(:version => 20120422165915) do
 
   create_table "admins", :force => true do |t|
     t.datetime "created_at", :null => false
