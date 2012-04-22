06cec7e4df1d7a1ef8d400003075dbf01c1e53f8
yahiaelgamal@gmail.com <yahiaelgamal@gmail.com@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-04-22 16:40:03 +0000

    Update issue 31
    Request storing and url delivery works and tested. 
    Acess_Token generation Not done yet
    validate uniqueness added

--- trunk/nokia_ruby/app/models/twitter_request.rb
+++ trunk/nokia_ruby/app/models/twitter_request.rb
@@ -2,4 +2,9 @@ class TwitterRequest < ActiveRecord::Base
   # attr_accessible :title, :body
   attr_accessible :request_token, :request_secret, :user_id
   belongs_to :user 
+
+  validates :request_secret, presence: true
+  validates :request_token, presence: true
+  validates :user_id, presence: true
+  validates :user_id, uniqueness: true
 end
--- trunk/nokia_ruby/app/models/user.rb
+++ trunk/nokia_ruby/app/models/user.rb
@@ -13,7 +13,7 @@ class User < ActiveRecord::Base
   :length => { :maximum => 20 }
   validates :email, :presence => true,
   :format=> {:with => email_regex }
- :uniqueness => { :case_sensitive => false}
+ #:uniqueness => { :case_sensitive => false}
   validates :first_name, :presence => true,
   :length => { :maximum => 20 }
   validates :last_name, :presence => true,     #un-comment after db handling
