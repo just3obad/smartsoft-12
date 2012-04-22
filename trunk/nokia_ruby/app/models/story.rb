95a4b2976d69cfbb74b60b1bbb8db541cbbb4a9f
gosgosakila@gmail.com <gosgosakila@gmail.com@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-04-22 21:53:55 +0000

    Update issue 27 Status Done Added the basic UI for the story page using simple bootstrap

--- trunk/nokia_ruby/app/models/story.rb
+++ trunk/nokia_ruby/app/models/story.rb
@@ -1,6 +1,6 @@
 class Story < ActiveRecord::Base
 #definition of some attributes:-
-# :rank==>hottness of a story, :category==> name of the related interest, :interest_id==>id of the related interest,
+# :rank==>hottness of a story, :interest_id==>id of the related interest,
 # :type==> 1 (Article) 2 (Image) 3 (video)
   attr_accessible :title, :date, :body, :rank,
                  :content, :deleted, :hidden , :interest_id
