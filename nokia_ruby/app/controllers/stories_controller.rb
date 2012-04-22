b06bc89537728cc37cb73f9644b6374e5f0b46ee
gosgosakila <gosgosakila@gmail.com>
2012-04-22 02:55:04 +0000

    Update issue 27 Status Inprogress Modifying the story and interest models by adding the relation one_to_many between them

--- nokia_ruby/app/controllers/stories_controller.rb
+++ nokia_ruby/app/controllers/stories_controller.rb
@@ -1,5 +1,6 @@
 class StoriesController < ApplicationController
-respond_to :html,:json
+  respond_to :html,:json
+  
   def show
    @comments = Comment.find_all_by_story_id(params[:id]) # get comments of this story
    @story = Story.find(params[:id])
