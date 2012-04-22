2b83e41097b64df0b44eb701e5cacdf671edcfae
yahiaelgamal <yahiaelgamal@gmail.com>
2012-04-22 18:05:07 +0000

    Update issue
    TwitterAccount#get_feeds works. returns a list of story (no content as it's not in the database yet)

--- nokia_ruby/app/controllers/twitter_accounts_controller.rb
+++ nokia_ruby/app/controllers/twitter_accounts_controller.rb
@@ -1,14 +1,5 @@
 require 'twitter'
 class TwitterAccountsController < ApplicationController 
-  def config_twitter
-    @t_account = TwitterAccount.find_by_user_id(params[:u_id]) 
-    Twitter.configure do |config|
-      config.consumer_key         = CONSUMER_TOKEN
-      config.consumer_secret      = CONSUMER_SECRET
-      config.oauth_token          = t_account.auth_token
-      config.oauth_token_secret   = t_account.auth_secret
-    end
-  end 
 
 
   def remove_twitter_account
