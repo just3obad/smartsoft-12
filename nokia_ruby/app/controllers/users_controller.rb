87f4e5a92dcc78544f07a343125cb6ac91617e03
rana.helal92 <rana.helal92@gmail.com>
2012-04-22 22:33:29 +0000

    Adding my methods again please do not delete them

--- nokia_ruby/app/controllers/users_controller.rb
+++ nokia_ruby/app/controllers/users_controller.rb
@@ -60,29 +60,42 @@ class UsersController < ApplicationController
     respond_with(@users = User.all)
   end
  #this method takes an array of interests and blocks these interests   from the user's feed by changing the is_blocked attribute to true. The method depends on another method that gets the list of stories under these interests.
-  
-  #@story_list = getStories(self.interests) 
-  def block_interest(story_list)
-    0.upto(story_list.length) do |i|
-       story_list[i].is_blocked = true
+   
+  def block_interest
+ @user_interests = User.find(:first,:conditions => ["id = ?",params[:id]],:select => "interests")
+    #@story_list = getStories(@user_interests)
+    0.upto(@story_list.length) do |i|
+       @story_list[i].is_blocked = true
     end
   end
 
   #this method takes a story as input and blocks it by setting the is_blocked attribute to true.
 
-  #story = Story.find(params[:id])
-  def block_story(story)
-    story.is_blocked = true
+  
+  def block_story
+   @story = Story.find(params[:id])
+    @story.is_blocked = true
   end
 
   #this method takes list of stories belonging to a friend as input and blocks feeds from this friend by setting is_blocked attribute to true. The method depends on another method that gets the stories belonging to a friend.
 
-  #@friend_stories = getFriendsStories(self.friends)
-  def block_friend_feed(friend_stories)
-    0.upto(friend_stories.length) do |i|
-       friend_stories[i].is_blocked = true
+  def block_friend_feed
+    @friends = User.find(:first,:conditions => ["id = ?",params[:id]],:select => "friends")
+    #@friend_stories = getFriendsStories(@friends)
+    0.upto(@friend_stories.length) do |i|
+       @friend_stories[i].is_blocked = true
     end
   end
+
+#this method gets the stories of a friend through method getFriendsStories and converts them to a json file.
+
+def friends_feed
+ @friends = User.find(:first,:conditions => ["id = ?",params[:id]],:select => "friends")
+#@friend_stories = getFriendsStories(@friends)
+respond_to do |format|
+       format.json { render json: @friend_stories }
+end
+end
 
 #this method Passes a list of Interests according to the user_id to getStories method which should return list of stories according to these Interests and it converts it to a json file.
 
