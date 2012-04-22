a11a9a2df4b7ded542c893ae805bb2f0ca0c75f7
mina.adel.fahmy <mina.adel.fahmy@gmail.com>
2012-04-22 13:15:17 +0000

    Update issue 51 Added ability to create HAccounts (gaheem_accounts) through JSON Status : InProgress

--- nokia_ruby/app/controllers/gaheem_accounts_controller.rb
+++ nokia_ruby/app/controllers/gaheem_accounts_controller.rb
@@ -1,5 +1,5 @@
 class GaheemAccountsController < ApplicationController
-
+  respond_to :html, :json
   # GET /gaheem_accounts/1/edit
   def edit
     @gaheem_account = GaheemAccount.find(params[:id])
@@ -7,11 +7,18 @@ class GaheemAccountsController < ApplicationController
 
   # PUT /gaheem_accounts/1
   # PUT /gaheem_accounts/1.json
-  
-  def new
-    @gaheem_account = GaheemAccount.new(params[:email, :password])
-  end
 
+  def create
+    @gaheem_account = GaheemAccount.new(params[:gaheem_account])
+
+    respond_to do |format|
+      if @gaheem_account.save
+        format.json {render json: @gaheem_account, status: :created, location: @gaheem_account}
+      else
+        format.json {render json: @gaheem_account.errors, status: :unprocessable_entity }
+    end
+  end
+  end  
   def update
     @gaheem_account = GaheemAccount.find(params[:id])
 
@@ -26,5 +33,8 @@ class GaheemAccountsController < ApplicationController
     end
   end
 
-
+ def index
+    @gaheem_accounts = GaheemAccount.all
+    respond_with(@gaheem_accounts)
+ end
 end
