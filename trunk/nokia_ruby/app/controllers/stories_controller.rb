bbf267c4c5ba08c3e4b493eda36e8797c22b0e10
elmenisy@gmail.com <elmenisy@gmail.com@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-04-23 16:25:54 +0000

    Update issue 35RoR Modified comments json template

--- trunk/nokia_ruby/app/controllers/stories_controller.rb
+++ trunk/nokia_ruby/app/controllers/stories_controller.rb
@@ -9,7 +9,7 @@ class StoriesController < ApplicationController
   end
     # show comments of a certain story
     def show_comments
-    respond_with (@comments = Comment.find_all_by_story_id(params[:id]))
+    @comments = Comment.find_all_by_story_id(params[:id])
     end
 
     # create a new comment for a certain story
