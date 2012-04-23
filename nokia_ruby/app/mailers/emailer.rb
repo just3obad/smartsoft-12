1e76a9a089ac0e205d0d564c93066d119654e43d
mina.adel.fahmy <mina.adel.fahmy@gmail.com>
2012-04-23 18:25:22 +0000

    Update issue 48 send email with resetting password Status : InProgress

--- nokia_ruby/app/mailers/emailer.rb
+++ nokia_ruby/app/mailers/emailer.rb
@@ -2,11 +2,17 @@ class Emailer < ActionMailer::Base
   default from: "kerolos.bebawy@gmail.com"
 
   def registration_confirmation(account)
-  @recipients  account.email
-  @from        "kerolos.bebawy@gmail.com"
-  @subject     "Thank you for Registering, Please Verify your account"
-  @body        (:account => account,:code ==>VerificationCode.find_by_user_id(user.id).code)
+  recipients  account.email
+  from        "kerolos.bebawy@gmail.com"
+  subject     "Thank you for Registering, Please Verify your account"
+  body        (:account => account,:code ==>VerificationCode.find_by_user_id(user.id).code)
   end
 
+  def password_reset(account)
+  recipients account.email
+  from       "mina.adel.fahmy@gmail.com"
+  subject    "Your password reset email"
+  body       (:account => account,:password ==>Haccount.new_pass(user.id))
+  end
 
 end
