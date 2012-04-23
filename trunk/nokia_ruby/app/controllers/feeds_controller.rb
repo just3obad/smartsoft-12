2974d3c623df8063bb07a869248b6f4dd6a3a73a
jailan.salah@gmail.com <jailan.salah@gmail.com@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-04-23 21:21:26 +0000

    Update issue 42
    controller  documentation

--- trunk/nokia_ruby/app/controllers/feeds_controller.rb
+++ trunk/nokia_ruby/app/controllers/feeds_controller.rb
@@ -9,12 +9,12 @@ end
 
   def create
     
-    @feed= Feed.new(params[:feed])
+    @feed= Feed.new(params[:feed])#retrieving the feed from the database using the table feed parameters
     
-    if @feed.save
+    if @feed.save # if we succeded to add a new RSS feed then a flash message in a green box will appear with a message indicating that , we will redirect to the interests page again .
       flash[:success] = "RSS added Successfully !"
       redirect_to :controller => 'interests', :action => 'show', :id => @feed.interest_id
-    else
+    else#if saving faild a flash message box will appear with the errors
      @title = "Add feed"
      render 'new'
     end
