196d7b5e1b3f987d0f9652e269362543c497437d
diab977@gmail.com <diab977@gmail.com@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-04-23 13:49:44 +0000

    Update issue 20

--- trunk/nokia_ruby/app/helpers/users_helper.rb
+++ trunk/nokia_ruby/app/helpers/users_helper.rb
@@ -23,9 +23,9 @@ end
  if first_user.nil?
   registered_per_day = []
  else
- first_user_create_date = User.first.created_at_before_type_cast.to_date #to get the creation date of the first user
+ first_user_create_date = User.first.created_at #to get the creation date of the first user
  #case 1 if the creation day was within last 30 days
- if first_user_create_date > 30.days.ago.to_date
+ if first_user_create_date >= 30.days.ago.to_date
 
  registered_per_day = User.where(:created_at => first_user_create_date.beginning_of_day..Time.zone.now.end_of_day).group("date(created_at)").select("created_at , count(id) as regs_day") #to get all the users who registered within first user creation until the current date and group by the date of creation
  (first_user_create_date.to_date..Time.zone.now.to_date).map do |date|
@@ -35,7 +35,7 @@ end
  #case 2 if the creation date was before 30 days ago
   end.inspect
  else
- registered_per_day = User.where(:created_at => 30.days.ago.to_date..Time.zone.now.end_of_day).group("date(created_at)").select("created_at , count(id) as regs_day") #to get all the users who registered within 30 days ago until the current date and group by the date of creation
+ registered_per_day = User.where(:created_at => 30.days.ago.beginning_of_day..Time.zone.now.end_of_day).group("date(created_at)").select("created_at , count(id) as regs_day") #to get all the users who registered within 30 days ago until the current date and group by the date of creation
  (30.days.ago.to_date..Time.zone.now.to_date).map do |date|
   reg = registered_per_day.detect { |reg| reg.created_at.to_date == date}
   reg && reg.regs_day.to_i || 0
@@ -50,10 +50,10 @@ end
  if first_user.nil?
   logged_per_day = []
  else
- first_user_log_date = UserLogIn.first.created_at_before_type_cast.to_date #to get the creation date of the first user
+ first_user_log_date = UserLogIn.first.created_at #to get the creation date of the first user
  #case 1 if the first login was within last 30 days
- if first_user_log_date > 30.days.ago.to_date
- logged_per_day = UserLogIn.where(:created_at => first_user_log_date..Time.zone.now.end_of_day).group("date(created_at)").select("created_at , count(distinct(id)) as logs_day") #to get all the users who logged in within first user login until the current date and group by the date of creation
+ if first_user_log_date >= 30.days.ago.to_date
+ logged_per_day = UserLogIn.where(:created_at => first_user_log_date.beginning_of_day..Time.zone.now.end_of_day).group("date(created_at)").select("created_at , count(distinct(id)) as logs_day") #to get all the users who logged in within first user login until the current date and group by the date of creation
  (first_user_log_date.to_date..Time.zone.now.to_date).map do |date|
   log = logged_per_day.detect { |log| log.created_at.to_date == date}
   log && log.logs_day.to_i || 0
@@ -61,7 +61,7 @@ end
  #case 2 if the first user login was before 30 days ago
   end.inspect
  else
- logged_per_day = UserLogIn.where(:created_at => 30.days.ago.to_date..Time.zone.now.end_of_day).group("date(created_at)").select("created_at , count(distinct(id)) as logs_day") #to get all the users who logged in within 30 days ago until the current date and group by the date of creation
+ logged_per_day = UserLogIn.where(:created_at => 30.days.ago.beginning_of_date..Time.zone.now.end_of_day).group("date(created_at)").select("created_at , count(distinct(id)) as logs_day") #to get all the users who logged in within 30 days ago until the current date and group by the date of creation
  (30.days.ago.to_date..Time.zone.now.to_date).map do |date|
   log = logged_per_day.detect { |log| log.created_at.to_date == date}
   log && log.logs_day.to_i || 0
@@ -74,7 +74,7 @@ end
  def get_all_users_start_date
  first_user_create_date = User.first.created_at_before_type_cast.to_date #to get the creation date of the first user
  #case 1 if the creation day was within last 30 days
- if first_user_create_date > 30.days.ago.to_date
+ if first_user_create_date >= 30.days.ago.to_date
  date = Time.zone.now.to_date - first_user_create_date #to set the start date of the statistics to the creation of the first User
  #case 2 if the creation date was before 30 days ago
  else
