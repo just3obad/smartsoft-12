ed56e793ab3644adce20d8f6cb3ba9b170c06ac4
jailan.salah <jailan.salah@gmail.com>
2012-04-23 21:22:11 +0000

    Update issue 42
    controller  documentation

--- nokia_ruby/app/controllers/interests_controller.rb
+++ nokia_ruby/app/controllers/interests_controller.rb
@@ -5,11 +5,11 @@ class InterestsController < ApplicationController
   end
 
   def show
-    @interest = Interest.find(params[:id])
+    @interest = Interest.find(params[:id])#retrieving the interest from the database using the id
 
-    @feed = Feed.find_by_interest_id(params[:id])
+    @feed = Feed.find_by_interest_id(params[:id])#retrieving the feeds for a certain interest in the database using the id of the interest
 
-    if @feed == nil 
+    if @feed == nil #if the list of RSS feeds if a certain interest is empty we will create a new one
        @feed = Feed.new
     end
     @title = @interest.name
@@ -19,11 +19,12 @@ class InterestsController < ApplicationController
 
 
   def new
-  @feed = Feed.new
+  @feed = Feed.new # creating a new interest and returning it in a variable @interest used in the form in new.html.erb 
   @interest = Interest.new
   @title = "Add interest"
   end
-
+  #" CREATE " is a method to create a new interest using if-else statements, which allows us to handle the cases of failure and success separately based on the value of @interest.save, if saving succeeds ( according to validations ) then we redirect to the main page of the interest 
+ # otherwise , we render once more  the "New" page 
   def create
    @interest = Interest.new(params[:interest])
    if @interest.save
