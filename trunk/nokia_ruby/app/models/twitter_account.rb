521da5dba9ab40724023e523069d884558e649cf
yahiaelgamal@gmail.com <yahiaelgamal@gmail.com@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-04-24 02:30:50 +0000

    Update issue 31 Labels: Doc-Ready
    Added documentation to 
    
    twitter_accounts_controller.rb
    twitter_requestss_controller.rb
    twitter_account.rb
    twitter_request.rb

--- trunk/nokia_ruby/app/models/twitter_account.rb
+++ trunk/nokia_ruby/app/models/twitter_account.rb
@@ -1,10 +1,17 @@
 require 'twitter'
 
 class TwitterAccount < ActiveRecord::Base
-  
+
+
+  # Those are our Consumer Token and Consumer secret that twitter
+  # provided us. This correspons our entity to twitter. The Consumer
+  # Secret should be saved in a safe place. 
   CONSUMER_TOKEN  = 'cuE56v2eK79gsxK1baasA'
   CONSUMER_SECRET = 'vIJmDpiqDNpiau2YaiHH19fW1zj5DRXjI1VUbuM7lk' 
 
+  # auth_secret and auth_token are the access keys to the twitter 
+  # accounts. Twitter provides them to us in phase to in the 
+  # authentication. see twitter_requests_controller.rb for more info 
   attr_accessible :auth_secret, :auth_token
   belongs_to :user, class_name: "User" 
 
@@ -14,6 +21,9 @@ class TwitterAccount < ActiveRecord::Base
   validates :user_id, uniqueness: true
 
 
+  # This method configure twitters Gem paramters. This configuratin 
+  # should be done whenever a feed will be request or a tweet will be
+  # posted
   def config_twitter
     Twitter.configure do |config|
       config.consumer_key         = CONSUMER_TOKEN
@@ -23,7 +33,13 @@ class TwitterAccount < ActiveRecord::Base
     end
     puts 'twitter configured'
   end 
-
+  
+  # This methods is a simple method to get the feed from a twitter account.
+  # The heart of this method is the much-richer Twitter::Client#home_timeline
+  # method. Which has many featurs and options. But for the sake of simplicity 
+  # I have pruned the option to one optional option which is the pages.  a page
+  # contains 20 tweets. Adding a new options will be as simple as putting them 
+  # into a hash and providing them to the home_timeline method. 
   def get_feed(page=1)
     self.config_twitter
     puts "getting the feed"
@@ -36,8 +52,9 @@ class TwitterAccount < ActiveRecord::Base
     end 
     stories
   end 
-
-  # This representation was asked by C1 lead. 
+  
+  # This method converts a Twitter::Status to Story class. This representation
+  # was asked by C1 
   def self.tweet_to_story(tweet)
     #story = [author: tweet['user']['name'], text: tweet['text']]
     story = Story.new
@@ -51,8 +68,9 @@ class TwitterAccount < ActiveRecord::Base
 
 
 #  def tweet(tweet)
+#    configure_twitter
 #    client = Twitter::Client.new
 #    client.update(tweet)
 #  end 
-#
+
 end
