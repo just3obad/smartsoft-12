9882c4907efef3c1cc639faf54a78b7ee3892d75
mina.workspace <mina.workspace@gmail.com>
2012-04-24 01:45:14 +0000

    Update issue 14 Labels: Docs-ready Added docs

--- nokia_ruby/app/models/log.rb
+++ nokia_ruby/app/models/log.rb
@@ -1,6 +1,7 @@
 class Log < ActiveRecord::Base
   attr_accessible :admin_id, :interest_id, :loggingtype, :message, :story_id, :user_id_1, :user_id_2
 end
+#used for rendering xls
 def as_xls(options = {})
   {
       "DateTime" => created_at.to_s,
