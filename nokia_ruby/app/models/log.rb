5debb464d363ef2c185f6776fbeb1dbae5896e0e
mina.workspace <mina.workspace@gmail.com>
2012-04-24 01:29:58 +0000

    Update issue 14 Modified controller ,Added new file for exporting as .xls,change in UI

--- nokia_ruby/app/models/log.rb
+++ nokia_ruby/app/models/log.rb
@@ -1,3 +1,9 @@
 class Log < ActiveRecord::Base
   attr_accessible :admin_id, :interest_id, :loggingtype, :message, :story_id, :user_id_1, :user_id_2
 end
+def as_xls(options = {})
+  {
+      "DateTime" => created_at.to_s,
+      "Message" => message
+  }
+end
