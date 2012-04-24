309a64e9763e94d3109bbfa3905f708d90637569
kerolos.bebawy <kerolos.bebawy@gmail.com>
2012-04-24 19:28:56 +0000

    updated Issue 46 Fixed the Emailer to be able to send emails to users when they want to resend the verification code

--- nokia_ruby/app/mailers/emailer.rb
+++ nokia_ruby/app/mailers/emailer.rb
@@ -1,18 +1,17 @@
 class Emailer < ActionMailer::Base
-  default from: "kerolos.bebawy@gmail.com"
+  default from: "info.2allak@gmail.com"
 
-  def registration_confirmation(account)
-  recipients  account.email
-  from        "kerolos.bebawy@gmail.com"
-  subject     "Thank you for Registering, Please Verify your account"
-  body        (:account => account,:code ==>VerificationCode.find_by_user_id(user.id).code)
+  def resend_code(account)
+  #Emailer.resend_code(@account).deliver
+  @code = VerificationCode.find_by_user_id(account.id).code
+  mail(:to => account.email, :subject => "2allak Verification Code")
   end
 
-  def password_reset(account)
-  recipients account.email
-  from       "mina.adel.fahmy@gmail.com"
-  subject    "Your password reset email"
-  body       (:account => account,:password ==>Haccount.new_pass(user.id))
-  end
+  #def password_reset(account)
+  #recipients account.email
+  #from       "mina.adel.fahmy@gmail.com"
+  #subject    "Your password reset email"
+  #body       (:account => account,:password ==>Haccount.new_pass(user.id))
+  #end
 
 end
--- nokia_ruby/config/environments/development.rb
+++ nokia_ruby/config/environments/development.rb
@@ -34,4 +34,14 @@ NokiaRuby::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'baci.lindsaar.net',
+    :user_name            => 'info.2allak@gmail.com',
+    :password             => 'info2allak',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  }
 end
