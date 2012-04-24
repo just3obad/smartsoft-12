7cba7a99fd644c3cbb0c3c3e6a3703ea995608fc
kemooo17@gmail.com <kemooo17@gmail.com@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-04-24 15:16:08 +0000

    update issue 10 : updated Flag and Like Dislike methods

--- trunk/nokia_ruby/app/controllers/likedislikes_controller.rb
+++ trunk/nokia_ruby/app/controllers/likedislikes_controller.rb
@@ -3,30 +3,46 @@ class LikedislikesController < ApplicationController
 respond_to :html,:json
 
 #this method is called when a user presses Like on the mobile App
-def thumb
+def like
   
    @user_id = params[:uid]
     @story_id = params[:sid]
-    @action = params[:act] 
+
+  
    thumped = Likedislike.where(:story_id => @story_id, :user_id => @user_id)
      if thumped.empty?
-        Likedislike.create!(:user_id => @user_id, :story_id => @story_id , :action => @action)
-       #@story = Story.find(@story_id)
-       #@story.likes = @story.likes+1
-       #@story.save   
-     end
+        Likedislike.create!(:user_id => @user_id, :story_id => @story_id , :action => 1)
   
-end
-
-#def dislike(user_id,story_id)
- #   thumped = Likedislike.where(:stroy_id => story_id, :user_id => user_id)
-  #   if thumped.empty?
-   #    Likedislike.create!(:user_id => user_id, :story_id => story_id , :action => -1 )
-    #   @story = Story.find(story_id)
-     #  @story.likes = @story.dislikes+1
-      # @story.save
-     #end
-#end
 
 
-end
+    else
+     
+     Likedislike.find(:first , :conditions => ["user_id = ? AND story_id = ?" ,@user_id , @story_id]).destroy
+      Likedislike.create!(:user_id => @user_id, :story_id => @user_id , :action => 1 )
+     
+
+  
+ end
+
+  end
+
+
+def dislike
+     @user_id = params[:uid]
+    @story_id = params[:sid]
+
+    thumped = Likedislike.where(:story_id => @story_id, :user_id => @user_id)
+     if thumped.empty?
+       Likedislike.create!(:user_id => @user_id, :story_id => @story_id , :action => -1 )
+ 
+    else
+      Likedislike.find(:first , :conditions => ["user_id = ? AND story_id = ?" ,@user_id , @story_id]).destroy
+      Likedislike.create!(:user_id => @user_id, :story_id => @user_id , :action => -1 )
+  
+     end
+
+end      
+end 
+
+
+
