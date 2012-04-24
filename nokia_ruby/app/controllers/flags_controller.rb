24b353842cf00ac359034670f0515b195a833265
kemooo17 <kemooo17@gmail.com>
2012-04-24 15:16:08 +0000

    update issue 10 : updated Flag and Like Dislike methods

--- nokia_ruby/app/controllers/flags_controller.rb
+++ nokia_ruby/app/controllers/flags_controller.rb
@@ -4,12 +4,21 @@ respond_to :html,:json
 
 def flag
 @story_id = params[:sid]
-@user_id = paramas [:uid]
-   thumped = Flag.where(:stroy_id => @story_id, :user_id => @user_id)
+@user_id = params[:uid]
+   f = false
+   thumped = Flag.where(:story_id => @story_id, :user_id => @user_id)
      if thumped.empty?
         Flag.create!(:user_id =>  @user_id, :story_id => @story_id)
       # @story = Story.find(story_id)
       # @story.flags = @story.flags+1
-      # @story.save   
+      # @story.save
+       f = true
      end
+respond_to do |format|
+    format.json { render json: f   }
+ end
+
+
+end
+
 end
