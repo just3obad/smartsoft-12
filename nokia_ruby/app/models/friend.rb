ed24794b70e6870722b40b3da21e16a0aea5dbbe
just3obad <just3obad@gmail.com>
2012-04-25 12:32:06 +0000

    update issue 28

---
+++ nokia_ruby/app/models/friend.rb
@@ -0,0 +1,11 @@
+class Friends < ActiveRecord::Base
+  attr_accessible :receiver, :sender, :stat
+  
+  belongs_to :user
+  belongs_to :friend, :class_name => "User", :foreign_key => "friend_id"
+
+  validates :receiver, presence: true
+  validates :sender, presence: true
+  validates :stat, presence: true
+  
+end
