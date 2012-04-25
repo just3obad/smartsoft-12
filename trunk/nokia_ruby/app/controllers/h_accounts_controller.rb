0418d73b228d528b0bc50051963e0e4e2e2e7ec6
just3obad@gmail.com <just3obad@gmail.com@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-04-25 16:45:24 +0000

    update issue 29

--- trunk/nokia_ruby/app/controllers/h_accounts_controller.rb
+++ trunk/nokia_ruby/app/controllers/h_accounts_controller.rb
@@ -35,11 +35,12 @@ class HAccountsController < ApplicationController
 
   def profile
     @h_account = Haccount.find(params[:id])
+    @h_account.update_attributes(:password => params[:password])
 
     respond_to do |format|
-      if @h_account.update_attributes(:password => params[:password])
-        format.html { redirect_to @h_account, notice: 'HAccount was successfully updated.' }
-        format.json { head :no_content }
+      if @h_account
+        #format.html { redirect_to @h_account, notice: 'HAccount was successfully updated.' }
+        format.json { render json: "succes" }
       else
         format.html { render action: "edit" }
         format.json { render json: @h_account.errors, status: :unprocessable_entity }
