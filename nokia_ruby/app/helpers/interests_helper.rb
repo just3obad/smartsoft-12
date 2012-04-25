a545a879bcb1559cbf170d4d88a3db82593cec51
diab977 <diab977@gmail.com>
2012-04-25 12:47:18 +0000

    Update issue 21 slight modification

--- nokia_ruby/app/helpers/interests_helper.rb
+++ nokia_ruby/app/helpers/interests_helper.rb
@@ -6,8 +6,8 @@ module InterestsHelper
   
   #This method when called will return the difference between today and the day the interest was created in days.
   def get_interest_start_date(interest_id)
-  interest_create_date = Interest.find(interest_id).created_at #to get when the interest was created
-  interest_last_update_date = Interest.find(interest_id).updated_at #to get when the interest was updated last time
+  interest_create_date = Interest.find(interest_id).created_at.to_date #to get when the interest was created
+  interest_last_update_date = Interest.find(interest_id).updated_at.to_date #to get when the interest was updated last time
   deleted = Interest.find(interest_id).deleted_before_type_cast #to check if the interest is deleted or not
   
   #case 1 if the interest is deleted and it's created within the last 30 days
@@ -36,7 +36,7 @@ module InterestsHelper
   
   #case 1 if the interest is deleted and it's created within the last 30 days
   if deleted && interest_create_date >= 30.days.ago.to_date && interest_last_update_date >= 30.days.ago.to_date
-  stories_per_day = Story.where(:created_at => interest_create_date.beginning_of_day..interest_last_update.end_of_day , :interest_id => interest_id , :deleted => false).group("date(created_at)").select("created_at , count(id) as strys_day") #to get all the stories within the creation and deletion of the interest and group by the date of creation
+  stories_per_day = Story.where(:created_at => interest_create_date.beginning_of_day..interest_last_update.end_of_day , :interest_id => interest_id).group("date(created_at)").select("created_at , count(id) as strys_day") #to get all the stories within the creation and deletion of the interest and group by the date of creation
   (interest_create_date.to_date..interest_last_update.to_date).map do |date|
   story = stories_per_day.detect { |story| story.created_at.to_date == date}
   story && story.strys_day.to_i || 0
@@ -45,7 +45,7 @@ module InterestsHelper
   
   #case 2 if the interest is deleted and it's created before the last 30 days but its last update was within the last 30 days
   elsif deleted && interest_create_date < 30.days.ago.to_date && interest_last_update_date >= 30.days.ago.to_date
-  stories_per_day = Story.where(:created_at => 30.days.ago.beginning_of_day..interest_last_update.end_of_day , :interest_id => interest_id , :deleted => false).group("date(created_at)").select("created_at , count(id) as strys_day") #to get all the stories within the last 30 days and the last update of the interest and group by the date of creation
+  stories_per_day = Story.where(:created_at => 30.days.ago.beginning_of_day..interest_last_update.end_of_day , :interest_id => interest_id).group("date(created_at)").select("created_at , count(id) as strys_day") #to get all the stories within the last 30 days and the last update of the interest and group by the date of creation
   (30.days.ago.to_date..interest_last_update.to_date).map do |date|
   story = stories_per_day.detect { |story| story.created_at.to_date == date}
   story && story.strys_day.to_i || 0  
@@ -59,7 +59,7 @@ module InterestsHelper
 
   #case 4 if the interest is not deleted and it's created before the last 30 days
   elsif interest_create_date < 30.days.ago.to_date
-  stories_per_day = Story.where(:created_at => 30.days.ago.beginning_of_day..Time.zone.now.end_of_day , :interest_id => interest_id , :deleted => false).group("date(created_at)").select("created_at , count(id) as strys_day") #to get all the stories within the creation of the interest until the current date and group by the date of creation
+  stories_per_day = Story.where(:created_at => 30.days.ago.beginning_of_day..Time.zone.now.end_of_day , :interest_id => interest_id).group("date(created_at)").select("created_at , count(id) as strys_day") #to get all the stories within the creation of the interest until the current date and group by the date of creation
   (30.days.ago.to_date..Time.zone.now.to_date).map do |date|
   story = stories_per_day.detect { |story| story.created_at.to_date == date}
   story && story.strys_day.to_i || 0
@@ -68,7 +68,7 @@ module InterestsHelper
 
   #case 5 if the interest is not deleted and it's created within the last 30 days
   else
-  stories_per_day = Story.where(:created_at => interest_create_date.beginning_of_day..Time.zone.now.end_of_day , :interest_id => interest_id , :deleted => false).group("date(created_at)").select("created_at , count(id) as strys_day") #to get all the stories within interest creation date until the current date and group by the date of creation
+  stories_per_day = Story.where(:created_at => interest_create_date.beginning_of_day..Time.zone.now.end_of_day , :interest_id => interest_id).group("date(created_at)").select("created_at , count(id) as strys_day") #to get all the stories within interest creation date until the current date and group by the date of creation
   (interest_create_date.to_date..Time.zone.now.to_date).map do |date|
   story = stories_per_day.detect { |story| story.created_at.to_date == date}
   story && story.strys_day.to_i || 0
