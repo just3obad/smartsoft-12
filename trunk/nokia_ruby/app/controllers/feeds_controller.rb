9bcfdf5ad413676c5a61d155bd13be25e94301c6
bassemawny92@gmail.com <bassemawny92@gmail.com@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-04-25 19:08:08 +0000

    Update Issue 33
    Deleting RSS feeds (still under construction)

--- trunk/nokia_ruby/app/controllers/feeds_controller.rb
+++ trunk/nokia_ruby/app/controllers/feeds_controller.rb
@@ -9,7 +9,7 @@ end
 
   def create
     
-    @feed= Feed.new(params[:feed])#retrieving the feed from the database using the table feed parameters
+    @feed= Feed.new(params[:feed]) #retrieving the feed from the database using the table feed parameters
     
     if @feed.save # if we succeded to add a new RSS feed then a flash message in a green box will appear with a message indicating that , we will redirect to the interests page again .
       flash[:success] = "RSS added Successfully !"
@@ -23,6 +23,14 @@ end
 
 
   def destroy
+  end
+  
+  def delete_page #method to redirect to a page where feeds will be deleted.
+   @feeds = Feed.find(params[:interest_id])
+  end
+
+  def delete # a method that deletes RSS feeds from the database.
+    
   end
 end
 
