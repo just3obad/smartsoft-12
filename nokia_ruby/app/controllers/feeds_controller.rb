f6f0b3efa5209e221c89f71bbbe960395dd54916
bassemawny92 <bassemawny92@gmail.com>
2012-04-26 03:05:00 +0000

    Update Issue 33
    Adding wrong RSS do not redirect to another page.

--- nokia_ruby/app/controllers/feeds_controller.rb
+++ nokia_ruby/app/controllers/feeds_controller.rb
@@ -12,11 +12,12 @@ end
     @feed= Feed.new(params[:feed]) #retrieving the feed from the database using the table feed parameters
     
     if @feed.save # if we succeded to add a new RSS feed then a flash message in a green box will appear with a message indicating that , we will redirect to the interests page again .
-      flash[:success] = "RSS added Successfully !"
+
       redirect_to :controller => 'interests', :action => 'show', :id => @feed.interest_id
-    else#if saving faild a flash message box will appear with the errors
-     @title = "Add feed"
-     render 'new'
+      flash[:warning] = "Thank you, we have updated your order with " 
+    else #if saving faild a flash message box will appear with the errors
+     redirect_to :controller => 'interests', :action => 'show', :id => @feed.interest_id
+     
     end
   end
 
