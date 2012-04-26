fec96378a7556ad9f94793e64d6c370a82fa57b2
just3obad <just3obad@gmail.com>
2012-04-26 09:55:33 +0000

    update issue 28

--- nokia_ruby/app/controllers/users_controller.rb
+++ nokia_ruby/app/controllers/users_controller.rb
@@ -83,7 +83,7 @@ end
     @user=User.find(@user_id).update_attributes(:name=>params[:name],:first_name=>params[:first_name],:last_name=>params[:last_name],:date_of_birth=>params[:date_of_birth] )
     @huser=Haccount.find(@user_id).update_attributes(:password => params[:password])
    if @user&@huser or @user or @huser
-    format.json { render json: "updated" }
+    #format.json { render json: "updated" }
     respond_with("updated")
     end
   end
