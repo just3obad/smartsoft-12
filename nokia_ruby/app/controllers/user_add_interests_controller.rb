e244890ac7150374e730774c60739e0da4ba6a2e
omar.saeed21 <omar.saeed21@gmail.com>
2012-04-26 14:05:29 +0000

    modified def add and def addwithduplicates

--- nokia_ruby/app/controllers/user_add_interests_controller.rb
+++ nokia_ruby/app/controllers/user_add_interests_controller.rb
@@ -14,6 +14,26 @@ end
 end
 #end
 
+
+
+
+def addwithduplicates
+@userid = params[:id]
+@interests = params[:interests]
+@interests.each do |element|
+  @intid = Interest.find_by_name(element).id
+  UserAddInterest.create(user_id:@userid , interest_id:@intid)
+end
+respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @userid }
+end
+end
+end
+
+
+
+
 def add #add interests user choosed into table
 @userid = params[:id]
 @interests = params[:interests]
