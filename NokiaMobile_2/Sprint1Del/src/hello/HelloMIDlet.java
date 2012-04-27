5b359a8119bc67f0327ead529f51404692e2f459
mina.adel.fahmy <mina.adel.fahmy@gmail.com>
2012-04-27 20:26:20 +0000

    Update to pre-project down state

--- NokiaMobile_2/Sprint1Del/src/hello/HelloMIDlet.java
+++ NokiaMobile_2/Sprint1Del/src/hello/HelloMIDlet.java
@@ -5064,7 +5064,7 @@ public class HelloMIDlet extends MIDlet implements CommandListener {
     public Alert getPleaseTryAgain() {
         if (PleaseTryAgain == null) {//GEN-END:|470-getter|0|470-preInit
             // write pre-init user code here
-            PleaseTryAgain = new Alert("alert3", "Unexpected Error Please Try Again", null, null);//GEN-BEGIN:|470-getter|1|470-postInit
+            PleaseTryAgain = new Alert("alert3");//GEN-BEGIN:|470-getter|1|470-postInit
             PleaseTryAgain.setTimeout(Alert.FOREVER);//GEN-END:|470-getter|1|470-postInit
             // write post-init user code here
         }//GEN-BEGIN:|470-getter|2|
