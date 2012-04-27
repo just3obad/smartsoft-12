581f226222939c24a71f3e0b1b871548981ed6c2
mohamed.shafei90 <mohamed.shafei90@gmail.com>
2012-04-27 19:05:00 +0000

    Update issue 17
    fixed error box problem

--- nokia_ruby/app/helpers/statistics_helper.rb
+++ nokia_ruby/app/helpers/statistics_helper.rb
@@ -38,10 +38,10 @@ include UsersHelper
   '''
  def get_user_stat(user_id)
   s = get_no_of_shares_user(user_id)
- n = get_no_of_likes_user(user_id)
- m = get_no_of_dislikes_user(user_id)
- p = get_no_of_flags_user(user_id)
- c = get_no_of_comments_user(user_id)
+  n = get_no_of_likes_user(user_id)
+  m = get_no_of_dislikes_user(user_id)
+  p = get_no_of_flags_user(user_id)
+  c = get_no_of_comments_user(user_id)
  data = "[#{s},#{n},#{m},#{p},#{c}]"
  end
  
