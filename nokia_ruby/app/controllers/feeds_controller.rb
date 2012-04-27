70e59d369e7c894ad75f340f633cac71d1f0fe2c
bassemawny92 <bassemawny92@gmail.com>
2012-04-27 18:50:29 +0000

    Update Issue 33
    Flash is colored right

--- nokia_ruby/app/controllers/feeds_controller.rb
+++ nokia_ruby/app/controllers/feeds_controller.rb
@@ -14,17 +14,20 @@ end
 
   def create
     #@id1 = Feed.find(params[:id]).interest_id
+    $saved
     @feed= Feed.new(params[:feed]) #retrieving the feed from the database using the table feed parameters
     @interestid= params[:feed][:interest_id]
     if params.has_key?(:feed) and StoriesHelper.check_rss(params[:feed][:link])
     if @feed.save
+    $saved=true
 
       redirect_to :controller => 'interests', :action => 'show', :id => @interestid
-      flash[:success] = "Link added successfully"
+      flash[:success] = "Link added successfully."
       StoriesHelper.fetch_rss(params[:feed][:link])
     end
     else #if saving faild a flash message box will appear with the errors
-     flash[:error] = "link not added. Please try again" 
+     $saved=false
+     flash[:error] = "Link is invalid. Please try again" 
       redirect_to :controller => 'interests', :action => 'show', :id => @feed.interest_id
     end
   end
