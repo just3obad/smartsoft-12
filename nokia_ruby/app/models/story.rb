a29c9fe8f6ab15550eb72efb44ceab6338e9ae7e
gosgosakila <gosgosakila@gmail.com>
2012-04-27 19:35:31 +0000

    Update issue 27 Modified UI of Story Page

--- nokia_ruby/app/models/story.rb
+++ nokia_ruby/app/models/story.rb
@@ -3,20 +3,20 @@ class Story < ActiveRecord::Base
 # :rank==>hottness of a story, :interest_id==>id of the related interest,
 # :type==> 1 (Article) 2 (Image) 3 (video)
   attr_accessible :interest_id, :title, :date, :rank, 
-		  :media_link, :is_blocked, :category, :content, :deleted, :hidden
+		  :media_link, :category, :content, :deleted, :hidden
   belongs_to :interests
-  has_many :comments
+  has_many   :comments
   #def initialize(title, date, body)
    # @title = title
     #@body = body
   #end
 
-  URL_regex = /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,6}(:[0-9]{1,5})?(\/.*)?$/ix
+  URL_regex = /^(?:(?:http|https):\/\/[a-z0-9]+(?:[\-\.]{1}[a-z0-9]+)*\.[a-z]{2,6}(?::[0-9]{1,5})?(\/.*)?)|(?:^$)$/ix
 # putting some validations on the title and interest_id that they are present
   validates :title , :presence=>true
   validates :interest_id, :presence=>true
 # checking that the media_link is a valid URL according to the regex defined above.
-#  validates :media_link, :format=> {:with => URL_regex}
+  validates :media_link, :format=> {:with => URL_regex}
 			 
 # get_story is a method that takes a specific story_id as an input  and searches the database for the stroy with this id and returns #this story to the caller
 
