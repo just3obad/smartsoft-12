6c2304d54cd201289917abb8e7c4a0ddf9605114
bassemawny92 <bassemawny92@gmail.com>
2012-04-27 17:45:58 +0000

    Update issue 33 Done with add / delete rss

--- nokia_ruby/app/controllers/feeds_controller.rb
+++ nokia_ruby/app/controllers/feeds_controller.rb
@@ -5,17 +5,21 @@ end
 def show
    redirect_to :controller => 'interests', :action => 'show', :id => @interest_id
 end
+def update
+end
+
 def new 
   @feed = Feed.new
 end
 
   def create
+    #@id1 = Feed.find(params[:id]).interest_id
     @feed= Feed.new(params[:feed]) #retrieving the feed from the database using the table feed parameters
-
+    @interestid= params[:feed][:interest_id]
     if params.has_key?(:feed) and StoriesHelper.check_rss(params[:feed][:link])
     if @feed.save
 
-      redirect_to :controller => 'interests', :action => 'show', :id => @feed.interest_id
+      redirect_to :controller => 'interests', :action => 'show', :id => @interestid
       flash[:success] = "Link added successfully"
       StoriesHelper.fetch_rss(params[:feed][:link])
     end
