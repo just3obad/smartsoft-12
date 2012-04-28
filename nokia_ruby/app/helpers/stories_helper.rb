75a3f360b4532e34a9bc01bc2e6d508cc41a85a1
mouaz.alabsawi <mouaz.alabsawi@gmail.com>
2012-04-28 12:27:48 +0000

    Update issue 30 leaving the hidden variable to its default value

--- nokia_ruby/app/helpers/stories_helper.rb
+++ nokia_ruby/app/helpers/stories_helper.rb
@@ -184,7 +184,7 @@ def fetch_rss(link)
       #getting the id of the interest 
       sinterest = Feed.where(:link => source).select("interest_id")
 
-      storynow = Story.new(:title => stitle, :rank => 0, :category => "RSS", :deleted => false, :hidden => false, :interest_id => sinterest, :media_link => "")
+      storynow = Story.new(:title => stitle, :rank => 0, :category => "RSS", :deleted => false, :interest_id => sinterest, :media_link => "")
 
 
 
