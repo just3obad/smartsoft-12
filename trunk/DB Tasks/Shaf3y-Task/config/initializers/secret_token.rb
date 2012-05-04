564b657df451fe3a710031dfb33ad9865473c863
mohamed.shafei90@gmail.com <mohamed.shafei90@gmail.com@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-05-04 16:32:03 +0000

    DB-Task

---
+++ trunk/DB Tasks/Shaf3y-Task/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shaf3y::Application.config.secret_token = '6fffecb819fc8b07f765e792791d6667550d9df7923b70c3e81c3d7b5b0ee1a3ae76234c602a34850403a59e430ba52da5a6980f0484b57beb9c2be7899e774a'
