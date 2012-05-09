8b86d8903f406646c04ab76bbcc84a815aaa1592
elmenisy <elmenisy@gmail.com>
2012-05-09 23:17:45 +0000

    Moved Log insertion to comment model and fixed the create_comment action in stories controller to be working with associations

--- La2etlak_Backend/app/models/comment.rb
+++ La2etlak_Backend/app/models/comment.rb
@@ -3,4 +3,10 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :story
   has_many :comment_up_downs
+  
+  # This method adds the details of this comment to the log file.
+  # It will be called after a successful creation of the comment
+  def add_to_log
+    Log.create!(loggingtype: 2,user_id_1: self.user.id ,user_id_2: nil,admin_id: nil,story_id: self.story.id ,interest_id: nil,message: (self.user.name+" commented on \"" + self.story.title + "\" with \"" + self.content + "\"").to_s )
+  end
 end
