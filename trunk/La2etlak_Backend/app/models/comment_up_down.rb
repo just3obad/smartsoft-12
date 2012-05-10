bfb91dae6acfa1c320e97e201c7b47c9991e5098
elmenisy@gmail.com <elmenisy@gmail.com@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-05-10 12:01:24 +0000

    Fixed some errors in the Comment and CommentUpDown models

--- trunk/La2etlak_Backend/app/models/comment_up_down.rb
+++ trunk/La2etlak_Backend/app/models/comment_up_down.rb
@@ -3,7 +3,13 @@ class CommentUpDown < ActiveRecord::Base
   belongs_to :user
   belongs_to :comment
   
-  add_to_log(commenter)
-     Log.create!(loggingtype: 2,user_id_1: self.user.id,user_id_2: commenter.id,admin_id: nil,story_id: nil,interest_id: nil,message: self.user.name +" thumbed UP the comment \"" +  self.comment.content +"\"" + " by " + commenter.name )
+  def add_to_log(commenter)
+    msg = nil
+    if self.action == 1 then
+      msg = "UP"
+    else
+      msg = "DOWN"
+    end
+     Log.create!(loggingtype: 2,user_id_1: self.user.id,user_id_2: commenter.id,admin_id: nil,story_id: nil,interest_id: nil,message: self.user.name + " thumbed " + msg + " the comment \"" +  self.comment.content +  "\"" + " by " + commenter.name )
   end
 end
