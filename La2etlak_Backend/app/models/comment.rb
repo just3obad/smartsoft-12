d5c188a9bf426dc7f45bc957972145b91b5dbe5f
elmenisy <elmenisy@gmail.com>
2012-05-10 00:53:04 +0000

    Moved logic of the ups and downs to model and added helpe methods to seperate the code and make it more readable

--- La2etlak_Backend/app/models/comment.rb
+++ La2etlak_Backend/app/models/comment.rb
@@ -9,4 +9,64 @@ class Comment < ActiveRecord::Base
   def add_to_log
     Log.create!(loggingtype: 2,user_id_1: self.user.id ,user_id_2: nil,admin_id: nil,story_id: self.story.id ,interest_id: nil,message: (self.user.name+" commented on \"" + self.story.title + "\" with \"" + self.content + "\"").to_s )
   end
+  
+  # This method checks if a user thumbed up this comment before
+  def upped_by_user?(user)
+    up = self.comment_up_downs.find_by_user_id_and_action(user.id,1) # get the up that a user gave to this comment before "if exists"
+    !up.nil? # if up is nil then return false if not then return true
+  end
+  
+  # This method checks if a user thumbed up this comment before
+  def downed_by_user?(user)
+    down = self.comment_up_downs.find_by_user_id_and_action(user.id,2) # get the down that a user gave to this comment before "if exists"
+    !down.nil? # if down is nil then return false if not then return true
+  end
+  
+  def up_comment?(user)
+    upped_before = self.upped_by_user?(user)
+    downed_before = self.downed_by_user?(user)
+    if !upped_before && !downed_before  #if user never upped or downed the comment then up it
+      up = Comment_up_down.create(:action => 1)
+      user.comment_up_downs << up
+      comment.comment_up_downs << up
+      up.add_to_log(self.user)
+      render json: "yes" and return true
+   #  self.comment_up_downs << Comment_up_down.create()
+    else if downed_before
+      self.comment_up_downs.find_by_user_id_and_action(user.id,2).destroy #if user disliked it, now make him like it!
+      up = Comment_up_down.create(:action => 1)
+      user.comment_up_downs << up
+      comment.comment_up_downs << up
+      up.add_to_log(self.user)
+      render json: "yes" and return true
+    else
+      render json: "no" and return false
+    end
+      
+  end
+  
+  def down_comment?(user)
+    upped_before = self.upped_by_user?(user)
+    downed_before = self.downed_by_user?(user)
+    if !upped_before && !downed_before  #if user never upped or downed the comment then up it
+      down = Comment_up_down.create(:action => 2)
+      user.comment_up_downs << down
+      comment.comment_up_downs << down
+      down.add_to_log(self.user)
+      render json: "yes" and return true
+   #  self.comment_up_downs << Comment_up_down.create()
+    else if upped_before
+      self.comment_up_downs.find_by_user_id_and_action(user.id,1).destroy #if user disliked it, now make him like it!
+      down = Comment_up_down.create(:action => 1)
+      user.comment_up_downs << down
+      comment.comment_up_downs << down
+      down.add_to_log(self.user)
+      render json: "yes" and return true
+    else
+      render json: "no" and return false
+    end
+      
+  end
+    
+  
 end
--- La2etlak_Backend/app/models/comment_up_down.rb
+++ La2etlak_Backend/app/models/comment_up_down.rb
@@ -2,4 +2,8 @@ class CommentUpDown < ActiveRecord::Base
   attr_accessible :action
   belongs_to :user
   belongs_to :comment
+  
+  add_to_log(commenter)
+     Log.create!(loggingtype: 2,user_id_1: self.user.id,user_id_2: commenter.id,admin_id: nil,story_id: nil,interest_id: nil,message: self.user.name +" thumbed UP the comment \"" +  self.comment.content +"\"" + " by " + commenter.name )
+  end
 end
