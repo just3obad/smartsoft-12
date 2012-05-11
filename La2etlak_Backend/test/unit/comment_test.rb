2f698e1caf807c52392131b979665dea05a5b7c7
elmenisy <elmenisy@gmail.com>
2012-05-11 22:20:19 +0000

    Update issue 93 Status: InProgress Commiting 2 tests for Comments model content validation the ( test_should_save_with_content ) will always pass by default

--- La2etlak_Backend/test/unit/comment_test.rb
+++ La2etlak_Backend/test/unit/comment_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save without content red" do
+    comment = Comment.new
+    assert !comment.save, "Should not save without content"
+  end
+  test "should save with content" do  
+    comment = Comment.new
+    comment.content = "I'm the content"
+    assert comment.save, "Should save with content"
+  end
 end
