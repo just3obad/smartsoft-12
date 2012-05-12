b8c09c6cd9a24df758cb311553a3017b1ee138f5
elmenisy <elmenisy@gmail.com>
2012-05-12 10:14:13 +0000

    Update issue 93 Added tests for ups and downs

--- La2etlak_Backend/test/unit/comment_test.rb
+++ La2etlak_Backend/test/unit/comment_test.rb
@@ -22,4 +22,45 @@ class CommentTest < ActiveSupport::TestCase
   	comment.user = User.first
   	assert !comment.save , "Should not save without a story"
   end
+  test "should up a comment" do
+    assert_difference('CommentUpDown.find_all_by_action(1).count',1) do
+      up_comment
+    end
+  end
+  test "should down a comment" do
+    assert_difference('CommentUpDown.find_all_by_action(2).count',1) do
+      down_comment
+    end
+  end
+  test "should un-up when downed" do
+    down_comment
+    assert_difference('CommentUpDown.find_all_by_action(2).count',-1) do
+      up_comment
+    end
+  end
+  test "should un-down when upped" do
+    up_comment
+    assert_difference('CommentUpDown.find_all_by_action(1).count',-1) do
+      down_comment
+    end
+  end
+
+  def up_comment
+    comment = Comment.first
+    comment.content = "content"
+    user = User.first
+    comment.user = user
+    comment.story = Story.first
+    comment.save
+    comment.up_comment(user)
+  end
+  def down_comment
+    comment = Comment.first
+    comment.content = "content"
+    user = User.first
+    comment.user = user
+    comment.story = Story.first
+    comment.save
+    comment.down_comment(user)
+  end  
 end
