c0539587660a208bb57817da66ce53fb3e07eb0a
elmenisy@gmail.com <elmenisy@gmail.com@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-05-12 21:30:37 +0000

    Update issue 93 Moved create comment test to the comment functional test Added 2 green tests: should up comment should down comment These were added to the story functional test class since these actions reside in the stories controller

--- trunk/La2etlak_Backend/test/functional/comments_controller_test.rb
+++ trunk/La2etlak_Backend/test/functional/comments_controller_test.rb
@@ -1,5 +1,11 @@
 require 'test_helper'
 
 class CommentsControllerTest < ActionController::TestCase
-
+   # Author: Menisy
+  test "should create comment" do
+    assert_difference('Comment.count') do
+      post :create, :comment => {:content => "lololo" , :story_id => Story.first.id , :user_id => User.first.id}
+    end
+    assert_redirected_to comment_path(assigns(:comment))
+  end
 end
