cb6eb9624eed87a14af8b4de6221b62d518f910f
EssamAhmedHafez <EssamAhmedHafez@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-05-12 21:58:31 +0000

    update issue 88 , test ready

--- trunk/La2etlak_Backend/test/functional/users_controller_test.rb
+++ trunk/La2etlak_Backend/test/functional/users_controller_test.rb
@@ -4,4 +4,18 @@ class UsersControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  #Author : Essam
+  test "filter selected social account view RED" do
+    user = User.new
+    user.email = 'essam@hafez.com'
+    user.twitter_account = twitter_accounts(:one)
+    user_feed = user.twitter_account.get_feed()
+    assert !user_feed.nil?
+    # add new route , match "user/social_feed/:id/:socialID" => "users#view_social_network_feed"
+    assert_routing "user/social_feed/:id/:socialID" , { :controller => "users" , :action => "view_social_network_feed"}
+    get "user/social_feed/:id/:socialID"
+    assert_response :success
+  end
+  
 end
