c1e45521dc65d10547b3b788d12474d56eb24314
elmenisy@gmail.com <elmenisy@gmail.com@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-05-12 23:03:05 +0000

    Update issue 93 3 tests went green: should create comment =>  comment controller test should up comment => stories controller test should down comment => stories controller test

--- trunk/La2etlak_Backend/test/functional/stories_controller_test.rb
+++ trunk/La2etlak_Backend/test/functional/stories_controller_test.rb
@@ -6,7 +6,7 @@ class StoriesControllerTest < ActionController::TestCase
   # end
 
   # Author: Menisy
-  test "should up comment RED" do
+  test "should up comment" do
   	assert_difference('CommentUpDown.find_all_by_action(1).count',1) do
   		get :up_comment, :comment_id => Comment.first.id, :user_id => User.first.id
   	end
@@ -14,7 +14,7 @@ class StoriesControllerTest < ActionController::TestCase
   end
 
   # Author: Menisy
-  test "should down comment RED" do
+  test "should down comment" do
   	assert_difference('CommentUpDown.find_all_by_action(2).count',1) do
   		get :down_comment, :comment_id => Comment.first.id, :user_id => User.first.id
   	end
