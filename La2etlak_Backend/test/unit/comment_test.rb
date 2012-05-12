6585f03e829f3722c2da0132e06661bb8925b405
elmenisy <elmenisy@gmail.com>
2012-05-12 01:49:11 +0000

    Update issue 93 Added 2 tests for user and story validations

--- La2etlak_Backend/test/unit/comment_test.rb
+++ La2etlak_Backend/test/unit/comment_test.rb
@@ -7,7 +7,19 @@ class CommentTest < ActiveSupport::TestCase
   end
   test "should save with content" do  
     comment = Comment.new
-    comment.content = "I'm the content"
+    comment.content = "I'm the content lol"
     assert comment.save, "Should save with content"
+  end
+  test "should not save without user red" do
+  	comment = Comment.new
+  	comment.content = "content again"
+  	comment.story = Story.first
+  	assert !comment.save , "Should not save without a user"
+  end
+    test "should not save without story red" do
+  	comment = Comment.new
+  	comment.content = "content again"
+  	comment.user = User.first
+  	assert !comment.save , "Should not save without a stpry"
   end
 end
