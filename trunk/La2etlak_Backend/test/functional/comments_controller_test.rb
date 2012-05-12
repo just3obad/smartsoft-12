f46ba74eb9d496c4c2199d8f8742b7bf2d22ca60
elmenisy@gmail.com <elmenisy@gmail.com@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-05-12 11:11:20 +0000

    Update issue 93 Removed tests from the comment_controller_test since they are not needed anymore 1 test turned green

--- trunk/La2etlak_Backend/test/functional/comments_controller_test.rb
+++ trunk/La2etlak_Backend/test/functional/comments_controller_test.rb
@@ -1,17 +1,5 @@
 require 'test_helper'
 
 class CommentsControllerTest < ActionController::TestCase
-  test "should create comment" do
-    assert_difference('Comment.count') do
-      post :create, :content => "lololo"
-    end
-    assert_redirected_to comment_path(assigns(:comment))
-    assert_equal 'comment success', flash[:notice]
-  end
-
-  test "should get index" do
-    get :index
-    assert_response :success
-  end
 
 end
