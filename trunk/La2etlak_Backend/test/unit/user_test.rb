b86c3513859e689e3dcd5183c603d88cc07f1ae8
christinesed <christinesed@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-05-12 20:03:21 +0000

    Update issue 86 commiting tests RED

--- trunk/La2etlak_Backend/test/unit/user_test.rb
+++ trunk/La2etlak_Backend/test/unit/user_test.rb
@@ -1,9 +1,67 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  ##########Author: christinesed@gmail.com ############
+  test "get no of users who signed in today one more user signed in today RED" do
+    count=User.get_no_of_users_signed_in_today
+    usr=User.new(:email=>"example@gmail.com")
+    assert usr.save
+    log=UserLogIn.new
+    log.user=usr
+    assert log.save
+    count2=User.get_no_of_users_signed_in_today
+    assert_equal(count+1,count2)
+  end
+
+   ##########Author: christinesed@gmail.com ############
+  test "get no of users who signed in today shouldn't add RED" do
+    count=User.get_no_of_users_signed_in_today
+    usr=User.new(:email=>"example@gmail.com")
+    assert usr.save
+    log=UserLogIn.new
+    log.user=usr
+    log.created_at=2.days.ago
+    assert log.save
+    count2=User.get_no_of_users_signed_in_today
+    assert_equal(count,count2)
+  end
+
+   ##########Author: christinesed@gmail.com ############
+  test "get no of users who signed in today three more users signed in today RED" do
+    count=User.get_no_of_users_signed_in_today
+    usr=User.new(:email=>"example@gmail.com")
+    usr1=User.new(:email=>"example1@gmail.com")
+    usr2=User.new(:email=>"example2@gmail.com")
+    assert usr.save
+    assert usr1.save
+    assert usr2.save
+    log=UserLogIn.new
+    log.user=usr
+    assert log.save
+    log=UserLogIn.new
+    log.user=usr1
+    assert log.save
+    log=UserLogIn.new
+    log.user=usr2
+    assert log.save
+    count2=User.get_no_of_users_signed_in_today
+    assert_equal(count+3,count2)
+  end
+
+   ##########Author: christinesed@gmail.com ############
+   test "get no of users who signed in today a user signed in more than once RED" do
+    count=User.get_no_of_users_signed_in_today
+    usr=User.new(:email=>"example@gmail.com")
+    assert usr.save
+    log=UserLogIn.new
+    log.user=usr
+    assert log.save
+    log=UserLogIn.new
+    log.user=usr
+    assert log.save
+    count2=User.get_no_of_users_signed_in_today
+    assert_equal(count+1,count2)
+  end
   
   #Author : Essam
   test "filter social networks RED" do
