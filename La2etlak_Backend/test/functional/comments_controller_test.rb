98d9d7c66cf333619d6880b5512ed981f22796e4
elmenisy <elmenisy@gmail.com>
2012-05-12 23:03:05 +0000

    Update issue 93 3 tests went green: should create comment =>  comment controller test should up comment => stories controller test should down comment => stories controller test

--- La2etlak_Backend/test/functional/comments_controller_test.rb
+++ La2etlak_Backend/test/functional/comments_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class CommentsControllerTest < ActionController::TestCase
    # Author: Menisy
-  test "should create comment RED" do
+  test "should create comment" do
     assert_difference('Comment.count') do
       post :create, :comment => {:content => "lololo" , :story_id => Story.first.id , :user_id => User.first.id}
     end
