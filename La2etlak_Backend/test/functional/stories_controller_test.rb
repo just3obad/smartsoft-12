a60e93eeaedf1a921b85461ce975712a41ce91b1
elmenisy <elmenisy@gmail.com>
2012-05-12 11:11:20 +0000

    Update issue 93 Removed tests from the comment_controller_test since they are not needed anymore 1 test turned green

--- La2etlak_Backend/test/functional/stories_controller_test.rb
+++ La2etlak_Backend/test/functional/stories_controller_test.rb
@@ -4,7 +4,9 @@ class StoriesControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
-  test "should create comment red" do
+  test "should create comment" do
+  	p Story.first.id
+  	p User.first.id
     assert_difference('Comment.count') do
       post :create_comment, :content => "lololo" , :id => Story.first.id , :user_id => User.first.id
     end
