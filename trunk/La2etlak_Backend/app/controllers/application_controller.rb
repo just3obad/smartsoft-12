39fb98f43fe09f35821fb20ad181e3b47fdf7d2b
kerolos.bebawy@gmail.com <kerolos.bebawy@gmail.com@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-05-12 20:08:29 +0000

    Added the helper method current_user to the application controller

--- trunk/La2etlak_Backend/app/controllers/application_controller.rb
+++ trunk/La2etlak_Backend/app/controllers/application_controller.rb
@@ -1,3 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+	helper_method :current_user
+
+	# This is a helper method that returns the User record
+	# for the currently logged in user, this method is
+  # added in the application controller since it is
+	# needed to be accessed from anywhere inside the
+	# application.
+	# Author: Kiro
+	private
+	def current_user_session
+ 	 return @current_user_session if defined?(@current_user_session)
+ 	 @current_user_session = UserSession.find
+	end
+
+	def current_user
+ 	 @current_user = current_user_session && current_user_session.record
+	end
+
 end
