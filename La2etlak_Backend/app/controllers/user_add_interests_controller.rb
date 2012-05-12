278a067e54681ea7f6e8f3d80b4b8daf579b1f19
omar.saeed21 <omar.saeed21@gmail.com>
2012-05-12 21:52:23 +0000

    updated issue 78 , 79 moved logic from controller to model

--- La2etlak_Backend/app/controllers/user_add_interests_controller.rb
+++ La2etlak_Backend/app/controllers/user_add_interests_controller.rb
@@ -14,46 +14,4 @@ user = User.find(@id)
 end
 end
 
-#end
-
-
-
-
-def addwithduplicates
-@userid = params[:id]
-@interests = params[:interests]
-#interestsss = UserAddInterest.find_all_by_user_id(@userid)
- #interestsss.each do |t|
- #t.destroy
- #end
-@interests.each do |element|
-  @intid = Interest.find_by_name(element).id
-  UserAddInterest.create(user_id:@userid , interest_id:@intid)
-end
-respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @userid }
-end
-end
-
-
-
-
-
-def add #add interests user choosed into table
-@userid = params[:id]
-@interests = params[:interests]
- interestsss = UserAddInterest.find_all_by_user_id(@userid)
- interestsss.each do |t|
- t.destroy
- end
-@interests.each do |element|
-  @intid = Interest.find_by_name(element).id
-  UserAddInterest.create(:user_id => @userid , :interest_id => @intid)
-end
-respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @userid }
-end
-end
 end
