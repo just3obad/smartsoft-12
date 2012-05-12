6c46833367487e87adb597dbde06666fbd73029d
mohamed.shafei90 <mohamed.shafei90@gmail.com>
2012-05-12 21:53:14 +0000

    fixing get_user_stat

--- La2etlak_Backend/app/models/user.rb
+++ La2etlak_Backend/app/models/user.rb
@@ -326,40 +326,6 @@ end
  data = "[#{reg},#{log}]"
  end
 end
-	'''
-  This is the method that should return the data of statistics of a user
-  with this format first element in the data arrays is ARRAY OF "No Of Shares",
-  second one is "No Of Likes"
-  third one is "No of Dislikes"
-  and forth one is "No of Flags"
-  and fifth one is "No of Comments"
-	'''
-  
- def get_user_stat(user_id)
-  s = get_no_of_shares_user(user_id)
-  n = get_no_of_likes_user(user_id)
-  m = get_no_of_dislikes_user(user_id)
-  p = get_no_of_flags_user(user_id)
-  c = get_no_of_comments_user(user_id)
- data = "[#{s},#{n},#{m},#{p},#{c}]"
- end
-
- def get_user_start_date(user_id)
-   user_reg_date = User.find(user_id).created_at_before_type_cast.to_date
-   last_updated = User.find(user_id).updated_at_before_type_cast.to_date
-   deactivated = User.find(user_id).deactivated_before_type_cast
-   if deactivated && user_reg_date > 30.days.ago.to_date && last_updated > 30.days.ago.to_date
-     date = Time.zone.now.to_date - user_reg_date
-   elsif deactivated && user_reg_date <= 30.days.ago.to_date && last_updated > 30.days.ago.to_date
-     date = Time.zone.now.to_date - 30.days.ago.to_date
-   elsif deactivated && user_reg_date <= 30.days.ago.to_date && last_updated <= 30.days.ago.to_date
-     date = -1
-   elsif user_reg_date <= 30.days.ago.to_date
-     date = Time.zone.now.to_date - 30.days.ago.to_date
-   else
-     date = Time.zone.now.to_date - user_reg_date
-   end
- end
 
   # This return the consumer for twitter authentication
   # Author: Yahia
