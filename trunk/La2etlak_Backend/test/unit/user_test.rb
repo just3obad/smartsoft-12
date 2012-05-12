9651d750fb14f03ccedc6ed72d7d3d7b304a24f7
EssamAhmedHafez <EssamAhmedHafez@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-05-12 13:56:00 +0000

    update issue 89 , added test to it

--- trunk/La2etlak_Backend/test/unit/user_test.rb
+++ trunk/La2etlak_Backend/test/unit/user_test.rb
@@ -4,4 +4,15 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  #Author : Essam
+  test "filter social networks RED" do
+    user = User.new
+    user.email = 'essam@hafez.com'
+    user.twitter_account = twitter_accounts(:one)
+    assert !user.twitter_account.nil?
+    user_feed = user.twitter_account.get_feed()
+    assert !user_feed.nil?
+  end
+
 end
