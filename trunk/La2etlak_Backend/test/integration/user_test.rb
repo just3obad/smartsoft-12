b0089f5fb148bdcd76a4cefbca88892c60b02176
rana.helal92@gmail.com <rana.helal92@gmail.com@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-05-12 23:01:55 +0000

    Update Issue 92 add integration tests to test routes

--- trunk/La2etlak_Backend/test/integration/user_test.rb
+++ trunk/La2etlak_Backend/test/integration/user_test.rb
@@ -40,4 +40,14 @@ class UserTest < ActionDispatch::IntegrationTest
      end
   end
 
+    #Author: Rana
+    test "route to block story RED" do
+      assert_routing 'users/block_story/1/1', {:controller => "users", :action => "block_story", :uid => "1", :id => "1" }
+    end
+
+    #Author: Rana   
+    test "route to block interest RED" do
+      assert_routing 'users/block_interest/1/1', {:controller => "users", :action => "block_interest", :uid => "1", :id => "1" }
+    end
+
 end
