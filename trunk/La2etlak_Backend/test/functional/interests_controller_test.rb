8f3d64375dc29184e5c0038907a419a8587e0984
jailan.salah@gmail.com <jailan.salah@gmail.com@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-05-13 09:15:57 +0000

    Update issue 59 Labels: Test-Ready
    Modifying Tests

--- trunk/La2etlak_Backend/test/functional/interests_controller_test.rb
+++ trunk/La2etlak_Backend/test/functional/interests_controller_test.rb
@@ -1,19 +1,26 @@
 require 'test_helper'
 
 class InterestsControllerTest < ActionController::TestCase
-#Author:Jailan
-test "should update interest" do
-  put :update, :id => interests(:one), :interest => {:name => 'hello', :description => 'desc2'}
-  assert_equal "hello", interests(:one).name 
-  assert_equal "desc2", interests(:one).description
 
+
+
+#Author: Jailan
+test "should update Interest GREEN" do
+  put :update, :id => interests(:one), :interest => {:name => 'MyString2'}
+ assert_equal "MyString2", Interest.find(interests(:one).id).name
+end
+
+#Author: Jailan
+test "shouldn't update Interest with a blank name GREEN" do
+  put :update, :id => interests(:one), :interest => {:description => 'lala'}
+ assert_not_equal "lala", Interest.find(interests(:one).id).name
 end
 
 
 
 
 #Author:Jailan
-  test "should block interest" do
+  test "should block interest RED" do
   put :toggle, :id => interests(:two), :interest => {:deleted => true}
   assert_equal true, assigns(:interest).deleted
 end
@@ -46,4 +53,5 @@ test "should have savechanges button RED" do
  assert_tag :tag =>"a" , :content =>"Restore"
 
 end
+
 end
