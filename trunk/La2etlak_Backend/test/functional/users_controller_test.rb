113cf8c6a904c3831428b1a8216794bbd714e5bc
yahiaelgamal <yahiaelgamal@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-05-13 00:18:13 +0000

    Update issue 71
    Added one test for the get http request 
    Fixed indentation problems

--- trunk/La2etlak_Backend/test/functional/users_controller_test.rb
+++ trunk/La2etlak_Backend/test/functional/users_controller_test.rb
@@ -6,7 +6,7 @@ class UsersControllerTest < ActionController::TestCase
     # assert true
    #end
 
-   test "should update" do
+  test "should update" do
     put :edit_info, id: 1, user: { name:"sdgssf", first_name:"uhsdgsf", last_name:"dshsfghsgd" }
     #assert_redirected_to post_path(assigns(:post))
     assert_template(expected = "show")
@@ -35,7 +35,7 @@ class UsersControllerTest < ActionController::TestCase
   
   
   #Author: Omar
-  	test "toggle view" do
+	test "toggle view" do
 		
 		user = User.new
 		user.email = "abc@abc.com"
@@ -44,65 +44,74 @@ class UsersControllerTest < ActionController::TestCase
 
 	end
 
-# Author: Omar
-		test "add selected interest in database" do
-		   user = User.create(:email => "a@abc.com")	
-		   interest = Array.new
-		   interest1 = Interest.create(:name => "interest 1")
-		   interest2 = Interest.create(:name => "interest 2")
-		   interest = [interest1.id , interest2.id]
-		   assert get(:addwithduplicates , {'interests' => interest} , {'user' => user} )
-		   
-		
-		
-		end
+  # Author: Omar
+  test "add selected interest in database" do
+     user = User.create(:email => "a@abc.com")	
+     interest = Array.new
+     interest1 = Interest.create(:name => "interest 1")
+     interest2 = Interest.create(:name => "interest 2")
+     interest = [interest1.id , interest2.id]
+     assert get(:addwithduplicates , {'interests' => interest} , {'user' => user} )
+     
+
+
+  end
 
   
   
     
-    #Author:Rana
-    test "get block story RED" do
-      this_interest = Interest.create :name => "Sports", :description => "hey sporty"
-      this_story= Story.new :title => "Story1", :interest_id => this_interest
-      this_story.interest = this_interest
-      this_story.save
-      this_user = User.create :name => "amr", :email => "amr@abc.com"
-      get :block_story, 'uid' => this_user.id, 'id' => this_story.id
-      assert_response :success 
-    end
+  #Author:Rana
+  test "get block story RED" do
+    this_interest = Interest.create :name => "Sports", :description => "hey sporty"
+    this_story= Story.new :title => "Story1", :interest_id => this_interest
+    this_story.interest = this_interest
+    this_story.save
+    this_user = User.create :name => "amr", :email => "amr@abc.com"
+    get :block_story, 'uid' => this_user.id, 'id' => this_story.id
+    assert_response :success 
+  end
 
-    #Author: Rana
-    test "get block interest RED" do
-      this_interest = Interest.create :name => "Sports", :description => "hey sporty"
-      this_story= Story.new :title => "Story1", :interest_id => this_interest
-      this_story.interest = this_interest
-      this_story.save
-      this_user = User.create :name => "amr", :email => "amr@abc.com"
-      get :block_interest, 'uid' => this_user.id, 'id' => this_story.id
-      assert_response :success 
-    end
+  #Author: Rana
+  test "get block interest RED" do
+    this_interest = Interest.create :name => "Sports", :description => "hey sporty"
+    this_story= Story.new :title => "Story1", :interest_id => this_interest
+    this_story.interest = this_interest
+    this_story.save
+    this_user = User.create :name => "amr", :email => "amr@abc.com"
+    get :block_interest, 'uid' => this_user.id, 'id' => this_story.id
+    assert_response :success 
+  end
 
-    #Author: Rana
-    test "get block friend RED" do
-      this_user = User.create :name => "amr", :email => "amr@abc.com"
-      my_friend = User.create :name => "ahmed", :email => "ahmed@abc.com"
-      get :block_friends_feed, 'id' => this_user.id, 'fname' => my_friend.name
-      assert_response :success 
-    end
+  #Author: Rana
+  test "get block friend RED" do
+    this_user = User.create :name => "amr", :email => "amr@abc.com"
+    my_friend = User.create :name => "ahmed", :email => "ahmed@abc.com"
+    get :block_friends_feed, 'id' => this_user.id, 'fname' => my_friend.name
+    assert_response :success 
+  end
 
-    #Author: Rana
-    test "get friend feed RED" do
-      this_user = User.create :name => "amr", :email => "amr@abc.com"
-      my_friend = User.create :name => "ahmed", :email => "ahmed@abc.com"
-      get :friends_feed, 'id' => this_user.id, 'fname' => my_friend.name
-      assert_response :success 
-    end
+  #Author: Rana
+  test "get friend feed RED" do
+    this_user = User.create :name => "amr", :email => "amr@abc.com"
+    my_friend = User.create :name => "ahmed", :email => "ahmed@abc.com"
+    get :friends_feed, 'id' => this_user.id, 'fname' => my_friend.name
+    assert_response :success 
+  end
 
-    #Author: Rana
-    test "get friend list RED" do
+  #Author: Rana
+  test "get friend list RED" do
       this_user = User.create :name => "amr", :email => "amr@abc.com"
       get :friends_list, 'id' => this_user.id
       assert_response :success
-   end
+  end
+
+  # Author: Yahia
+  test "connect social accounts RED" do
+    get :connect_social_accounts, {}, {:user_id => 1}
+    assert_response :success, "Get request should be successfull"
+
+  end 
+
+
 
 end
