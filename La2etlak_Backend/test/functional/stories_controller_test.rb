8cc5bf83bb4ad32ed5acced5afc5d84f55e865f2
elmenisy <elmenisy@gmail.com>
2012-05-13 01:26:09 +0000

    Update issue 93 Added mobile show view test, green.

--- La2etlak_Backend/test/functional/stories_controller_test.rb
+++ La2etlak_Backend/test/functional/stories_controller_test.rb
@@ -20,6 +20,27 @@ class StoriesControllerTest < ActionController::TestCase
   	end
   	assert_redirected_to :action => "mobile_show", :id2 => User.first.id
   end
+
+  # Author: Menisy
+  test "mobile show view" do
+  	story = Story.first
+  	comment = Comment.new
+  	comment.content = "lol"
+  	comment.user = User.first
+  	comment.story = story
+  	comment.save
+  	get :mobile_show, :id => Story.first.id , :id2 => User.first.id
+  	assert_select 'ul.nav' do
+  		assert_select 'li', 6				# 2 for the navbar + 4 four the 1st comment
+		end
+		assert_select 'div[class=story-component-box-mobile]',1
+		assert_select 'input[class=btn btn-large]',1
+		assert_select 'div[class=story-comment-box-mobile]',true
+		assert_select 'textarea[class=story-textarea-mobile]',1
+		assert_select 'title', {:count => 1, :text => "My Story Page!"},
+    "Wrong title or more than one title element"
+	end
+
   
   
   #Author : Omar
