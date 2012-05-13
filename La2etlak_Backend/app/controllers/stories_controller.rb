6c5e26f3cc1f039b9ee785cb78e01b86249af729
omar.saeed21 <omar.saeed21@gmail.com>
2012-05-13 16:20:34 +0000

    Update issue 79 modified method in controller and removed repeated method from model

--- La2etlak_Backend/app/controllers/stories_controller.rb
+++ La2etlak_Backend/app/controllers/stories_controller.rb
@@ -12,8 +12,10 @@ def show
    def get
 	id = params[:id]
 	@story = Story.find(id)
-	@likes = @story.likes
-	@dislikes = @story.dislikes
+	@likes = @story.liked
+	@dislikes = @story.disliked
+	@action1 = @story.check_like
+	@action2 = @story.check_dislike
 	render :layout => "mobile_template"
   end
   
