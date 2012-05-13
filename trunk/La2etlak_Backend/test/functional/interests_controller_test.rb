357ebef602569120337c618fefbbb3025ca94b45
jailan.salah@gmail.com <jailan.salah@gmail.com@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-05-13 04:59:20 +0000

    Update issue 59 Labels: Test-Ready
    Adding Tests

--- trunk/La2etlak_Backend/test/functional/interests_controller_test.rb
+++ trunk/La2etlak_Backend/test/functional/interests_controller_test.rb
@@ -1,7 +1,49 @@
 require 'test_helper'
 
 class InterestsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+#Author:Jailan
+test "should update interest" do
+  put :update, :id => interests(:one), :interest => {:name => 'hello', :description => 'desc2'}
+  assert_equal "hello", interests(:one).name 
+  assert_equal "desc2", interests(:one).description
+
+end
+
+
+
+
+#Author:Jailan
+  test "should block interest" do
+  put :toggle, :id => interests(:two), :interest => {:deleted => true}
+  assert_equal true, assigns(:interest).deleted
+end
+
+#Author:Jailan
+test "should restore interest RED" do
+  put :toggle, :id => interests(:one), :interest => {:deleted => false}
+  assert_equal false, assigns(:interest).deleted
+end
+
+
+
+#Author:Jailan
+
+ test "should have restore button when blocked RED" do
+ put :toggle, :id => interests(:two), :interest => {:deleted => true}
+ assert_select 'submit[class=btn btn-success]',1
+
+end
+#Author:Jailan
+test "should have block button when not blocked RED" do
+ put :toggle, :id => interests(:one), :interest => {:deleted => false}
+ assert_tag :tag =>"a" , :content =>"Block"
+
+end
+
+#Author:Jailan
+test "should have savechanges button RED" do
+ put :toggle, :id => interests(:two), :interest => {:deleted => true}
+ assert_tag :tag =>"a" , :content =>"Restore"
+
+end
 end
