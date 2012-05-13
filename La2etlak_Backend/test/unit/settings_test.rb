60b180dfdf003e8911e5ec9fe1cf1c91a3cb279e
gosgosakila <gosgosakila@gmail.com>
2012-05-13 08:42:55 +0000

    Update issue 69 Status: InProgress

--- La2etlak_Backend/test/unit/settings_test.rb
+++ La2etlak_Backend/test/unit/settings_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class SettingsTest < ActiveSupport::TestCase
   # author : Gasser
   test "admin should configure flags threshold" do
-    settings = Settings(:one)
+    settings = Settings.create!(:key=>"flags_threshold", :value=>20)
     old_value = settings.value
     settings.configure_flags_threshold (50)
     assert_equal settings.value, 50
@@ -12,7 +12,7 @@ class SettingsTest < ActiveSupport::TestCase
 
   # author : Gasser
   test "admin should not set the threshold with less than 20" do
-    settings = Settings(:one)
+    settings = Settings.create!(:key=>"flags_threshold", :value=>20)
     old_value = settings.value
     settings.configure_flags_threshold (10)
     assert_equals settings.value, 30 
@@ -21,12 +21,17 @@ class SettingsTest < ActiveSupport::TestCase
 
   # author : Gasser
   test "stories should be hidden after setting the threshold" do
-    settings = Settings(:one)
+    settings = Settings.create!(:key=>"flags_threshold", :value=>20)
     stories = Story.all
     settings.configure_flags_threshold (10)
     stories.each do |story|
       assert_boolean !story.hidden 
     end
   end 
-
+  # author : Gasser
+  test "admin should disable auto hiding" do 
+    settings = Settings.create!(:key=>"flags_threshold", :value=>20)
+    old_value = settings.value
+    settings.disable_auto_hiding
+  end   
 end
