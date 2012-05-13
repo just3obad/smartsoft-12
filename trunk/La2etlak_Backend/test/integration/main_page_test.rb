cb24ced38fc632febf4dec1ace0f48b5d893995e
mina.workspace@gmail.com <mina.workspace@gmail.com@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-05-13 01:47:56 +0000

    Update issue 60 Labels: Test-Ready
    Tests for the admin main page [RED]

---
+++ trunk/La2etlak_Backend/test/integration/main_page_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class MainPageTest < ActionDispatch::IntegrationTest
+  #this test is to make sure that the route is not lost 
+  test "AdminMainPageRoute" do
+        assert_routing '/admins/index', { :controller => "admins", :action => "index"}
+      end
+  #this test is to make sure that the main page does exist and i get a 200OK response     
+  test "AdminMainPage" do
+        get "/admins/index"
+        assert_response :success
+     end
+  #this test is for testing the existance of feed div
+  test "AutoCompleteResultDiv RED" do
+        get "/admins/index"
+       assert_select "div[id=feeds]"
+     end
+  #test get_feed
+  test "get feeds RED" do
+        Interest.create!(:name => "Music", :description => "Music washes away from the soul the dust of everyday life", :image => "http://data.whicdn.com/images/15521117/hamster-music_165451585_thumb.jpg")
+        @feeds = Admin.get_feed
+        assert_equal(@feeds.length,1)
+      end
+end
