52a2c24a62f76725ab0ccc6dc9ce2210c6280f41
gosgosakila@gmail.com <gosgosakila@gmail.com@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-05-13 08:31:43 +0000

    Update issue 68 Fixing syntax errors

--- trunk/La2etlak_Backend/test/unit/settings_test.rb
+++ trunk/La2etlak_Backend/test/unit/settings_test.rb
@@ -3,25 +3,30 @@ require 'test_helper'
 class SettingsTest < ActiveSupport::TestCase
   # author : Gasser
   test "admin should configure flags threshold" do
-    settings = Settings.first
-    assert_difference ('settings.value',30) do
-      settings.configure_flags_threshold (50)
-    end
+    settings = Settings(:one)
+    old_value = settings.value
+    settings.configure_flags_threshold (50)
+    assert_equal settings.value, 50
   end
+
+
   # author : Gasser
-  test "admin should not set the threshold with less than 20"
-  settings = Settings.first
-    assert_difference ('settings.value',20, "You can't put the threshold with less than 20") do
-      settings.configure_flags_threshold (10)
-    end
+  test "admin should not set the threshold with less than 20" do
+    settings = Settings(:one)
+    old_value = settings.value
+    settings.configure_flags_threshold (10)
+    assert_equals settings.value, 30 
   end
+
+
   # author : Gasser
-  test "stories should be hidden after setting the threshold"
-    settings = Settings.first
-    stories = stories.all
+  test "stories should be hidden after setting the threshold" do
+    settings = Settings(:one)
+    stories = Story.all
     settings.configure_flags_threshold (10)
     stories.each do |story|
-    assert_boolean !story.hidden 
+      assert_boolean !story.hidden 
     end
-  end
+  end 
+
 end
