1ebf3909568fc5a47aae245cb5d476b14d813169
rana.helal92 <rana.helal92@gmail.com>
2012-05-13 16:30:54 +0000

    Update Issue 92: adding methods to controller and removing my old methods

--- La2etlak_Backend/app/controllers/users_controller.rb
+++ La2etlak_Backend/app/controllers/users_controller.rb
@@ -139,86 +139,6 @@ end
     respond_with(@users = User.all)
   end
 
-#This method blocks interests from the user's feed by changing the is_blocked attribute to true. The method depends on another method that gets the list of stories under these interests. The inputs to this method are the user_id and story_id
-   
-  def block_interest
-  @user_id = params[:uid]
-  @story_id = params[:id]
-  @interest_id = Story.find_by_id(@story_id).interest_id
-  @interest_list = Array.new
-   interest =  BlockInterest.find_by_user_id_and_interest_id(@user_id,@interest_id)
-   if interest.nil?
-      BlockInterest.create!(:user_id => @user_id, :interest_id => @interest_id) 
-      render text: "interest blocked successfully"
-   else 
-      render text: "interest already blocked"    
-   end
-   #log file
-  @username = User.find_by_id(@user_id).name
-  @storytitle = Story.find_by_id(@story_id).title
-  @interesttitle = Interest.find_by_id(@interest_id).name
-  @message = "#{@username}blocks interest#{@interesttitle}"
-  Log.create!(loggingtype: 3,user_id_1: @user_id,user_id_2: nil, admin_id: nil, story_id: nil, interest_id: @interest_id, message: @message)
-  end
-
-  #This method takes a story id as input and blocks its story by setting the is_blocked attribute to true. If the story is not found, it responds with a message that the story is not found.
-
-  def block_story
-   @user_id = params[:uid]
-   @story_id = params[:id]
-   story = BlockStory.find_by_user_id_and_story_id(@user_id,@story_id)
-   if story.nil?
-      BlockStory.create!(:user_id => @user_id, :story_id => @story_id)     
-      render text: "Story blocked successfully"
-   else 
-      render text: "Story already blocked"
-   end
-   #for log file
-   @username = User.find_by_id(@user_id).name
-   @storytitle = Story.find_by_id(@story_id).title
-   @message = "#{@username}blocks story with title:#{@storytitle}" 
-   Log.create!(loggingtype: 2,user_id_1: @user_id,user_id_2: nil, admin_id: nil, story_id: @story_id, interest_id: @interest_id, message: @message)
-  end
-
-
-  #This method gets the stories of a friend through method get_friends_stories() and converts them to a json file. It takes as input the user_id.
-
-  def friends_feed
-    @user_id = params[:id]
-    @friend_id = params[:fid]
-    @friend_stories = User.find_by_id(@user_id).get_one_friend_stories(@friend_id)
-    respond_with(@friend_stories)
-  end
-
-
-  #This method blocks feeds from this friend by setting is_blocked attribute to true. The method depends on another method that gets the stories belonging to a friend. It takes as an input the user_id.
-
-  def block_friends_feed
-    @user_id = params[:id]
-    @friend_id = params[:fid]
-    sender = Friend.find_by_sender_and_receiver_and_status(@user_id, @friend_id,1)
-    if !sender.nil?
-      sender.status = 3
-      sender.save
-      render text: "friend is blocked successfully sender"
-    else 
-      receiver = Friend.find_by_sender_and_receiver_and_status(@friend_id,@user_id,1)
-      if !receiver.nil?
-         receiver.status = 4
-         receiver.save
-         render text: "friend is blocked successfully receiver"
-      else 
-         render text: "error while blocking"
-      end
-    end
-    #for log file    
-    @username = User.find_by_id(@user_id).name
-    @user2 = User.find_by_id(@friend_id).name
-    @message = "#{@username}blocks the feed of#{@user2}" 
-    Log.create!(loggingtype: 0,user_id_1: @user_id,user_id_2: @friend_id, admin_id: nil, story_id: nil, interest_id: nil, message: @message)
-  end
-
-
  # Author: Yahia
   def connect_social_accounts
     # render :layout => "mobile_template"
@@ -313,6 +233,24 @@ end
     # @user = User.find(session[:user_id])
   end 
 
+  #method that calls the method in model to block story and renders the view
+  #Author: Rana
+  def block_story
+    @user = current_user
+    @story_id = params[:id]
+    @story = Story.find_by_id(@story_id)
+    @text = @user.block_story1(@story)
+    render @text
+  end
 
+  #method that calls the method in model to block interest and renders the view
+  #Author: Rana
+  def block_interest
+    @user_id = current_user
+    @story_id = params[:id]
+    @story = Story.find_by_id(@story_id)
+    @text = current_user.block_interest1(@story)
+    render @text
+  end
 
 end
