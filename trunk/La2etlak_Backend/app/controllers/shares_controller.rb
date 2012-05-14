960c5e52e61288afb68c496da27017d6477ccf89
mina.adel.fahmy@gmail.com <mina.adel.fahmy@gmail.com@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-05-14 19:12:20 +0000

    Update issue 77 Status: InProgress Labels: Code-Ready Scenario-Ready Doc-Ready
    Added sharers for facebook, twitter, and La2etlak along with with flashes for La2etlak and the documentation

--- trunk/La2etlak_Backend/app/controllers/shares_controller.rb
+++ trunk/La2etlak_Backend/app/controllers/shares_controller.rb
@@ -1,2 +1,21 @@
 class SharesController < ApplicationController
+  #$$$$$$$$$$$$$$$$ Mina Adel $$$$$$$$$$$$$$$$
+  #Here I create a new entry in the Share table, create a flash the states that the share was successful (or not) and pass the flash on to
+  #the stories/:id page (which makes the flash show up in that page)
+  #No view is assigned to this method
+  def user_share_story
+    @user = current_user
+    @story_id = params[:id]
+    usershare = Share.new(:user_id => @user.id, :story_id => @story_id)
+    if(usershare.save)
+      flash.now[:notice] = "You have successfully shared this story, it will now appear on your friends' feeds"
+      redirect_to("/stories/"+@story_id+"/get", :flash => flash)
+    else 
+      flash.now[:error] = "Story not successfully saved, please try again later"
+      redirect_to("/stories/"+@story_id+"/get", :flash => flash)
+  end
+
+  #render :layout => "mobile_template"
+  end
+  #$$$$$$$$$$$$$$$$ Mina Adel $$$$$$$$$$$$$$$$
 end
