938f1fd15a121b3c2614f918c66826c74b4573b1
kemooo17 <kemooo17@gmail.com>
2012-05-14 17:28:13 +0000

    Update issue 99 Added current_user attribute

--- La2etlak_Backend/app/controllers/likedislikes_controller.rb
+++ La2etlak_Backend/app/controllers/likedislikes_controller.rb
@@ -4,15 +4,11 @@ respond_to :html,:json
 
 #this method is called when a user presses thump/up action the mobile App , it adds a new record to Likedislike table with the User_id  and story_id ,, if the user had thumped this story b4 and tried to thump it again the old record is Deleted and a new Record is added.
 def thumb
-action = params[:action]  
-user_id = params[:uid]
-story_id = params[:sid]
-user = User.find(user_id)
-story = Story.find(story_id)
-f =  user.thumb_story(story,action)
-respond_to do |format|
-   format.json { render json: f}
- end
+	action = params[:action]  
+	story_id = params[:sid]
+	user = current_user
+	story = Story.find(story_id)
+	user.thumb_story(story,action)
 
   end
 end 
