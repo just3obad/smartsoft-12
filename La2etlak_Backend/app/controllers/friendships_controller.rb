8f970703f4505a267804e23fb5ec06bd039135c2
yahiaelgamal <yahiaelgamal@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-05-14 20:52:58 +0000

    Update issue 74
    Migrated the current code to use the new authentication system

--- La2etlak_Backend/app/controllers/friendships_controller.rb
+++ La2etlak_Backend/app/controllers/friendships_controller.rb
@@ -2,17 +2,17 @@ class FriendshipsController < ApplicationController
 
   # Author: Yahia 
   def index
-    current_user = User.find(session[:user_id])
-    @friends = current_user.friends
-    @pending_invited_by = current_user.pending_invited_by
-    @pending_invited = current_user.pending_invited
+    @user = current_user
+    @friends = @user.friends
+    @pending_invited_by = @user.pending_invited_by
+    @pending_invited = @user.pending_invited
   end
 
   # Author: Yahia 
   def create
-    current_user = User.find(session[:user_id])
-    @friend = User.find(params[:user_id])
-    @friendship_created = current_user.invite(@friend)
+    @user = current_user
+    @friend = User.find(params[:friend_id])
+    @friendship_created = @user.invite(@friend)
     if @friendship_created
       # flash.now[:notice] = "Friendship invitation created  #{@friend.email}"
       render layout: 'mobile_template', text: "Friendship invitation created  #{@friend.email}"
@@ -21,20 +21,20 @@ class FriendshipsController < ApplicationController
 
   # Author: Yahia 
   def approve
-    current_user = User.find(session[:user_id])
-    @friend = User.find(params[:user_id])
-    @friendship_approved = current_user.approve(@friend)
-    @friends = current_user.friends
-    @pending_invited_by = current_user.pending_invited_by
+    @user = current_user
+    @friend = User.find(params[:friend_id])
+    @friendship_approved = @user.approve(@friend)
+    @friends = @user.friends
+    @pending_invited_by = @user.pending_invited_by
     #flash.now[:notice] = "Friendship approved #{@friend.email} a t approuve"
     render layout: 'mobile_template', text: "Friendship approved  #{@friend.email}"
   end
 
   # Author: Yahia 
   def remove
-    current_user = User.find(session[:user_id])
-    @friend = User.find(params[:user_id])
-    @friendship = current_user.send(:find_any_friendship_with, @friend)
+    @user = current_user
+    @friend = User.find(params[:friend_id])
+    @friendship = @user.send(:find_any_friendship_with, @friend)
     if @friendship
       @friendship.delete
       @removed = true
