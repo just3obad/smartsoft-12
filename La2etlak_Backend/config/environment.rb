682ce5bd28819ec3205592e2a3d31bb6e75d782e
mina.workspace <mina.workspace@gmail.com>
2012-05-14 04:20:48 +0000

    Update issue 60
    Updating environment.rb

--- La2etlak_Backend/config/environment.rb
+++ La2etlak_Backend/config/environment.rb
@@ -1,6 +1,6 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
-
+include ActionView::Helpers::DateHelper
 # Initialize the rails application
 NokiaRuby::Application.initialize!
 
