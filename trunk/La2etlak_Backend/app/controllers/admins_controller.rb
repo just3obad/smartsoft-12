ef9f002d6a3c47df136521c097c1109eaa2d6b46
lydia.sidhom@gmail.com <lydia.sidhom@gmail.com@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-05-14 22:39:10 +0000

    Update issue 83
    Adding the view for the search page

--- trunk/La2etlak_Backend/app/controllers/admins_controller.rb
+++ trunk/La2etlak_Backend/app/controllers/admins_controller.rb
@@ -1,6 +1,9 @@
 class AdminsController < ApplicationController
 
   def search
+    $usersSearch = "true"
+    $storiesSearch = "true"
+    $interestsSearch = "true"
     if params[:autocomplete][:query].length > 0
       $users = Admin.search(params[:autocomplete][:query])
       if $users.length == 0
@@ -9,10 +12,36 @@ class AdminsController < ApplicationController
     else
       $users = "Please enter query into the search bar."
     end
+    
+    #these attributes are added this way temporarilly until Mostafa comitts his
+    #work so that I can work on the view of the search
+    $interests = Interest.all
+    $stories = Story.all
 
     respond_to do |format|
       format.html #index.html.erb
       format.json { render json: $users }
+    end
+  end
+  
+  #Author: Lydia
+  def filter
+    $usersSearch = params[:users]
+    $storiesSearch = params[:stories]
+    $interestsSearch = params[:interests]
+     render :template => 'admins/search'
+  end
+  
+  #Author: Lydia
+  def all_results
+    if params[:type] == "1"
+      @results = $users
+    end
+    if params[:type] == "2"
+      @results = $stories
+    end
+    if params[:type] == "3"
+      @results = $interests
     end
   end
   
