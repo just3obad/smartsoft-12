8695f4d1d4a399ec7b8eefb37fb57fb45807a36e
gosgosakila <gosgosakila@gmail.com>
2012-05-15 20:43:03 +0000

    Update issue 68 Labels: Code-Ready Moved the UI from show to index of the settings page and modified the routes

--- La2etlak_Backend/app/controllers/users_controller.rb
+++ La2etlak_Backend/app/controllers/users_controller.rb
@@ -45,31 +45,6 @@ class UsersController < ApplicationController
 		end
 	end
 
-  # Action to be called from the connet_to_social network view
-  # which redirects to the facebook api using koala
-  # Author: Menisy
-  def authenticate_facebook_init
-    path = Koala::Facebook::OAuth.new.url_for_oauth_code(:callback => "http://localhost:3000/fb/done/")  
-    redirect_to path
-  end
-
-  # Action to be called in the call back url after authentication take place
-  # Author: Menisy
-  def authenticate_facebook_done
-    token = Koala::Facebook::OAuth.new("http://localhost:3000/fb/done/").get_access_token(params[:code]) if params[:code]
-    fb_account = FacebookAccount.new(:auth_token => token,:auth_secret => "1")
-    fb_account.user = current_user
-    graph =  Koala::Facebook::API.new(token)
-    user = graph.get_object("me")
-    if fb_account.save
-      flash[:notice] = "Facebook account added successfully green" +user.to_s
-      redirect_to :controller => "stories", :action => "mobile_show", :id => 1
-    else
-      flash[:notice] = "Facebook account was not added red" + user.to_s
-      redirect_to :controller => "stories", :action => "mobile_show", :id => 1
-    end
-  end
-
 	def resetPassword
 		@user = current_user
 		newpass = @user.resetPassword
@@ -100,33 +75,24 @@ class UsersController < ApplicationController
 def feed
 
  	user = current_user
-	int_name = params[:interest]
-	#if(user.user_add_interests == [])
-	#	@stories = Story.get_stories_ranking_last_30_days
-	#else
-
- 	
+ 	int_name = params[:interest]
    	 if(int_name)
     	 @stories = user.get_feed(int_name)
 	 else
 	 @stories =  user.get_feed("null")
 	end
-	#@stories = @stories + user.get_friends_stories
-
 	 respond_to do |format|
     	 format.json { render json: @stories }
    	 # Author : Mina Adel
   	 format.html { render :layout => "mobile_template"}
   	 #
-	#end
  end
-   
 end
 ########################
 
 
-#$$$$$$$$$$$$$$$$$$$$$$$$$$$ Author Omar  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-#this method return current user interests and all interests on the system and render to mobile_template (toggle view )
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$ Author Omar $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+#this method return current user interests and all interests on the system and render to mobile_template (toggle view 
 def toggle
 @user = current_user
 @user_interests = @user.user_interests
@@ -141,15 +107,15 @@ id = params[:id]
 user.toggle_interests(id)
  redirect_to "/mob/toggle"
 end
+end
+render :layout => "mobile_template"
+end
 
 #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 
  #Author: Christine
- # @user is the user that we will show
- # @logs contain recent activity of the user in the last 30 days
- # @friends contain the list of users who are friends with our user
- # @interests is the list of interests that the user is subscribed to
  def show
+    @user = User.find(params[:id])
     @user = User.find(params[:id])
     @logs = @user.get_recent_activity(30.days.ago)
     @friends = @user.friends
@@ -161,17 +127,38 @@ end
   end
 
 
+   def new
+    @user = User.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @user }
+    end
+    end
+
+  def create
+    @user = User.new(params[:user.downcase])
+    respond_to do |format|
+      if @user.save
+        @user.haccount = Haccount.new(:email => @user.email.downcase,
+                                      :password=>params[:password.downcase], :user_id => @user.id)
+        if @user.haccount.generateVerificationCode?
+          Emailer.resend_code(@user.haccount).deliver
+          @uLog = UserLogIn.create(:user_id => @user.id)
+          format.json { render json: @user, status: :created }
+        end
+      else
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
 
   def index
     respond_with(@users = User.all)
   end
 
-=begin
-  This is the method responsible of redirecting  the user to
-  the connect_social_account web page
-  Author: Yahia
-=end
+ # Author: Yahia
   def connect_social_accounts
     # render :layout => "mobile_template"
     # FIXME  
@@ -182,22 +169,76 @@ end
   end 
   
 
-  
+=begin
+  This is the first phase of the OAuth phase that is required by twitter.
+  In this phase, first a new Consumer gets created which is basically 
+  a client that represents our app talking to twitter. 
+
+  The client asks twitter for a request_token, from which a URL will 
+  be generated. The callback url should be changed to the proper server 
+  URL. 
+
+  The browser will be redirected to the generated authorization URL. After
+  that, twitter will redirect the user back to our app. 
+
+  Author: Yahia
+=end
+  def generate_request_token
+    #FIXME FOR THE SAKE OF TESTING
+    session[:user_id] = 1
+
+    #FIXME change IP 
+    request_token = User.twitter_consumer.get_request_token(:oauth_callback => 
+                "http://127.0.0.1:3000/users/twitter/generate_access_token")
+
+    url = request_token.authorize_url
+    #puts 'URL IS ' + url
+    redirect_to(url)
+  end 
+
+=begin
+  This is the second phase of authentication. In this phase, the user should have 
+  authenticated our app through twitter. Then we use the request token and secret 
+  token from that exact user to get our access tokens from twitter. Through the 
+  access token, we can get the feeds or tweet on behalf of the user. 
+
+  This is done by simply
+  requesting the access token by the oauth_token and oauth_verifier which twitter
+  put in the params array. Through this access token the twitter accoun can be made
+  thorugh which the system fetches tweets.
+
+  Author: Yahia
+=end 
+  def generate_access_token
+    # FIXME FOR THE SAKE OF TESTING
+    session[:user_id] = 1
+    @user = User.find(session[:user_id])
+    request_token = OAuth::RequestToken.new(User.twitter_consumer,
+                    params["oauth_token"], params["oauth_verifier"])
+
+    t_account = @user.create_twitter_account(request_token)
+
+    unless t_account.new_record?
+      render(:layout => 'mobile_template', 
+              :text => "User #{ session[:user_id] }" + 
+                      "created a new twitter account")
+    else 
+      render(:layout => 'mobile_template', 
+              :text => 'Something wrong, couldn\'t save account')
+    end 
+  end 
+
+  # Author: Yahia
+  def search_members
+    # FIXME 
+    # session[:user_id] = 1
+    # string = params[:query]
+    # @user = User.find(session[:user_id])
+  end 
   #Author: Bassem
-  #The method takes the user Id as a parameter, passes it to the model where the user is found
-  #and his deactivated attribute is set to true
   def deactivate
-    @user = User.find(params[:id])
-    @user.deactivate_user(params[:id])
-    redirect_to(:action => 'show', :id => @user.id)
-  end
-  #Author: Bassem
-  #The method takes the user Id as a parameter, passes it to the model where the user is found
-  #and his deactivated attribute is set to false
-  def activate
-    @user = User.find(params[:id])
-    @user.activate_user(params[:id])
-    redirect_to(:action => 'show', :id => @user.id)
+    User.find_by_id(params[:id]).deactivated = true
+    respond_with true
   end
 
   #method that calls the method in model to block story and renders the view
@@ -207,8 +248,7 @@ end
     @story_id = params[:id]
     @story = Story.find_by_id(@story_id)
     @text = @user.block_story1(@story)
-    flash[:notice] = "#{@text}"
-    redirect_to action: "feed"
+    render @text
   end
 
   #method that calls the method in model to block interest and renders the view
@@ -218,8 +258,7 @@ end
     @story_id = params[:id]
     @story = Story.find_by_id(@story_id)
     @text = @user.block_interest1(@story)
-    flash[:notice] = "#{@text}"
-    redirect_to action: "feed"
+    render @text
   end
 
   #The method that calls the method in the model to block friend feed and renders the view
@@ -229,25 +268,7 @@ end
       @friend_id = params[:id]
       @friend = User.find_by_id(@friend_id)
       @text = @user.block_friends_feed1(@friend) 
-      flash[:notice] = "#{@text}"
-      redirect_to action: "feed"
-  end
-
-  #The method that calls method in the model to get friend stories and renders the view
-  #Author: Rana
-  def friends_feed
-      @user = current_user
-      @friend_id = params[:id]
-      @my_friend_stories = @user.get_one_friend_stories(@friend_id)
-      render layout:"mobile_template", template: "users/friend_feed"
-  end
-
-  #The method that calls method in the model to get the user's friends' email and renders the view
-  #Author: Rana
-  def friends_list
-      @user = current_user
-      @my_friends = @user.get_friends_email
-      render layout: "mobile_template", template: "users/friends_list"
+      render @text
   end
 
 
@@ -269,6 +290,7 @@ end
    Case 3 : If the firstname is greater than 20 chars, he will be notified of this error
    Case 4 : If the lastname is greater than 20 chars, he will be notified of this error
    Case 5 : If there is a password missmatch, he will be notified of this error
+   Case 6 : If the pasword is less than 4 chars, he will be notified of this error
 =end
   def update
    @user = current_user
