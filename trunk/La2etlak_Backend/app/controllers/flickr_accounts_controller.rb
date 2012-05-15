9e5501dc8129b5f66ddc53833565f04ef8300f50
just3obad@gmail.com <just3obad@gmail.com@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-05-15 18:52:15 +0000

    update issue 100

---
+++ trunk/La2etlak_Backend/app/controllers/flickr_accounts_controller.rb
@@ -0,0 +1,50 @@
+require 'flickraw'
+class FlickrAccountsController < ApplicationController
+
+  
+
+  API_KEY='443b0e6f88e26df854bdf0d7f7a8c1d5'
+  SHARED_SECRET='ff3e3ebe8f31e039'
+
+  #@callback_url='http://localhost:3000/flickr_accounts/callback'
+
+  FlickRaw.api_key=API_KEY
+  FlickRaw.shared_secret=SHARED_SECRET
+
+  def auth
+  flickr = FlickRaw::Flickr.new
+  token = flickr.get_request_token(:oauth_callback => ('http://127.0.0.1:3000/users/flickr/callback'))
+  # You'll need to store the token somewhere for when the user is returned to the callback method
+  # I stick mine in memcache with their session key as the cache key
+
+  @auth_url = flickr.get_authorize_url(token['oauth_token'], :perms => 'read')
+  # Stick @auth_url in your template for users to click
+  redirect_to(@auth_url)
+  puts "End of auth"
+  end
+
+   def callback
+   	
+  flickr = FlickRaw::Flickr.new
+
+  token = # Retrieve from cache or session etc - see above
+  oauth_token = params[:oauth_token]
+  oauth_verifier = params[:oauth_verifier]
+
+  raw_token = flickr.get_oauth_token(request_token['oauth_token'], request_token['oauth_token_secret'], oauth_verifier)
+  # raw_token is a hash like this {"user_nsid"=>"92023420%40N00", "oauth_token_secret"=>"XXXXXX", "username"=>"boncey", "fullname"=>"Darren%20Greaves", "oauth_token"=>"XXXXXX"}
+  # Use URI.unescape on the nsid and name parameters
+
+  oauth_token = raw_token["oauth_token"]
+  oauth_token_secret = raw_token["oauth_token_secret"]
+
+  # Store the oauth_token and oauth_token_secret in session or database
+  #   and attach to a Flickraw instance before calling any methods requiring authentication
+
+  # Attach the tokens to your flickr instance - you can now make authenticated calls with the flickr object
+  flickr.access_token = oauth_token
+  flickr.access_secret =[oauth_token_secret]
+
+   end
+
+end
