e3a2e8633b0cd19b634369737db624824e194d62
kelbhaey@gmail.com <kelbhaey@gmail.com@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-05-16 20:00:11 +0000

    Update issue 67 Status: InProgress modified the routes to have mob at the begining

--- trunk/La2etlak_Backend/config/routes.rb
+++ trunk/La2etlak_Backend/config/routes.rb
@@ -130,11 +130,11 @@ NokiaRuby::Application.routes.draw do
   # $$$$$$$$$$$$ MENISY END $$$$$$$$$$$$$$$$$$$$$$
 
     # $$$$$$$$$$$$$$ Khaled $$$$$$$$$$$$$$$$$$$$$$$$$
-  match "stories/recommend_story_mobile_show/:sid" => "stories#recommend_story_mobile_show"
-  match "stories/recommend_success_mobile_show/:sid" => "stories#recommend_success_mobile_show"
-  match "/stories/liked_mobile_show/:id" => "stories#liked_mobile_show", :as => :like
-  match "/stories/disliked_mobile_show/:id" => "stories#disliked_mobile_show", :as => :dislike
-  match "/users/get_friends_email_mobile_show" => "users#get_friends_email_mobile_show", :as => :email
+
+  match "mob/stories/recommend_success_mobile_show/:sid" => "stories#recommend_success_mobile_show"
+  match "mob/stories/recommend_story_mobile_show/:sid" => "stories#recommend_story_mobile_show"
+  match "mob/stories/liked_mobile_show/:id" => "stories#liked_mobile_show", :as => :like
+  match "mob/stories/disliked_mobile_show/:id" => "stories#disliked_mobile_show", :as => :dislike
    # $$$$$$$$$$$$$$ Khaled $$$$$$$$$$$$$$$$$$$$$$$$$
 
    #$$$$$$$$$$$$$$$$$$$$$ BASSEM !! $$$$$$$$$$$$$$$$$$
