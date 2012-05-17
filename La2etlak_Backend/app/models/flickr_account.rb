4f2452e3d57a26b37b1a3104b9cb450e3ebd9b34
just3obad <just3obad@gmail.com>
2012-05-17 09:23:43 +0000

    update issue 100

--- La2etlak_Backend/app/models/flickr_account.rb
+++ La2etlak_Backend/app/models/flickr_account.rb
@@ -1,4 +1,4 @@
 class FlickrAccount < ActiveRecord::Base
   attr_accessible :consumer_key, :secret_key, :user_id
-  belongs_to :user_id, class_name: "User" 
+  belongs_to :user, class_name: "User" 
 end
