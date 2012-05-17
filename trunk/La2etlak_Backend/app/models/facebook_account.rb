411eebadd548ad66634f4cad51d5957018c26305
elmenisy@gmail.com <elmenisy@gmail.com@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-05-17 21:33:08 +0000

    Update issue 97 Fixed types for facebook returned feeds Fixed indentation Added prints to check for the method being called on the server trace

--- trunk/La2etlak_Backend/app/models/facebook_account.rb
+++ trunk/La2etlak_Backend/app/models/facebook_account.rb
@@ -15,38 +15,49 @@ class FacebookAccount < ActiveRecord::Base
 	# Author: Menisy
   def get_feed
   	begin
-  	new_token = Koala::Facebook::OAuth.new("http://localhost:3000/fb/done/").exchange_access_token(self.auth_token)
-  	puts(">>>>>>>>>>>>>>>>>>>>>>>>>>\n\n\n"+new_token+"\n\n\n\n")
-  	self.auth_token = new_token.to_s
-  	self.auth_secret = self.auth_secret.to_i+1
-  	self.save!
- 	  graph = Koala::Facebook::API.new(new_token)
-  	g = graph.get_connections("me","home")
-  	feed = Array.new
-  	pic = ""
-  	content = ""
-  	if g
-  		g.each do |s| 
-				title = s["from"]["name"]+" shared:"
-				if s["type"] == ("photo" || "swf" || ("link" && s["picture"]))
-    			content = (s["picture"] || s["link"])
-      	end 
- 				s["message"]
- 				if s["caption"]
-    			content = content + s["caption"]
- 				end
- 				story = Story.new
- 				story.title = title
- 				story.content = content
- 				story.deleted = false
- 				story.hidden = false
- 				feed << story
- 			end
-  		return feed
-		end	
-	  rescue
-	  	self.destroy
+      p("\n\n\n>>>>>>>>>>>>>>>>>>>>>>>>>> \n\n\ngetting feed\n\n")
+      new_token = Koala::Facebook::OAuth.new("http://localhost/fb/done/").exchange_access_token(self.auth_token)
+      puts(">>>>>>>>>>>>>>>>>>>>>>>>>>\n\n\n"+new_token+"\n\n\n\n")
+      self.auth_token = new_token.to_s
+      self.auth_secret = self.auth_secret.to_i+1
+      self.save!
+      graph = Koala::Facebook::API.new(new_token)
+      g = graph.get_connections("me","home")
+      feed = Array.new
+      pic = ""
+      content = ""
+      media = ""
+      if g
+        g.each do |s| 
+          title = s["from"]["name"]+" shared:"
+          if s["type"] == "photo"
+            title = title+"\n"+s["message"] if s["message"]
+            media = s["picture"]
+            if s["caption"]
+              content = content + s["caption"]
+            end
+          elsif s["type"] == "video"
+            title = title+"\n"+s["name"] if s["name"]
+            media_link = s["link"]
+            content = s["message"] if s["message"]
+            content = content + s["description"] if s["description"]
+          else
+            next
+          end
+          story = Story.new
+          story.title = title
+          story.media_link = media
+          story.content = content
+          story.deleted = false
+          story.hidden = false
+          feed << story
+        end
+        p("\n\n\n\n\n\n\n\n!!!!!!!!!"+feed+"\n\n\n\n")
+        return feed
+      end	
+    rescue
+      self.destroy
       return []
-  	end
+    end
   end
 end
