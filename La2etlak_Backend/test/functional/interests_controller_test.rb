13191146b5bfe12e5f3f678c75dbb4f86114f043
jailan.salah <jailan.salah@gmail.com>
2012-05-17 11:29:51 +0000

    Update issue 58
    functional tests all passed

--- La2etlak_Backend/test/functional/interests_controller_test.rb
+++ La2etlak_Backend/test/functional/interests_controller_test.rb
@@ -3,11 +3,27 @@ require 'test_helper'
 class InterestsControllerTest < ActionController::TestCase
 
 
+  #Author Jailan
+  #checking route is not lost 
+  test "Interest main page routes GREEN" do
+     interest = Interest.first
+     get :show, :id => interest.id
+     assert_routing '/interests/'+interest.id.to_s, {:controller =>"interests", :action => "show", :id=> "1"}
+    end
+
+  #Author Jailan
+  #this test is to make sure that the main page does exist and i get a 200OK response     
+  test "Interest Main Page GREEN" do
+       get :show, :id => 1
+      assert_response :success
+    end
+
+ 
 
 #Author: Jailan
 test "should update Interest GREEN" do
-  put :update, :id => interests(:one), :interest => {:name => 'MyString2'}
- assert_equal "MyString2", Interest.find(interests(:one).id).name
+  put :update, :id => interests(:two), :interest => {:name => 'MyString2'}
+ assert_equal "MyString2", Interest.find(interests(:two).id).name
 end
 
 #Author: Jailan
@@ -16,17 +32,26 @@ test "shouldn't update Interest with a blank name GREEN" do
  assert_not_equal "lala", Interest.find(interests(:one).id).name
 end
 
+#Author: Jailan
+test "shouldn't update Interest when it's blocked GREEN" do
+  put :toggle, :id => interests(:two), :interest => {:deleted => true}
+  assert_equal true, assigns(:interest).deleted
+  put :update, :id => interests(:two), :interest => {:name => 'lala'}
+
+ assert_not_equal "lala", Interest.find(interests(:two).id).name
+end
+
 
 
 
 #Author:Jailan
-  test "should block interest RED" do
+  test "should block interest GREEN" do
   put :toggle, :id => interests(:two), :interest => {:deleted => true}
   assert_equal true, assigns(:interest).deleted
 end
 
 #Author:Jailan
-test "should restore interest RED" do
+test "should restore interest GREEN" do
   put :toggle, :id => interests(:one), :interest => {:deleted => false}
   assert_equal false, assigns(:interest).deleted
 end
@@ -35,23 +60,41 @@ end
 
 #Author:Jailan
 
- test "should have restore button when blocked RED" do
- put :toggle, :id => interests(:two), :interest => {:deleted => true}
- assert_select 'submit[class=btn btn-success]',1
-
+ test "should have restore button when blocked GREEN" do
+ put :toggle, :id => 2, :interest => {:deleted => false}
+ get :show, :id => 2
+assert_select 'div[id = "restore"]'
 end
 #Author:Jailan
-test "should have block button when not blocked RED" do
- put :toggle, :id => interests(:one), :interest => {:deleted => false}
- assert_tag :tag =>"a" , :content =>"Block"
+test "should have block button when not blocked GREEN" do
+ put :toggle, :id => 1, :interest => {:deleted => true}
+ get :show, :id => 1
+assert_select 'div[id = "block"]'
 
 end
 
 #Author:Jailan
-test "should have savechanges button RED" do
- put :toggle, :id => interests(:two), :interest => {:deleted => true}
- assert_tag :tag =>"a" , :content =>"Restore"
+  test "should have an update form to fill with save changes button GREEN" do
 
-end
+     get :show, :id => 1
+    assert_select 'div[id = "update"]'
+
+  end
+
+#Author:Jailan
+  test "should have a status form showing the Interests Status GREEN" do
+     get :show, :id => 1
+    assert_select 'div[id = "status"]'
+
+  end
+
+
+#Author:Jailan "Testing the browse plugin"
+  test "should have an image saved  after creating GREEN" do
+     get :show, :id => 1
+    assert_select 'div[id = "image"]'
+
+  end
+
 
 end
