c9b997cc0d0afea1edb1c252cb296b2234be75cd
just3obad@gmail.com <just3obad@gmail.com@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-05-17 09:23:16 +0000

    update issue 100

--- trunk/La2etlak_Backend/app/controllers/flickr_accounts_controller.rb
+++ trunk/La2etlak_Backend/app/controllers/flickr_accounts_controller.rb
@@ -6,7 +6,6 @@ class FlickrAccountsController < ApplicationController
   API_KEY='443b0e6f88e26df854bdf0d7f7a8c1d5'
   SHARED_SECRET='ff3e3ebe8f31e039'
 
-  #@callback_url='http://localhost:3000/flickr_accounts/callback'
 
   FlickRaw.api_key=API_KEY
   FlickRaw.shared_secret=SHARED_SECRET
@@ -14,57 +13,89 @@ class FlickrAccountsController < ApplicationController
 
   def auth
 
-@user = User.find(1)
-  flickr = FlickRaw::Flickr.new
-  token = flickr.get_request_token(:oauth_callback => (
+    @user = User.find(1)
+    flickr = FlickRaw::Flickr.new
+    token = flickr.get_request_token(:oauth_callback => (
     'http://127.0.0.1:3000/users/flickr/callback'))
-  @user.name = token['oauth_token_secret']
-  @user.save
+    @user.name = token['oauth_token_secret']
+    @user.save
 
 
-  # You'll need to store the token somewhere for when the user is returned to the callback method
-  # I stick mine in memcache with their session key as the cache key
-
-
-auth_url = flickr.get_authorize_url(token['oauth_token'], :perms => 'read')
-  redirect_to(auth_url)
-
-
-
-  
+    auth_url = flickr.get_authorize_url(token['oauth_token'], :perms => 'read')
+    redirect_to(auth_url)
 
   end
 
-   def callback
+  def callback 
+    @user = User.find(1)
 
-    
-  flickr = FlickRaw::Flickr.new
-
-
- # request_token = User.find(1).name# Retrieve from cache or session etc - see above
-  
-  oauth_token = params[:oauth_token]
-  oauth_verifier = params[:oauth_verifier]
-  
-
-  raw_token = flickr.get_access_token(params[:oauth_token], User.find(1).name, params[:oauth_verifier])
+    flickr = FlickRaw::Flickr.new
+    oauth_token = params[:oauth_token]
+    oauth_verifier = params[:oauth_verifier]
+    raw_token = flickr.get_access_token(params[:oauth_token], User.find(1).name, params[:oauth_verifier])
 
 
   # raw_token is a hash like this {"user_nsid"=>"92023420%40N00", "oauth_token_secret"=>"XXXXXX", "username"=>"boncey", "fullname"=>"Darren%20Greaves", "oauth_token"=>"XXXXXX"}
   # Use URI.unescape on the nsid and name parameters
 
-  oauth_token = raw_token["oauth_token"]
-  oauth_token_secret = raw_token["oauth_token_secret"]
+    oauth_token = raw_token["oauth_token"]
+    oauth_token_secret = raw_token["oauth_token_secret"]
 
   # Store the oauth_token and oauth_token_secret in session or database
   #   and attach to a Flickraw instance before calling any methods requiring authentication
 
   # Attach the tokens to your flickr instance - you can now make authenticated calls with the flickr object
-  flickr.access_token = oauth_token
-  flickr.access_secret =[oauth_token_secret]
+    flickr.access_token = oauth_token
+    flickr.access_secret =oauth_token_secret
 
- render :text => "LOLOLOLOLOLO"
+    if @user.flickr_account
+      @user.flickr_account.delete
+    end
+
+  
+
+    flickr_account = FlickrAccount.create(consumer_key: oauth_token , secret_key: oauth_token_secret)
+    @user.flickr_account = flickr_account
+
+    unless flickr_account.new_record?
+      flash[:notice] = 'Flickr account created green'
+    else 
+      flash[:notice] = 'Flickr account couldn\'t be created red'
+    end 
+    redirect_to controller: 'users', action: 'connect_social_accounts'
+
+
+   # puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+
+
+   #  login = flickr.test.login
+   #  puts "You are now authenticated as #{login.username} with token #{flickr.access_token} and secret #{flickr.access_secret}"
+   #  puts "`````````````````````````````````````````````````````````````````````"
+   #  list   = flickr.photos.getRecent
+   #  # photos_list = Array.new
+
+   #  # list.each do |photo|
+   #  #   photos_list.push(photo)
+   #  # end
+   #  puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+   #  id     = list[0].id
+   #  puts id 
+   #  secret = list[0].secret
+   #  puts secret 
+
+   #  info = flickr.photos.getInfo :photo_id => id, :secret => secret
+   #  puts info 
+
+
+   #   puts info.title           # => "PICT986"
+   #   puts info.dates.taken     # => "2006-07-06 15:16:18"
+
+   #   puts '~~~~~~~~~~~~~~~~~~~~~~~'
+     #render :text => "HAAHAHAHAH"
+
+ 
 
     end
+
 
 end
