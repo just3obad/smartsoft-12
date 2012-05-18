d525fd633bd58b9be4794da3a0c3b0b3574ab496
diab977 <diab977@gmail.com>
2012-05-18 11:09:17 +0000

    Update issue 102 moving view tests to functional tests

--- La2etlak_Backend/test/functional/statistics_controller_test.rb
+++ La2etlak_Backend/test/functional/statistics_controller_test.rb
@@ -4,4 +4,20 @@ class StatisticsControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+   ##########Author: Diab ############
+  test "user statistics page has a list of users who added it" do
+   get :users, :id=>1
+   assert_select 'div[id = shared_stories]'
+ end
+
+ ##########Author: Diab ############
+  test "all interests page has div chart" do
+   get :all_interests, :id=>1
+    assert_select 'div[id = ccc]'
+   end
+   ##########Author: Diab ############
+  test "interest statistics page has a list of users who added it" do
+   get :interests, :id=>1
+   assert_select 'div[id = adders]'
+ end
 end
