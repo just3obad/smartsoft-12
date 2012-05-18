184d05689140b86bced442f5503f91e5530aa0a9
kerolos.bebawy@gmail.com <kerolos.bebawy@gmail.com@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-05-18 17:55:43 +0000

    Update issue 106
    added some tests

--- trunk/La2etlak_Backend/test/functional/user_sessions_controller_test.rb
+++ trunk/La2etlak_Backend/test/functional/user_sessions_controller_test.rb
@@ -5,7 +5,7 @@ class UserSessionsControllerTest < ActionController::TestCase
 	setup :activate_authlogic
 	
 	# Author: Kiro
-	test "should be able to create the session when requesting token RED" do
+	test "should be able to create the session when requesting token" do
 
    		post :requestToken, :user_session => { :email => 'ben@gmail.com', :password => 'benrocks'}
 			assert_not_nil assigns(:user_session)
@@ -16,7 +16,7 @@ class UserSessionsControllerTest < ActionController::TestCase
 	end
 
 	# Author: Kiro
-	test "session should not be created when requesting token RED" do
+	test "session should not be created when requesting token" do
 
 		post :requestToken, :user_session => { :email => 'nouser@gmail.com', :password => 'ahmedrocks'}
 		assert_nil assigns(:user), "created a session for a user that doesnt exist"
@@ -32,12 +32,41 @@ class UserSessionsControllerTest < ActionController::TestCase
 	end
 	
 	# Author: Kiro
-	test "should login the user with token RED" do
+	test "should login the user with token" do
 
 		assert_nil controller.session["user_credentials"]
    	post :login_with_token, :token => users(:ben).perishable_token
 		assert_equal controller.session["user_credentials"], users(:ben).persistence_token
 			
 	end
+	
+	test "when a user logs in a user_session should be created for him" do
+
+		post :create, :user_session => { :email => "ben@gmail.com", :password => "benrocks" }
+		assert !assigns(:user_session).new_record?, "The user session does not have a value"
+	
+	end
+
+	test "when an invalid user logs in a user_session should not be created for him RED" do
+
+		post :create, :user_session => { :email => "ben@gmail.com", :password => "benrock" }
+		assert assigns(:user_session).new_record?, "1-The user session should not be saved"
+
+		post :create, :user_session => { :email => "b@gmail.com", :password => "benrocks" }
+		assert assigns(:user_session).new_record?, "2-The user session should not be saved"
+
+		post :create, :user_session => { :email => "sdfsdf", :password => "benrocks" }
+		assert assigns(:user_session).new_record?, "3-The user session should not be saved"
+
+	end
+
+	test "after a user logs in he should be redirected to his mainfeed RED" do
+
+		post :create, :user_session => { :email => "ben@gmail.com", :password => "benrocks" }
+		assert !assigns(:user_session).new_record?, "The user session was no created"
+		assert_redirected_to({:controller => 'users', :action => 'feed'}, "The user wasn't directed to the main feed")
+
+	end
+
 
 end
