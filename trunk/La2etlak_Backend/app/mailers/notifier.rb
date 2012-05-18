001c3351cd519a3368e08ad80a63d8e7296ae69b
mouaz.alabsawi@gmail.com <mouaz.alabsawi@gmail.com@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-05-18 17:45:44 +0000

    update issue 112

--- trunk/La2etlak_Backend/app/mailers/notifier.rb
+++ trunk/La2etlak_Backend/app/mailers/notifier.rb
@@ -10,7 +10,7 @@ class Notifier < ActionMailer::Base
     #sent_on       Time.now
     #body  = :edit_password_reset_url => edit_password_reset_url(admin.perishable_token)
     @edit_password_reset_url = edit_password_reset_url(admin.perishable_token)
-mail(:to => admin.email, :subject => "Password Reset Instructions", :edit_password_reset_url => @edit_password_reset_url)
+mail(:to => admin.email, :subject => "Password Reset Instructions", :edit_password_reset_url => @edit_password_reset_url, :body => @edit_password_reset_url)
   end  
 
 end  
