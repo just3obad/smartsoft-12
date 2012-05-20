6c0520f8f64119a623735a0876663697a00043be
gosgosakila <gosgosakila@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-05-20 19:25:13 +0000

    Update issue 91 Labels: Test-Ready Fixed the test of this issue

--- La2etlak_Backend/test/unit/feed_test.rb
+++ La2etlak_Backend/test/unit/feed_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class FeedTest < ActiveSupport::TestCase
 
   # author : Gasser
-  test "admin should enter the same feed for more than one interest RED " do
+  test "admin should enter the same feed for more than one interest " do
      feed1 = Feed.new(:link=>"http://www.xkcd.com/rss.xml")
      feed2 = Feed.new(:link=>"http://www.xkcd.com/rss.xml")
      interest1 = Interest.create!(:name=>"interest1")
@@ -13,7 +13,7 @@ class FeedTest < ActiveSupport::TestCase
      feed1.save
      feed2.save
      assert_equal feed1.interest.name, "interest1"
-     assert_equal feed1.interest.name, "interest1"
+     assert_equal feed2.interest.name, "interest2"
   end
 
 
