7e449c00eb3fbc35f96154ac5766200cecf13d41
diab977 <diab977@gmail.com>
2012-05-20 16:05:07 +0000

    Update issue 102 updating tests for the new authentication system

--- La2etlak_Backend/test/functional/statistics_controller_test.rb
+++ La2etlak_Backend/test/functional/statistics_controller_test.rb
@@ -4,19 +4,27 @@ class StatisticsControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+ setup :activate_authlogic
+  
    ##########Author: Diab ############
   test "user statistics page has a list of users who added it" do
+    admin1 = Admin.create!(email:"admin1@gmail.com", password:"123456", password_confirmation:"123456")
+    AdminSession.create admin1
    get :users, :id=>1
    assert_select 'div[id = shared_stories]'
  end
 
  ##########Author: Diab ############
   test "all interests page has div chart" do
+    admin1 = Admin.create!(email:"admin1@gmail.com", password:"123456", password_confirmation:"123456")
+    AdminSession.create admin1
    get :all_interests, :id=>1
     assert_select 'div[id = ccc]'
    end
    ##########Author: Diab ############
   test "interest statistics page has a list of users who added it" do
+    admin1 = Admin.create!(email:"admin1@gmail.com", password:"123456", password_confirmation:"123456")
+    AdminSession.create admin1
    get :interests, :id=>1
    assert_select 'div[id = adders]'
  end
