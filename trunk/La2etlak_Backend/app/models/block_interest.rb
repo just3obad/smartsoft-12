e3a5b02e0b0f2ffbc51ae33744a8fc9f5c2643c4
rana.helal92@gmail.com <rana.helal92@gmail.com@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-05-20 09:07:40 +0000

    Update Issue 92 fixing relations to block_interest and block_story

--- trunk/La2etlak_Backend/app/models/block_interest.rb
+++ trunk/La2etlak_Backend/app/models/block_interest.rb
@@ -1,8 +1,8 @@
 class BlockInterest < ActiveRecord::Base
   attr_accessible :user_id, :interest_id
   
-  belongs_to :interest, class_name: "Interest"
-  belongs_to :user, class_name: "User"
+  belongs_to :blocked_interest, class_name: "Interest", foreign_key: "interest_id"
+  belongs_to :blocker, class_name: "User", foreign_key: "user_id"
   
   validates :interest_id, presence: true
   validates :user_id, presence: true
--- trunk/La2etlak_Backend/app/models/block_story.rb
+++ trunk/La2etlak_Backend/app/models/block_story.rb
@@ -1,8 +1,8 @@
 class BlockStory < ActiveRecord::Base
   attr_accessible :user_id, :story_id
   
-  belongs_to :story, class_name: "Story"
-  belongs_to :user, class_name: "User"
+  belongs_to :blocked_story, class_name: "Story", foreign_key: "story_id"
+  belongs_to :blocker, class_name: "User", foreign_key: "user_id"
   
   validates :story_id, presence: true
   validates :user_id, presence: true
