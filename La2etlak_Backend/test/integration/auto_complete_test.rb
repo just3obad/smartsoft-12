9ce64e20b7a01e5362377c0343252326b74b07fa
mina.workspace <mina.workspace@gmail.com>
2012-05-20 03:35:01 +0000

    Update issue 57 Labels: Test-Ready Update My Test to the Conventions

--- La2etlak_Backend/test/integration/auto_complete_test.rb
+++ La2etlak_Backend/test/integration/auto_complete_test.rb
@@ -1,35 +1,20 @@
 require 'test_helper'
-
+#Author MESAI
 class AutoCompleteTest < ActionDispatch::IntegrationTest
-  #this test is to make sure that the route is not lost 
-  test "AdminMainPageRoute" do
-     assert_routing '/admins/index', { :controller => "admins", :action => "index"}
-    end
-  #this test is to make sure that the main page does exist and i get a 200OK response     
-  test "AdminMainPage" do
-      get "/admins/index"
-      assert_response :success
-    end
+  setup do
+        @admin1 = Admin.create!(email:"admin1@gmail.com", password:"123456", password_confirmation:"123456")
+        post '/admin/login', :admin_session => {:email => 'admin1@gmail.com', :password => '123456'}
+      end
   #this test is to make sure that the route of auto complete is not lost 
   test "AutoCompleteRoute" do
      post 'autocomplete/auto_complete_for_autocomplete_query', {:controller =>"autocomplete" , :action => "auto_complete_for_autocomplete_query",:autocomplete=>{:query =>""}}
   end
-  #this test is to make sure that i have a field for search and autocomplete
-  test "AutoCompleteFieldExistance" do
-     get "/admins/index"
-    assert_select "input[id=autocomplete_query]"
-  end
-  #this test is to make sure that that there is a hidden div for results of auto complete
-  test "AutoCompleteResultDiv" do
-     get "/admins/index"
-    assert_select "div[id=autocomplete_query_auto_complete]"
-  end
-  #this test is to make sure that the auto completion really works
-  test "AutoCompleteTest" do
-     get "/admins/index"
-     User.create!(name: "dummy0", email: "dummy0@gmail.com", password:"123456" , password_confirmation: "123456")
-     post 'autocomplete/auto_complete_for_autocomplete_query', {:controller =>"autocomplete" , :action => "auto_complete_for_autocomplete_query",:autocomplete=>{:query =>"d"}}
-     @response.content_type = 'text/html'
-     assert_tag :tag =>"ul" , :child => {:tag => "li", :content =>"dummy0"}
-   end
+   #this test is to make sure that the auto completion really works
+   test "Admin should get result from the AutoCompletion" do
+   	  get "/admins/index"
+      User.create!(name: "dummy0", email: "dummy0@gmail.com", password:"123456" , password_confirmation: "123456")
+      post 'autocomplete/auto_complete_for_autocomplete_query', {:controller =>"autocomplete" , :action => "auto_complete_for_autocomplete_query",:autocomplete=>{:query =>"d"}}
+      @response.content_type = 'text/html'
+      assert_tag :tag =>"ul" , :child => {:tag => "li", :content =>"dummy0"}
+    end
 end
