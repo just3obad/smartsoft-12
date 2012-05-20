69aed36ef9db2842b6fee8a462dbc4b55cc0a3ef
EssamAhmedHafez <EssamAhmedHafez@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-05-20 12:41:53 +0000

    Update issue 90 Labels: Doc-Ready Code-Ready modified required documentation and code

--- trunk/La2etlak_Backend/app/controllers/tumblr_accounts_controller.rb
+++ trunk/La2etlak_Backend/app/controllers/tumblr_accounts_controller.rb
@@ -1,9 +1,23 @@
 class TumblrAccountsController < ApplicationController
   require 'Tumblr'
+  
+=begin
+  renders the login form login.html.erb
+  input: none
+  output: email and password
+  Author: Essam Hafez
+=end
   def login 
   
   end 
   
+=begin
+  creates the new tumblr account and relates it to the current user and saves the email and password the user entered
+  then redirected back to social accounts page
+  input: email and password
+  output: none
+  Author : Essam Hafez
+=end
   def connect_tumblr
     email = params[:email]
     password = params[:password]
@@ -12,8 +26,6 @@ class TumblrAccountsController < ApplicationController
     tm.password = password
     tm.save
     current_user.tumblr_account = tm
-    #user = User.first
-    #user.tumblr_account = tm
     redirect_to action:'connect_social_accounts' , controller:'users'
   end
   
--- trunk/La2etlak_Backend/app/models/tumblr_account.rb
+++ trunk/La2etlak_Backend/app/models/tumblr_account.rb
@@ -1,18 +1,22 @@
 class TumblrAccount < ActiveRecord::Base
-  attr_accessible :consumer_key, :secret_key
-  belongs_to :user_id, class_name: "User" 
+  belongs_to :user, class_name: "User" 
   
   #CONSUMER_KEY  = 'QQQxBUYP17iv7ByUdfP1jNfuAoSBwA3NxMoH7jlvo3ImjjVCNU '
   #SECRET_KEY = 'XX8QVI0fUhU1j5v2nypLq67aCIkWBhA06bB19dYh42ahd6akdo'
   
   #get_feed issue 90
-  # gets user tumblr feeds
-  # a gem is used called matenia-tumblr-api
-  # the user enters his email and password in the login.html.view,
-  # values are added to the tumbr account model and saved
-  #when get_feed is called the authentication is done through the first line
-  # then blog name is get
-  # finally looping and converting blogs into stories
+=begin
+     gets user tumblr feeds
+     a gem is used called matenia-tumblr-api
+     the user enters his email and password in the login.html.view,
+     values are added to the tumbr account model and saved
+     when get_feed is called the authentication is done through the first line
+     then blog name is get
+     finally looping and converting blogs into stories
+   Input: no input  
+   Output: list of stories
+   Author: Essam Hafez
+=end
   
   def get_feed
     new_tumblr = Tumblr::User.new(self.email, self.password) #Authentication
@@ -22,28 +26,35 @@ class TumblrAccount < ActiveRecord::Base
     posts = Tumblr::Post.all #Get user posts
     posts.each do |post| #loop and convert to story type
       s = TumblrAccount.convert_blog_to_story(post)
-      stories.push(s)
+      stories.push(s) #adds element to list
     end
-      stories
+    stories #returned
   end
   
+
+=begin
   #gets a blog and grabs out the needed content from it to fit our application
   #a story needs a title, content and media link
-  
-    def self.convert_blog_to_story(post)
-      story = Story.new #new Story
-      story.instance_variables
-      story.category = 'tumblr'
-      story.title = post["slug"] # title encapsulated into "slug" hash
-      s = post["type"] # get post type
-      x = s <=> ("photo")  # checks if the story type is photo
-      if(x==0)
-        photo_type = post["photo_url"] 
-        story.media_link = post["photo_url"][3]["__content__"] #gets photo @ position 3 with max width of 250px
-        story.content = post["photo_caption"]
-      else
-        story.content = post["regular_body"]
-      end
-      return story
+  input: blog from tumblr
+  output: story
+  Author: Essam Hafez
+=end  
+  def self.convert_blog_to_story(post)
+    story = Story.new #new Story
+    #story.instance_variables
+    story.category = 'tumblr' #sets story category to tumblr
+    story.title = post["slug"] # title encapsulated into "slug" hash
+    s = post["type"] # get post type
+    x = s <=> ("photo")  # checks if the story type is photo 
+    if(x==0) # story is a photo
+      photo_type = post["photo_url"] #Grabs required parameters 
+      story.media_link = post["photo_url"][3]["__content__"] #gets photo @ position 3 with max width of 250px
+      story.content = post["photo_caption"] # get caption in case phoro can be loaded
+    elsif(x==1)
+      story.content = post["regular_body"] #story body encapsulated in regular body
+    else
+      story.content = ""
     end
+    return story
+  end
 end
