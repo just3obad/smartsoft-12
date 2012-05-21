0a576867b824594d6dee91b39cb63887796217a3
elmenisy <elmenisy@gmail.com>
2012-05-21 23:35:41 +0000

    Update issue 97 Moved Facbook account functionality to  facebook accounts controller.

--- La2etlak_Backend/app/controllers/users_controller.rb
+++ La2etlak_Backend/app/controllers/users_controller.rb
@@ -66,40 +66,6 @@ class UsersController < ApplicationController
 		end
 	end
 
- # Action to be called from the connet_to_social network view
-  # which redirects to the facebook api using koala
-  # Author: Menisy
-  def authenticate_facebook_init
-    path = Koala::Facebook::OAuth.new.url_for_oauth_code(:callback => "http://localhost:3000/fb/done/",:permissions => "read_stream")  
-    redirect_to path
-  end
-
-  # Action to be called in the call back url after authentication take place
-  # Author: Menisy
-  def authenticate_facebook_done
-    if !params[:code]
-      flash[:notice] = "Facebook account was not added red"
-      redirect_to :controller => "users", :action => "feed"
-      return
-    end
-    token = Koala::Facebook::OAuth.new("http://localhost:3000/fb/done/").get_access_token(params[:code]) if params[:code]
-    fb_account = FacebookAccount.find_or_create_by_user_id(current_user.id)
-    fb_account.auth_token = token
-    fb_account.auth_secret = 1
-    graph =  Koala::Facebook::API.new(token)
-    user = graph.get_object("me")
-    fb_id = user["id"]
-    fb_account.facebook_id = fb_id
-    if fb_account.save
-      fb_account.add_to_log
-      flash[:notice] = "Facebook account was added successfully green"
-      redirect_to :controller => "users", :action => "connect_social_accounts"
-    else
-      flash[:notice] = "Facebook account was not added red" + user.to_s
-      redirect_to :controller => "users", :action => "connect_social_accounts"
-    end
-  end
-
 =begin
 	Description: This method takes the email that the user entered and tries
 	to find it in the database, if this email doesn't exist
