408dcc8419bfb3e130be7d50ab9ff57719b28c2e
EssamAhmedHafez <EssamAhmedHafez@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-05-21 21:37:00 +0000

    Update issue 90 Tumblr to tumblr

--- La2etlak_Backend/app/models/tumblr_account.rb
+++ La2etlak_Backend/app/models/tumblr_account.rb
@@ -1,6 +1,6 @@
 class TumblrAccount < ActiveRecord::Base
   belongs_to :user, class_name: "User" 
-  require 'Tumblr'
+  require 'tumblr'
   #CONSUMER_KEY  = 'QQQxBUYP17iv7ByUdfP1jNfuAoSBwA3NxMoH7jlvo3ImjjVCNU '
   #SECRET_KEY = 'XX8QVI0fUhU1j5v2nypLq67aCIkWBhA06bB19dYh42ahd6akdo'
   
