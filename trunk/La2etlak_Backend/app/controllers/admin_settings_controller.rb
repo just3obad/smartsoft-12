db7e26289744aa8cd7115cc4fbb8abebefcb14a9
gosgosakila <gosgosakila@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-05-23 18:38:10 +0000

    Update issue 69 Handling failure scenarios

--- trunk/La2etlak_Backend/app/controllers/admin_settings_controller.rb
+++ trunk/La2etlak_Backend/app/controllers/admin_settings_controller.rb
@@ -5,26 +5,35 @@ class AdminSettingsController < ApplicationController
   	def index
   		@admin = Admin.new
       @admin2 = current_admin
-  	end
-	
-	# author : Gasser
-	# a method in the admin_settings controller passed from the show page of the settings page
-	# this function just calls the static method in the model and passes the value entered in the 
-	# form then it renders the show page again.
+  	end	
+=begin	
+	Method Description: A method in the admin_settings controller passed 
+	from the show page of the settings page this function just calls the 
+	static method in the model and passes the value entered in the form 
+	then it redirects to the settings page again.
+	Author : Gasser
+=end
 	def configure_flags_threshold
 		if params[:auto] == "on"
-			Admin_Settings.configure_flags_threshold params[:valuee] , false
-		else 
-			Admin_Settings.configure_flags_threshold params[:valuee] , true
-		end 
-		#if $flash_success = "true" 
-		#	flash[:success] = "The settings are changed successfully"
-		#end
-		if $flash_error ==  "true"
-			flash[:error] = "Please enter a valid string"
+			if params[:valuee].to_i == 0
+			#	flash[:not_entering_numbers] = "Please enter a valid number. $red"
+				Admin_Settings.configure_flags_threshold $current_flags_threshold , false
+			end
+		else
+			if params[:valuee].to_i == 0
+				flash[:not_entering_numbers] = "Please enter a valid number. $red"
+				Admin_Settings.configure_flags_threshold $current_flags_threshold , true
+			else
+				flash[:success] = "Settings changed successfully. $green"
+				Admin_Settings.configure_flags_threshold params[:valuee] , true
+			end
 		end
-		
-
+		if @flash_success == "true"
+			flash[:success] = "Settings changed successfully. $green"
+		end
+		#if $current_auto_hiding == 0
+		#	flash[:changed_settings] = "Settings changed successfully. $green"	
+		#end
 		redirect_to '/admin_settings'		
 	end
 	
