363be73a2e0f9a533482c4cc5f329cce313a28d1
elmenisy@gmail.com <elmenisy@gmail.com@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-05-23 03:58:57 +0000

    Update issue 93 Modified and added tests

--- trunk/La2etlak_Backend/test/functional/stories_controller_test.rb
+++ trunk/La2etlak_Backend/test/functional/stories_controller_test.rb
@@ -6,63 +6,76 @@ setup :activate_authlogic
   #   assert true
   # end
 
- setup :activate_authlogic
+
 
   # Author: Menisy
   test "should up comment" do
+    user1 = users(:ben)
+    UserSession.create(user1)
+    comment = Comment.new
+    comment.user = users(:one)
+    comment.story = stories(:one)
+    comment.content = "testing testing 1 2"
+    comment.save!
   	assert_difference('CommentUpDown.find_all_by_action(1).count',1) do
-  		get :up_comment, :comment_id => Comment.first.id, :user_id => User.first.id
+  		get :up_comment, :comment_id => comment.id
   	end
-  	assert_redirected_to :action => "mobile_show", :id2 => User.first.id
+  	assert_redirected_to :action => "get"
   end
 
   # Author: Menisy
   test "should down comment" do
+    user1 = users(:ben)
+    UserSession.create(user1)
+    comment = Comment.new
+    comment.user = users(:one)
+    comment.story = stories(:one)
+    comment.content = "testing testing 1 2"
+    comment.save!
   	assert_difference('CommentUpDown.find_all_by_action(2).count',1) do
-  		get :down_comment, :comment_id => Comment.first.id, :user_id => User.first.id
+  		get :down_comment, :comment_id => comment.id
   	end
-  	assert_redirected_to :action => "mobile_show", :id2 => User.first.id
+  	assert_redirected_to :action => "get"
   end
 
   # Author: Menisy
   test "should see notification" do
-  	requester = User.find(2)
-  	requester.invite User.first
-  	get :mobile_show, :id => Story.first.id , :id2 => User.first.id
+    user1 = users(:ben)
+    UserSession.create(user1)
+  	requester = users(:five)
+  	requester.invite user1
+  	get :recommend_story_mobile_show, :sid => Story.first.id
   	assert_select 'div[id=notification]',true
   end
 
   # Author: Menisy
   test "should not see notification" do
-  	get :mobile_show, :id => Story.first.id , :id2 => User.find(3)
+    user1 = users(:one)
+    UserSession.create(user1)
+  	get :recommend_story_mobile_show, :sid => Story.first.id
   	assert_select 'div[id=notification]',false	
   end
 
-  test "should be redirected to pending requests page RED" do
-  	# will still be implemented as soon
-  	# as the pending requests page gets
-  	# implemented by Yahia
-  	assert false, "to be implemented"
-  end
-
-
   # Author: Menisy
-  test "mobile show view" do
+  test "get show view" do
+    user1 = users(:ben)
+    UserSession.create(user1)
   	story = Story.first
+    story.interest = interests(:one)
+    story.rank = 5
+    story.save!
   	comment = Comment.new
   	comment.content = "lol"
   	comment.user = User.first
   	comment.story = story
   	comment.save
-  	get :mobile_show, :id => Story.first.id , :id2 => User.first.id
+  	get :mobile_show, :id => story.id
   	assert_select 'ul.nav' do
-  		assert_select 'li', 6				# 2 for the navbar + 4 four the 1st comment
+  		assert_select 'li', true
 		end
-		assert_select 'div[class=story-component-box-mobile]',1
 		assert_select 'input[class=btn btn-large]',1
-		assert_select 'div[class=story-comment-box-mobile]',true
-		assert_select 'textarea[class=story-textarea-mobile]',1
-		assert_select 'title', {:count => 1, :text => "My Story Page!"},
+		assert_select 'div[class=story-comment-box-mobile well]',true
+		assert_select 'title', {:count => 1, :text => "LA2ETLAK!"},
     "Wrong title or more than one title element"
 	end
 
