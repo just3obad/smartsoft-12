976a30efec22b2eeb020b53090538999abe0cf44
gosgosakila <gosgosakila@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-05-23 18:37:36 +0000

    Update issue 68 Handling failure scenarios

--- La2etlak_Backend/app/models/admin_settings.rb
+++ La2etlak_Backend/app/models/admin_settings.rb
@@ -1,55 +1,48 @@
 class Admin_Settings < ActiveRecord::Base
   attr_accessible :key, :value
-  # author: Gasser
-  # A static method that takes as an input the threshold of the flags 
-  # if first loads the flags_thershold value from the admin_settings table 
-  # then it checks if the input passed from the from was not empty then 
-  # it parses the input to integer, and checks if the admin entered a number
-  # that is less than the minimum threshold, it forces it to be 30 (Default 
-  # Value) If the number was bigger than 30 then it updates the table with 
-  # the new value and saves this update.
-  # Then it loops on all the stories to update their status by checking the 
-  # number of flags for each one if it is greater than the threshold it hides
-  # this story else it un-hides it.This is done by setting the hidden flag in 
-  # each story.All of that after checking the global variable auto_hiding which
-  # the admin changes from the checkbox.
+
+=begin
+  Method Descritpion: A static method that takes as an input the threshold 
+  of the flags if first loads the flags_thershold value from the admin_settings
+  table then it checks if the input passed from the from was not empty then 
+  it parses the input to integer, and checks if the admin entered a number
+  that is less than the minimum threshold, it forces it to be 30 (Default 
+  Value) If the number was bigger than 30 then it updates the table with 
+  the new value and saves this update. Then it loops on all the stories to 
+  update their status by checking the number of flags for each one if it is
+  greater than the threshold it hides this story else it un-hides it.This is
+  done by setting the hidden flag in each story.All of that after checking 
+  the global variable auto_hiding which the admin changes from the checkbox.
+  Parameters: (flags_number, auto_hiding flag)
+  Author: Gasser
+=end
   def self.configure_flags_threshold (flags_number, disable)
-      #$flash_success = "false"
-      $flash_error = "false"
+      @flash_success = "false"
+      @flash_error = "false"
       flags_threshold = Admin_Settings.find_by_key("flags_threshold")
       auto_hiding = Admin_Settings.find_by_key("auto_hiding")
-      current_flags = flags_threshold.value
+      # current_flags = flags_threshold.value
       flags = flags_number.to_i
   		if disable == true
         auto_hiding.value = 1
-  		  if !(flags == 0)
-  			  if flags < 30
-  				  flags_threshold.value = current_flags
-            #$flash_success = "false"
-  			  else
-            #$flash_success = "true"
-  				  flags_threshold.value=flags
-	  		  end
-  			  flags_threshold.save
-  			  stories=Story.all
-  			  stories.each do |story|
-  				  if story.flags.count >= flags
-  					 story.hidden = true
-  				  elsif story.flags.count < flags
+  			flags_threshold.value=flags
+  		  flags_threshold.save
+        @flash_success = "true"
+  		  stories=Story.all
+			  stories.each do |story|
+ 				  if story.flags.count >= flags
+   					 story.hidden = true
+				  elsif story.flags.count < flags
   					 story.hidden = false
-            end
-            story.save
           end
-        else 
-          #$flash_success = "false"
-          $flash_error = "true" 
+            story.save
         end
       else
         auto_hiding.value = 0
       end
       auto_hiding.save
-      $current_auto_hiding = Admin_Settings.find_by_key("flags_threshold").value
-      $current_flags_threshold = Admin_Settings.find_by_key("auto_hiding").value
+      $current_auto_hiding = Admin_Settings.find_by_key("auto_hiding").value
+      $current_flags_threshold = Admin_Settings.find_by_key("flags_threshold").value
   end
 
   # author : Gasser
