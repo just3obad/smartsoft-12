e98a0eeece0753898a5a7b22c1c978ebf27a8d91
bassemawny92@gmail.com <bassemawny92@gmail.com@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-05-23 15:59:29 +0000

    RSS bug fixed

--- trunk/La2etlak_Backend/app/controllers/feeds_controller.rb
+++ trunk/La2etlak_Backend/app/controllers/feeds_controller.rb
@@ -12,39 +12,37 @@ end
 def new 
   @feed = Feed.new
 end
-
-  def create
+=begin
+  Method Description: Here I find the feed by the link given from the form and check if it is 
+  present in the database or not and if it is present I give him an error message to tell him
+  that he can not add the same RSS Feed for more than 1 interest.
+  Author: Gasser
+=end 
+ def create
     #@id1 = Feed.find(params[:id]).interest_id
     $saved
     $savedinterest = false
-=begin
-  Method Description: Here I find the feed by the link given from the form and check if it is 
-  present in the database or not and if it is present I give him a warning message to notify him that 
-  this feed already exists in another interest.
-  Author: Gasser
-=end 
-    feed = Feed.find_by_link(params[:feed][:link])  
-    if !feed.nil? 
-      flash[:notify] = "Be Careful, You have entered this RSS Feed in another interest before."   
-    end
-    # end of my code
+
+    feed = Feed.find_by_link(params[:feed][:link]) 
     @feed= Feed.new(params[:feed]) #retrieving the feed from the database using the table feed parameters
-    @interestid= params[:feed][:interest_id]
-    if params.has_key?(:feed) and StoriesHelper.check_rss(params[:feed][:link])
-     if @feed.save
-      $saved=true
+    @interestid= params[:feed][:interest_id] 
+    if feed.nil?    
+      if params.has_key?(:feed) and StoriesHelper.check_rss(params[:feed][:link])
+        if @feed.save
+          redirect_to :controller => 'interests', :action => 'show', :id => @interestid
+          flash[:feed_added] = "Link added successfully. $green"
+          StoriesHelper.fetch_rss(params[:feed][:link])
+          Log.create(loggingtype:3, interest_id:@interestid, message:"A new RSS link is added.")
+        else #if saving faild a flash message box will appear with the errors
+        flash[:invalid_link] = "Link is invalid. Please try again" 
+        redirect_to :controller => 'interests', :action => 'show', :id => @interestid
+       end 
+     end
+    else 
+      flash[:same_rss_feed] = "Sorry, you cannot add the same RSS feed more than once $red"
       redirect_to :controller => 'interests', :action => 'show', :id => @interestid
-      flash[:success] = "Link added successfully."
-      StoriesHelper.fetch_rss(params[:feed][:link])
-      Log.create(loggingtype:3, interest_id:@interestid, message:"A new RSS link is added.")
-      end
-    else #if saving faild a flash message box will appear with the errors
-     $saved=false
-     flash[:error] = "Link is invalid. Please try again" 
-     redirect_to :controller => 'interests', :action => 'show', :id => @feed.interest_id
     end
-    
-  end
+end
 
   def destroy
         @id = Feed.find(params[:id]).interest_id
