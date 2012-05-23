779532e29e0ed3942ac0822440d39b50e0e4f3ab
EssamAhmedHafez <EssamAhmedHafez@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-05-23 13:53:08 +0000

    Update issue 88 moved test from functional to unit

--- trunk/La2etlak_Backend/test/functional/users_controller_test.rb
+++ trunk/La2etlak_Backend/test/functional/users_controller_test.rb
@@ -133,19 +133,7 @@ class UsersControllerTest < ActionController::TestCase
 # $$$$$$$$$$$$$$$$$$$ 3OBAD $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$  
 
   
-  #Author : Essam
-  test "filter selected social account view RED" do
-    user = User.new
-    user.email = 'essam@hafez.com'
-    user.twitter_account = twitter_accounts(:one)
-    user_feed = user.twitter_account.get_feed()
-    assert !user_feed.nil?
-    # add new route , match "user/social_feed/:id/:socialID" => "users#view_social_network_feed"
-    assert_routing "user/social_feed/:id/:socialID" , { :controller => "users" , :action => "view_social_network_feed"}
-    get "user/social_feed/:id/:socialID"
-    assert_response :success
-  end
-  
+
   #Author : Mina Adel
   test "show main feed" do
     user = User.new(:email => "test@test.com")
