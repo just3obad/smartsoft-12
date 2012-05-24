a8ab0b87c6e8f4042bbf77549719ba9ce75e33f9
gosgosakila <gosgosakila@678c7408-d971-e1f1-1d7b-832187e7e2a5>
2012-05-24 17:21:19 +0000

    Update issue 68 Labels: Scenario-Ready Added the button in the settings page

--- trunk/La2etlak_Backend/config/routes.rb
+++ trunk/La2etlak_Backend/config/routes.rb
@@ -219,6 +219,7 @@ NokiaRuby::Application.routes.draw do
 #L $$$$$$$$$$$$$$$$ MESAI $$$$$$$$$$$$$$$$$$$$$$$
   
   # $$$$$$$$$$$$$$ GASSER $$$$$$$$$$$$$$$$$$$$$$$
+  match 'admin_settings/configure_auto_hiding', to: 'admin_settings#configure_auto_hiding'
   match '/users/force_reset_password/:id', to: 'users#force_reset_password'
   #get 'admin_settings', to: 'admin_settings#index'
   post 'admin_settings/configure_flags_threshold', to: 'admin_settings#configure_flags_threshold'  
