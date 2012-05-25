d6c7b3d47ab4fd6688d4180469163e2bf91403c9
guc <guc@sakr.me>
2012-05-25 18:00:49 +0000

    Update issue 68 Applied patch submitted by Yahia to fix startup bug caused by initializers/admin_settings

--- La2etlak_Backend/config/initializers/admin_settings.rb
+++
@@ -1,5 +0,0 @@
-	if Admin_Settings.find_by_key("flags_threshold").nil?
-		Admin_Settings.create!(:key=>"flags_threshold", :value=>10)
-		Admin_Settings.create!(:key=>"auto_hiding", :value=>1)
-		Admin_Settings.create!(:key=>"statistics_time_span", :value=>30)
-	end
